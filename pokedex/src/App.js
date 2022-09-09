d054b255f3ed2965976f046236c68a1c3343aa20
Frank Collins <fwc3rd@gmail.com>
2022-09-09 18:03:16 -0400

    [App.js] BrowserRouter and component import/route

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Main from './components/webpage/MainScreen'
 
 import './App.css';
@@ -5,6 +6,14 @@ import './App.css';
 function App() {
   return (
     <div className="Home-Body Column-Center">
+      <BrowserRouter>
+      <Routes>
+
+      <Route path={'/'} element={<Main/>}/>
+      
+      </Routes>
+
+      </BrowserRouter>
       <Main/>
     </div>
   );
