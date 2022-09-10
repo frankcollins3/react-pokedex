91bd3c9fe6d6ab15d981b0747c8cb612f0911194
Frank Collins <fwc3rd@gmail.com>
2022-09-10 11:32:29 -0400

    [Screen] created input & button to manipulate createRef()len state

--- pokedex/src/components/element/Screen.js
+++ pokedex/src/components/element/Screen.js
@@ -4,6 +4,9 @@ import React, { useEffect, useState, useRef, createRef } from 'react';
 
 function Screen() {
     const [pokemon, setPokemon] = useState([])
+    const [refLength, setRefLength] = useState()
+    const [isInputHovered, setIsInputHovered] = useState('false')
+    const [hoverCount, setHoverCount] = useState(0)
 
 
 
@@ -18,14 +21,39 @@ function Screen() {
     }, [])
 
     const checkRefs = () => {
-        // console.log('pokemon')
-        // console.log(pokemon)
+        console.log('refLength')
+        console.log(refLength)
+    }
+
+    const updateValue = ( {target: {value}}) => {   // didn't know you could set {value} object as value for key value pair.
+        console.log(`value ${value}`)
+        if (value) console.log(value)
+        setRefLength(value) // this is basically event.target.value
+    }
+
+    // const inputEnter = () => setIsInputHovered(true)
+    const inputEnter = async (event) => { 
+        await setHoverCount(hoverCount + 1)
+
+        if (isInputHovered == 'false') await setIsInputHovered('true') 
+    }
+    const inputExit = async (event) => { if (isInputHovered == 'true' && hoverCount < 3) setIsInputHovered('false') }
+
+    const handleInput = async (event) => {        
+        let inputval = event.target.value
+        let isInputInteger = inputval.replace(/[a-z]/g, '')     // this is escaping/removing all letters/alpha-char. 
+        if (isInputInteger.length) setRefLength(isInputInteger) // checking against refLength, which would only pass  if it was a non [a-z] set of numbers/char
+        else return // if its true, return/stop/get-out.
     }
 
     return (
         <div className="Screen-Wrapper">
+            <input onMouseEnter={inputEnter} onMouseLeave={inputExit} onChange={handleInput}type="text" value={isInputHovered == 'false'  ? 'How Many Pokemon?' : '' }/>
+            {/* <input onMouseEnter={inputEnter}onChange={inputEnter}type="text" value={isInputHovered == 'false' ? 'how many pokemon?' : '' }/> */}
+
             <button onClick={APIcall} className="navBall" id="Pokeball"> </button>      
-            <button onClick={checkRefs} className="navBall" id="Greatball"> </button>
+            <button onClick={updateValue} className="navBall" id="Greatball"> </button>
+            <button onClick={checkRefs} className="navBall" id="Ultraball"> </button>
 
         <div className="Screen Column-Between">
                {pokemon.map( (pokemap, index) => {
