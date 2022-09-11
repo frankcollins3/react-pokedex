462f66a3c186b51731968be74dbe6651f3f30efc
Frank Collins <fwc3rd@gmail.com>
2022-09-10 23:45:56 -0400

    [Screen.js] react-jquery-plugin init && $(element).behavior(test)

--- pokedex/src/components/element/Screen.js
+++ pokedex/src/components/element/Screen.js
@@ -1,14 +1,24 @@
 import APIcall from '../utility/pokeAPI'
 
 import React, { useEffect, useState, useRef, createRef } from 'react';
+import { $ } from 'react-jquery-plugin';
+
+console.log({ $ })
+
 
 function Screen() {
-
+    // state and jquery functions
     const [pokemon, setPokemon] = useState([])
     const [refLength, setRefLength] = useState()
+
     const [preInputValue, setPreInputValue] = useState([])
     const [isInputHovered, setIsInputHovered] = useState('false')
+    const [dexClick, setDexClick] = useState('false')
+
     const [hoverCount, setHoverCount] = useState(0)
+
+
+
 
     let pokeRefs = useRef([]);      
     // let pokeRefs = [createRef(), createRef()]
@@ -20,7 +30,16 @@ function Screen() {
         })
     }, [])
 
+    const checkRefs = () => {        
+        console.log('in the checkRefs function')
+        pokeRefs.current.map( (el, i) => {
+            console.log('el')
+            console.log(el)
+        })
+    }
+
     const updateValue = ( {target: {value}}) => {   // didn't know you could set {value} object as value for key value pair.
+        console.log(`value ${value}`)
         if (value) console.log(value)
         setRefLength({value}) // this is basically event.target.value
     }
@@ -36,10 +55,13 @@ function Screen() {
         return    
     }
 
+
+    // have to get these two strings connected.
+
     const handleInput = async ({ target: {value}}) => {      
         let inputval = value // event.target.value
         let isInputInteger = value.replace(/[a-z]/g, '')     // this is escaping/removing all letters/alpha-char. 
-        const newInput = `${preInputValue} ${inputval}`                       
+        const newInput = `${preInputValue} ${inputval}`                        
         let cleaninput = newInput.replace(/[\s[a-z]/g, '') 
         let length = cleaninput.length - 1
         // let length = cleaninput.length - 1
@@ -52,6 +74,10 @@ function Screen() {
             await setPreInputValue(`${preInputValue} ${inputstate}`)
         }
 
+
+
+// setPreInputValue(preInputValue.toString() + {value}) no/fair-guess ([object Object][object Object][object Object])
+
         if (isInputInteger.length) { 
         await setRefLength(isInputInteger) 
         pokeRefs.current = pokeRefs.current.splice(0, isInputInteger)    
@@ -61,48 +87,69 @@ function Screen() {
         }
         else {
             setRefLength('')
+            console.log('no integer')
             return 
-        }}
-    
+        }    
+            // if its true, return/stop/get-out.
+        // else return 
+    }
+
+    const keyDownHandler = (event) => {
+        console.log('lets see that')
+
+    }
+    keyDownHandler()
+
+    $('.Close-Pokedex').click((event)=> {
+        // $(event.target).css('border', '20px solid orange');
+        $(event.target).addClass('Pokedex-Animate')
+        // $(event.target).css('background-image', "url('/img/openPokeDex.png')")
+        console.log($(event.target))
+        let classbucket = $(event.target)[0].attributes.class.textContent
+        
+
+
+
+    })
+
+
+    if (dexClick == 'true') {
     return (
-        <div className="Screen-Wrapper">
+        <>
+
+            <div className="Screen-Wrapper">
             <div className="Input-Wrapper Column-Center">                
             <input id={'Screen-Input'} onMouseEnter={inputEnter} onMouseLeave={inputExit} onChange={handleInput}type="text"/>
             <label htmlFor={'Screen-Input'}> {preInputValue == 'undefined' ? '' : preInputValue}  </label>
             </div>
 
-             <button onClick={APIcall} className="navBall" id="Pokeball"> </button>      
-            <button onClick={updateValue} className="navBall" id="Greatball"> </button>
-            <button onClick={checkRefs} className="navBall" id="Ultraball"> </button> 
-
         <div className="Screen Column-Between">
                <ul>
                {pokeRefs.current.map((el, i) =>
                     <div key={`key${i}`} className="Map-Parent">
-                        <p key={i}> {i + 1}</p>
                         <img 
                         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`}
                         />
+                        {/* <p key={i}> {el.name} </p>  took coming up an on-ramp to see why this wouldn't work. The original pokemon reference is gone.              */}
                     </div>
                     )}
                     </ul>      
         </div>               {/* screen end  */}
-
         </div>
+                    </>
+    )
+}       // if (dexClick  == 'true') { end }
+else { 
+    return (
+            <div className="Close-Pokedex"> </div>
+
+            
     )
 }
+}       // function Screen() { end }
 export default Screen
 
 
-// 
-{/* {pokemon.map( (pokemap, index) => {
-                return (
-                    <div key={index+1}  className="Map-Parent">
-                    <li className="Poke-Card" key={index}> {pokemap.name} </li>                    
-                    </div>
-                    )
-                })}   */}
-
-
-            {/* <input onMouseEnter={inputEnter} onMouseLeave={inputExit} onChange={handleInput}type="text" value={isInputHovered == 'false'  ? 'How Many Pokemon?' :  }/> */}
-            {/* <input onMouseEnter={inputEnter}onChange={inputEnter}type="text" value={isInputHovered == 'false' ? 'how many pokemon?' : '' }/> */}
+{/* <button onClick={APIcall} className="navBall" id="Pokeball"> </button>      
+<button onClick={updateValue} className="navBall" id="Greatball"> </button>
+<button onClick={checkRefs} className="navBall" id="Ultraball"> </button>  */}
