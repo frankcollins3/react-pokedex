c832bcd80f0297da64e585bd1820a623f0370ef4
Frank Collins <fwc3rd@gmail.com>
2022-09-14 11:27:11 -0400

    [Utility] pure function() {
    This pure function takes in 2 arguments
    
    function ClassAction(elem, class) {
         // the element to apply our class
        //  the className (as string) to do so. 
    }
    the intention is to take our [Bootstrap.js] <BootstrapButton> and give it some life by adding a CSS class with @keyframes animation
    to rock the ball back and forth, just like the original pokemon game.
    This ball rocking back and forth will [hint || indicate] that we would be saving this pokemon to our database, or that we just initiated behavior to do so.

---
+++ pokedex/src/components/utility/ClassAction.js
@@ -0,0 +1,10 @@
+// 2 arguments the element and the className we will be giving it.
+export default function ClassAction(event, elem, action) {
+    console.log(event)
+    console.log(event.target)
+
+    console.log(elem) // string
+    console.log(action) // strin
+    console.log('hey here we go!');
+}
+
