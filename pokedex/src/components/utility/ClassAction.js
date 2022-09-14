b9430aa6a07e03fc86a636e18012053ebf7419e7
Frank Collins <fwc3rd@gmail.com>
2022-09-14 12:43:18 -0400

    [UtilityFunctions/ClassAction] upon Action () => setClass!
    This works! 
    
    ... dont mean to pass judgment but:
    I now see why so many React.js jobs are also node/nearly-full-stack jobs
    
    I'm questioning amongst myself why someone would be into react and shun doing database/back-end work
     when so much of best-practices-reactjs is based on importing/exporting functionality and referenced elements, which is exactly what database/modeling is.
    
    👍 
    we dont need those arrays but it was a great idea.
    I got the idea from a piece of suggestion:
    during the ejs pokedex i had problems with the pokemon being able to sometimes be rendered randomly nearly right next to each other.
    The piece of advice was mentioning to store every randomly-appended randomFromPokeAPI-pokemon into an array, and use that array as a catch:
    
    Save the locations appended and don't allow a pokemon to be re-appended.

--- pokedex/src/components/utility/ClassAction.js
+++ pokedex/src/components/utility/ClassAction.js
@@ -1,10 +1,31 @@
-// 2 arguments the element and the className we will be giving it.
-export default function ClassAction(event, elem, action) {
-    console.log(event)
-    console.log(event.target)
+import { $ } from 'react-jquery-plugin'; 
 
-    console.log(elem) // string
-    console.log(action) // strin
+// 2 arguments the element and the className we will be giving it.
+export default function ClassAction(elem, classToSet) {
+    const elemArray = [] || new Array()
+    const classArray  = new Array() || [];
+    
+    console.log('elem')
+    console.log(elem)
+
+    console.log('classToSet')
+    console.log(classToSet)
+    // elem.forEach( (el) => {
+    //     elemArray.push(el)
+    // })
+
+    // $(classToSet).each( (c) => {
+    //     classArray.push(c)
+    // })
+
+    // console.log(elemArray)
+    // console.log(elemArray[0])
+    // console.log('jq')
+    // console.log($(elemArray)[0])
+
+    elem.css('border', '5px solid hotpink')
+    elem.addClass(classToSet)
+
     console.log('hey here we go!');
 }
 
