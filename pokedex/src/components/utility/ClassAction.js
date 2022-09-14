c9c3d956641ba830e07b8d3ee6b4398c608a557e
Frank Collins <fwc3rd@gmail.com>
2022-09-14 13:19:55 -0400

    [ClassAction] new parameter to separate {+ || -} > new component to do so
    instead of creating a new utility exported-utility-function component,
    A third parameter (set) has been added to this function.
    
    *** just added [typeof] to our function to verify we would be having as string as our argument before producing any changes/output/ ***

--- pokedex/src/components/utility/ClassAction.js
+++ pokedex/src/components/utility/ClassAction.js
@@ -1,31 +1,21 @@
 import { $ } from 'react-jquery-plugin'; 
 
 // 2 arguments the element and the className we will be giving it.
-export default function ClassAction(elem, classToSet) {
-    const elemArray = [] || new Array()
-    const classArray  = new Array() || [];
-    
-    console.log('elem')
-    console.log(elem)
+export default function ClassAction(set, elem, classToSet) {
+    console.log(`set: ${set} elem: ${elem} classToSet: ${classToSet}`)
+    if (typeof set === 'string') {
+        console.log("weve got a string")
+    } else { console.log("hey man whats up with you")}
+    // i struggled with thinking-in-funcExports. I was going to rewrite this whole function as newComponent to be ClassRemove.js but then realized i could add a third parameter to the original functional declaration to specify add or Not. 
+    if (typeof set === 'string' && set === 'add') elem.addClass(classToSet)
+    if (typeof set === 'string' && set === 'remove') elem.removeClass(classToSet)       
+}
+    // Had a problem where scrolling through observers was creating a count, and when we sent over arguments. we would get blasted with our args being sent as many times as the number of pokemon rendered [chosen by the user input]
+    // Using an Array into which we may push our many-in-amount arguments/parameters, we can throw: i.e. (10 jqELementPokebutton) into the array, and select just one element from the array. 
 
-    console.log('classToSet')
-    console.log(classToSet)
-    // elem.forEach( (el) => {
+        // const elemArray = [] || new Array()
+    // const classArray  = new Array() || [];
+
+   // elem.forEach( (el) => {
     //     elemArray.push(el)
     // })
-
-    // $(classToSet).each( (c) => {
-    //     classArray.push(c)
-    // })
-
-    // console.log(elemArray)
-    // console.log(elemArray[0])
-    // console.log('jq')
-    // console.log($(elemArray)[0])
-
-    elem.css('border', '5px solid hotpink')
-    elem.addClass(classToSet)
-
-    console.log('hey here we go!');
-}
-
