692311efa67be8f321efc52c395184a0bd6aa787
Frank Collins <fwc3rd@gmail.com>
2022-09-15 12:24:28 -0400

    [PokeAPI] and <RandomPokemonBar.js> loop trick.
    APIcall(all||random||specify, 'name||id', pokemontypeForRandomizer, randomAmount)
    our random Amount, when specified to be 3 returns 4 random Pokemon. 
    
            if (typeof randomAmount == 'number' && randomAmount < 5 && randomAmount % 2 !== 0) {            
    This is the conditional logic that is accepting our argument.
    
    👎 
    1) because of modulo % 2 == 0 / confirm-odd-number-logic: If the goal output is 3 random Pokemon and APIcall(3) returns 4... 
    1.1) if I change APIcall(2) it invalidates our modulo logic.
    
    👍 
    we can change: 
    1) APIcall(2) and then: 
        if (typeof randomAmount == 'number' && randomAmount < 5 && randomAmount + 1  % 2 !== 0) {   
    1.1 randomAmount + 1 
    
    1.2: 
        for (i; i < randomAmount -1 ; i++) {        
                    RandomAndReset()
                }
    we can also change randomAmount - 1 since 3 is returning 4 pokemon.
    
    of course, relative to my experience level, but this is one of the more code-like things i've ever done.

--- pokedex/src/components/element/RandomPokemonBar.js
+++ pokedex/src/components/element/RandomPokemonBar.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react'
 import APIcall from '../utility/pokeAPI'
 import ReturnRandomPoke from '../utility/RandomPokemon'
 import { $ } from 'react-jquery-plugin'; 
+import GiveAndGet from '../utility/GiveAndGetData'
+// console.log('GiveAndGet')
+// console.log(GiveAndGet)
+
 
 let i = 0;
 // jquery
@@ -10,8 +14,10 @@ let BarCont = $('.Poke-Bar')
 
 
 
-console.log("hey from randompokemon bar.js")    
 function Bar(props) {
+    // console.log('props')
+    // console.log(props)
+
     for (i; i < props.randomPokemon; i++) {
         // let button = document.createElement('div').addClass('PokeDisplay')
     }
@@ -20,21 +26,51 @@ function Bar(props) {
     
     // doesnt like accepting [state, setState] as props and trying to use setState. warning:
     // react_devtools_backend.js:4026 Warning: Cannot update a component (`BootstrapScreen`) while rendering a different component (`Bar`). To locate the bad setState() call inside `Bar`, follow the stack trace as described in
-    useEffect( () => {
-        ReturnRandomPoke(1).then(async(data) => {
-            ReturnRandomPoke(2).then(async(info) => {              
-                ReturnRandomPoke(3).then(async(poke) => {                  
-                    props.setRandomPokemon([data, info, poke])      // heaven stairway
+    // console.log("atleast were over here")
+    useEffect(  () => {
+        // console.log('useeffect in the randomPokemonBar')
+        const randomizer = async () => {
+            ReturnRandomPoke(1).then(async(data) => {
+                ReturnRandomPoke(2).then(async(info) => {              
+                    ReturnRandomPoke(3).then(async(poke) => {                  
+                        await props.setRandomPokemon([data, info, poke])      // heaven stairway
+                    })
                 })
             })
+        }
+        randomizer()
 
-        })
+        const randomizer2 = () => {
+            console.log("in the randomizer 2 function")
+            APIcall('random', null, null, 3)                    
+        }
+        randomizer2()
+// a little weird. if you enter 3 it returns 4 pokemon.     (4) [{…}, {…}, {…}, {…}] !! ha. changed 3 to 2 and was rejected by condition [% 2 !== 0] not being met. I took out condition and now it (2) creates 3 randomPokemon. 
+        const checkTypes = () => {
+            console.log('check types function')
+            props.randomPokemon.forEach(async(proprandom) => {
+                // console.log('proprandom')
+                // console.log(proprandom)
+                let name = proprandom.name
+                // console.log('name')
+                // console.log(name)
+                await GiveAndGet(name, 'type')
+            })
+        }
+        checkTypes()
+
         // for (i = 0; i < 3; i++) {
         //     ReturnRandomPoke(3).then(async (data) => {            
         //         await props.setRandomPokemon(data)
         //     })
         // }
     }, []) // canceling-out-dependency array.
+
+    let checkAgain = () => {
+        console.log('props.randomPokemon')
+        console.log(props.randomPokemon)
+    }
+    
     
     let pokemap = props.randomPokemon.map( (mapitem, idx) => {
         return ( 
@@ -42,7 +78,7 @@ function Bar(props) {
         className="Poke-Bar-Pokemon" 
         key={idx}
         src={mapitem.img}
-        // src={mapitem.img} must be tired. got enthused thinking you could cut off /${cleanid} and replace it with {mapitem.img/${idx}} not the case.
+        // src={mapitem.img} must be tired. got so excited thinking you could cut off /${cleanid} and replace it with {mapitem.img/${idx}} not the case.
         >  
         </img> 
         )
@@ -50,8 +86,7 @@ function Bar(props) {
     })
 
     const checkState = async () => {
-        console.log('props.randomPokemon')
-        console.log(props.randomPokemon)
+        // checkTypes()
     }
 
     
@@ -61,8 +96,9 @@ function Bar(props) {
     
     return (
         <>
-        <div className="Poke-Bar Row-Center"> </div>
-            <ul className="Row-Center">
+        <div className="Poke-Bar Row-Between"> </div>
+        <button onClick={checkAgain} type="button" className="navBall" id="Ultraball"> </button>
+            <ul className="Row-Between">
             {/* {randomPokeStateMap} */}
             {pokemap}
             </ul>
