847a3147a30f19b8eca16f8b3e659b5bf8438265
Frank Collins <fwc3rd@gmail.com>
2022-09-15 00:13:49 -0400

    [Bar.js] successful image map and render
    setRandomPokemon appended below the pokedex

--- pokedex/src/components/element/RandomPokemonBar.js
+++ pokedex/src/components/element/RandomPokemonBar.js
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import APIcall from '../utility/pokeAPI'
 import ReturnRandomPoke from '../utility/RandomPokemon'
+import { $ } from 'react-jquery-plugin'; 
 
 let i = 0;
+// jquery
+let BarCont = $('.Poke-Bar')
+
+
+
 
 console.log("hey from randompokemon bar.js")    
 function Bar(props) {
-    console.log('props')
-    console.log(props)
+    for (i; i < props.randomPokemon; i++) {
+        // let button = document.createElement('div').addClass('PokeDisplay')
+    }
     // 
     let looplength = props.randomPokemon.length        
     
@@ -29,25 +36,40 @@ function Bar(props) {
         // }
     }, []) // canceling-out-dependency array.
     
+    let pokemap = props.randomPokemon.map( (mapitem, idx) => {
+        return ( 
+        <img 
+        className="Poke-Bar-Pokemon" 
+        key={idx}
+        src={mapitem.img}
+        // src={mapitem.img} must be tired. got enthused thinking you could cut off /${cleanid} and replace it with {mapitem.img/${idx}} not the case.
+        >  
+        </img> 
+        )
+        // return ( <li className="Poke-Bar-Pokemon" key={idx}> {mapitem.name} </li> )
+    })
 
     const checkState = async () => {
         console.log('props.randomPokemon')
         console.log(props.randomPokemon)
     }
+
+    
+
+
+
+    
     return (
         <>
         <div className="Poke-Bar Row-Center"> </div>
             <ul className="Row-Center">
             {/* {randomPokeStateMap} */}
+            {pokemap}
             </ul>
-        <button onClick={checkState} type="button" className="navBall" id="Greatball"> </button>
-        {/* {for (i; i < looplength; i++) {
-            {createDisplay}
-        })} */}
+        {/* <button onClick={checkState} type="button" className="navBall" id="Greatball"> </button> */}
+
         </>
-        // {for (i; i < props.randomPokemon.length)}
-        // possible carousel
+     
     )
 }
 export default Bar
-// export default Bar
