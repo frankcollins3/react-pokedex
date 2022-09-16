4e0f556c3ec14ce3a35bd43ee93c5676363d0834
Frank Collins <fwc3rd@gmail.com>
2022-09-15 23:44:17 -0400

    [ObjectTool]
    <Utility/Evolution.js> 
    1) import ObjectTemplate from './ObjectTool'  2) invoke function ---> ObjectTemplate('charizard').  // 
    
    Utility/ObjectTool.js:
    
    export default async function ObjectTemplate(nameOrId) {                        // nameOrId is ('charizard') our only func parameter of ObjectTemplate() invoke
    
    if (typeof nameOrId === 'string') {                   ||                  } else if (typeof nameOrId === 'number') {
    // this verifies our data as being a pokemon.name (can loop through results and do strict equality check to double verify) 
    
    let specificFetch = await APIcall('specify', nameOrId)     // access our imported API call. 
    
    1) first argument 'SPECIFY' comes from:
     export default async function APIcall(METHOD, poke, type, randomAmount) {   
    
    specify is linked to method and is an argument telling us to run conditional logic to 'specify' pokemon instead of return ALLpokemon
    
    * let singledatacall = await Axios.get(`${singleurl}${poke}`)   from /api/v2/pokemon/'bulbasaur' (i.e.)
    ** const  mypoke = {name: singledata.name, id: singledata.id, image: sprites/pokemon/${singledata.id}.png` }      
    
    (the ALLpokemon res.data.results has [data.name && data.url]
    (the /api/v2/pokemon endpoint has abilities, moves, height, weight, stats, EVERYTHING we need to know.
    
    👍  
    return [singledata, mypoke] LUMPED these 2 objects together.
    👍 👍 
    let pokemonObject = { name: '', id: '', /sprites/pokemon/` } 
    // created an empty pokemon object which was the original intention: to only return this here for sake of having /pokemon/${} malleable image url
    👍 👍 👍 
    let emptyObject = {empty: pokemonObject}
    specificFetch.push(emptyObject)  
    
    // it's neat that this can happen. to just create separate objects and push em through and export/import.
    
    if anyones down here, hello, and excuse me for the longer explanations. I got a front end job teaching bootstrap, css, react.js 7th/8th 1st/2nd year lvl.
    
    especially grateful to do alternate things with objects, and to respecify an object a few ways and know a few more nooks/crannies/tricks of reference.

--- pokedex/src/components/utility/ObjectTool.js
+++ pokedex/src/components/utility/ObjectTool.js
@@ -1,11 +1,44 @@
 import APIcall from './pokeAPI'
 export default async function ObjectTemplate(nameOrId) {
-    if (typeof nameOrId === 'string') {
-        console.log('hey string')
+    if (typeof nameOrId === 'string') {    
+       let specificFetch = await APIcall('specify', nameOrId)         
+        let pokemonObject = {
+            name: '',
+            id: '',
+            url: `https:raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+        }
+        let emptyObject = {empty: pokemonObject}
+        specificFetch.push(emptyObject)             // relative-to-personal-experience-level but this is insane that you can do this. it is defeating the purpose of this being a quick tool to get a quick object to render.
+        // i kind of want to make this a component so that we could have inputs to check how many object keys the user wants to specify. Then they can enter the names of the keys and leave the values blank 
+
+        console.log('specificFetch')
+        console.log(specificFetch)
+        console.log(specificFetch[0])
+        console.log(specificFetch[1])
+        console.log(specificFetch[2])
+        
+        
+
     } else if (typeof nameOrId === 'number') {
         console.log("nice numbers")
-        let dataForObject = await APIcall('specify', nameOrId)   
-        console.log('dataForObject')
-        console.log(dataForObject)
+        let dataForObject = await APIcall('specify', nameOrId)           
     }
 }
+
+
+// (2) [{…}, {…}] this has everything you need! I was also trying to merge 2 arrays or map together. 
+// 0: {abilities: Array(2), base_experience: 267, forms: Array(1), game_indices: Array(20), height: 17, …}
+// 1: {name: 'charizard', id: 6, image: 'https:raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'}
+// length: 2
+// [[Prototype]]: Array(0)
+
+
+// let imageurl = `https:raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${singledata.id}.png`
+
+// let preObjectData = await APIcall('all')
+// let pokeArray = preObjectData.pokemon
+// pokeArray.forEach( (result) => {
+//     if (result.name === nameOrId) {
+    //     }
+    // })
+//  // {name: 'charizard', url: 'https://pokeapi.co/api/v2/pokemon/6/'}                                
