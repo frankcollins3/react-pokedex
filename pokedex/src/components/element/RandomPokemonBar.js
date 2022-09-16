8912a69ec35e06d095bad589ee482966ecec45cc
Frank Collins <fwc3rd@gmail.com>
2022-09-16 12:15:36 -0400

    [PokemonBar] ASYNC issue with maps!
    👎 
     evolvebucket = await (EvolutionChain(randomid))        this code is within the useEffect( () => 
                evolvebucket.map( (map) => {                    this works successfully.
                    // console.log(map)
                })      
    
    evolvebucket.map( (map) => {                    line 60-62 not contained by the useEffect:  THIS DOES NOT WORK RETURNS UNDEFINED!
            console.log('map')
        })
    
    👍 button.onClick(=> let checkAgain =  console.log(evolvebucket)      LINE 54 works 
    
    
    
    PROPOSED PLANS OF APPROACH:
    1)  change declaration of evolvebucket:
    [1.1] first was:---> let evolvebucket;
    [1.2] 2nd(as committed above) ---> let evolvebucket = [];
    [1.3] 3rd(tbd)----> let evolvebucket = ['hey', 'hi', 'wow']
    1) First action will be to declare evolvebucket in a way that our below-useEffect evolvebucket.map() declaration doesn't return an undefined error.
    can do conditional logic within map to check if its the '[hi', 'hey'] map items or the evolvebucket(Evolution) map items.
    ********** similar enough to not need its own section: a proposed solution is also to wrap map in a conditional and setTimeout **********
    * another approach tied to the option above is to trigger this to some kind of event behavior. *
    EVENT:  const [mapInit, setMapInit] = useState() initialize state.
    EVENT: $(document).mousemove( () => if (mapInit == 'false') evolvebucket = await EvolutionChain(id)
    // we can set a conditional to make sure the mousemove doesn't fire 1000s of times while using the app, only once until the condition and state is met and set
    mousemove: an invisible event that doesn't need conscious effort, and will probably thoughtlessly be the first event someone would fire upon opening the app
    
    
    hmm... will $(document).ready() do anything for this? I wonder if its powerful enough to override react. 
    I also believe ComponentDidMount is stronger than useEffect in terms of having the contained-by-DIDMOUNT-functionality be more enacted upon load. 
    That might be completely wrong.

--- pokedex/src/components/element/RandomPokemonBar.js
+++ pokedex/src/components/element/RandomPokemonBar.js
@@ -3,62 +3,59 @@ import APIcall from '../utility/pokeAPI'
 import ReturnRandomPoke from '../utility/RandomPokemon'
 import { $ } from 'react-jquery-plugin'; 
 import GiveAndGet from '../utility/GiveAndGetData'
+import EvolutionChain from '../utility/Evolution'
 // console.log('GiveAndGet')
 // console.log(GiveAndGet)
+let id = [1, 4, 7]
+let randomid = id[Math.floor(Math.random()*id.length)]
+
+// EvolutionChain(randomid)
+
+
+// EvolutionChain('charizard')
 
 
 let i = 0;
 // jquery
 let BarCont = $('.Poke-Bar')
 
+let evolvebucket = [];                 // i like all lowercase variable declarations now because it can give a visual separator from Components or imported utility functions like EvolutionChain
+
 
 
 
 function Bar(props) {
-    // console.log('props')
-    // console.log(props)
-
+        
     for (i; i < props.randomPokemon; i++) {
         // let button = document.createElement('div').addClass('PokeDisplay')
     }
-    // 
+
     let looplength = props.randomPokemon.length        
-    
-    // doesnt like accepting [state, setState] as props and trying to use setState. warning:
-    // react_devtools_backend.js:4026 Warning: Cannot update a component (`BootstrapScreen`) while rendering a different component (`Bar`). To locate the bad setState() call inside `Bar`, follow the stack trace as described in
-    // console.log("atleast were over here")
-    useEffect(  () => {
-    
+    useEffect(() => {
+        (async () => {
+            evolvebucket = await (EvolutionChain(randomid))    
+            evolvebucket.map( (map) => {
+                // console.log('map')
+                // console.log(map)
+            })        
+        })()
 
         const randomizer2 = () => {
-            console.log("in the randomizer 2 function")
             APIcall('random', null, null, 3).then(async(data) => {      // 3 hours. this now works for 1 or 3
-                    console.log(data)
                     await props.setRandomPokemon(data)
             })        
             // a little weird. if you enter 3 it returns 4 pokemon.     (4) [{…}, {…}, {…}, {…}] !! ha. changed 3 to 2 and was rejected by condition [% 2 !== 0] not being met. I took out condition and now it (2) creates 3 randomPokemon. 
         }
         randomizer2()
 
-        const checkTypes = () => {
-            console.log('check types function')
-            console.log('props.randomPokemon')
-            console.log(props.randomPokemon)
-        }
         checkTypes()
+    }, [])  
 
-        // for (i = 0; i < 3; i++) {
-        //     ReturnRandomPoke(3).then(async (data) => {            
-        //         await props.setRandomPokemon(data)
-        //     })
-        // }
-    }, []) // canceling-out-dependency array.
-
-    let checkAgain = () => {
-        console.log('props.randomPokemon')
-        console.log(props.randomPokemon)
-    }
+    let checkAgain = () => { console.log(evolvebucket }
     
+    evolvebucket.map( (map) => {
+        console.log('map')
+    })
     
     let pokemap = props.randomPokemon.map( (mapitem, idx) => {
         return ( 
@@ -66,30 +63,22 @@ function Bar(props) {
         className="Poke-Bar-Pokemon" 
         key={idx}
         src={mapitem.img}
-        // src={mapitem.img} must be tired. got so excited thinking you could cut off /${cleanid} and replace it with {mapitem.img/${idx}} not the case.
         >  
         </img> 
         )
-        // return ( <li className="Poke-Bar-Pokemon" key={idx}> {mapitem.name} </li> )
     })
-    console.log(pokemap.length)
-
-    const checkState = async () => {
-        // checkTypes()
-    }
 
     return (
         <>
         <div className={pokemap.length === 3 ? "Row-Between" : "Row-Center"}> </div>
-        {/* <button onClick={checkAgain} type="button" className="navBall" id="Ultraball"> </button> */}
+        <button onClick={checkAgain} type="button" className="navBall" id="Ultraball"> </button>
             <ul className={pokemap.length === 3 ? "Row-Between" : "Row-Center"}>
             {/* {randomPokeStateMap} */}
-            {pokemap}
+             {evolvebucket} || {pokemap}
+            {/* {evolvebucket} {pokemap} */}
             </ul>
         {/* <button onClick={checkState} type="button" className="navBall" id="Greatball"> </button> */}
-
-        </>
-     
+        </>     
     )
 }
 export default Bar
