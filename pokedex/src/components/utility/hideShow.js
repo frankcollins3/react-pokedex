9e0c4c6f49431744b37667897cbb6f9444ff1797
Frank Collins <fwc3rd@gmail.com>
2022-09-22 22:50:34 -0400

    [HideShow]
    updating HideShow tool to be able to take in an array as an argument so we can apply:
    conditional inline-Styling that renders: 'none' display or not based on hideState().includes()

--- pokedex/src/components/utility/hideShow.js
+++ pokedex/src/components/utility/hideShow.js
@@ -1,8 +1,31 @@
 import { $ } from 'react-jquery-plugin'; 
 export default async function toggleHideShow (elem, action) {
-    if (typeof elem === 'object' && action === 'hide') {
-        $(elem).hide()
-    } else if (action === 'show') $(elem).show()
+        console.log('elem')
+        console.log(elem)
 
-    console.log(typeof elem) // returns object        const screenTestObj = useRef(BAR)        <div id="Bar-Wrap" ref={screenTestObj} 
+        console.log('action')
+        console.log(action)
+
+    if (action === 'hide') {
+        console.log('action')
+
+        elem.hide()
+    // if (typeof elem === 'object' && action === 'hide') {
+    } else if (typeof elem === 'object' && action === 'show') {
+        elem.show()
+    }
+    
 }
+
+// I want to create a: [hideState, setHideState] hook so we can use toggleHideShow([]) on an array. Surprised to see: typeof food/array return object. 
+// we can use the distinguishing factor separating arrays and objects (arrays return .length true value an object returns a falsy value)
+// i also thought it'd be possible to use a for loop, and if error (an array should return an error to try performing for loop) IF forLoop/Array Error: array.loop(elem)=>toggleHide(elem)
+console.log("check array")
+let food = ['hotdogs', 'cookies', 'blizzards']
+const shakes = { blizzard: 'peanut butter', shake: 'vanilla'}
+// console.log(typeof food) interesting enough this returns an object but returns a .length of 3
+// console.log(typeof shakes) this returns object as well but returns undefined for length. that is how we can check for an array.
+console.log(food.length)
+console.log(shakes.length)
+// check array
+// Bootstrap.js:21 object
