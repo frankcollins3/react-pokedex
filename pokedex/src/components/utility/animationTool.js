42cf75d5790eaac3fe701eb30e598ea3460794f4
Frank Collins <fwc3rd@gmail.com>
2022-09-23 20:44:38 -0400

    Update animationTool.js
    1) animation.js export pure function for utility:
    export default async function animate (target, n, propertyBucket, valueBucket, duration, complete) { 
    
    2) bootstrap.js: our file where we handle our animate() invocation. animate() is also an input-accepting pure function used as an exportable utility
    
    👍  animate(hiddenTag, '2', ['opacity', 'border'], ['0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0'], ['500', '1000', '3000'], myCSS)  
           myCSS set as the 'complete' parameter of our pure function that handles selectors and animations designated upon them.
    
    👎 
            animate(hiddenTag, '2', ['opacity', 'border'], ['0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0'], ['500', '1000', '3000'], myCSS())  
           myCSS invoked when sending over the parameter.

--- pokedex/src/components/utility/animationTool.js
+++ pokedex/src/components/utility/animationTool.js
@@ -1,7 +1,13 @@
-export default async function animate (selector, n, propertyBucket, valueBucket, duration, complete) { ''
+export default async function animate (target, n, propertyBucket, valueBucket, duration, complete) { 
     console.groupCollapsed() // *** console.group() || console.groupCollapsed()
     console.log('propertyBucket')
     console.log(propertyBucket)
+    console.log('duration')
+    console.log(duration)
+    console.log('valueBucket')
+    console.log(valueBucket)
+    console.log('complete')
+    console.log(complete)
     console.groupEnd()
     
     let opacityKey = propertyBucket[0]
@@ -14,29 +20,30 @@ export default async function animate (selector, n, propertyBucket, valueBucket,
     let valueKey5 = valueBucket[4]
     let valueKey6 = valueBucket[5]
     let valueKey7 = valueBucket[6]
-    let valueKey8 = valueBucket[7]
+    let valueKey8 = valueBucket[7] 
     let valueKey9 = valueBucket[8]
     let valueKey10 = valueBucket[9]
-
+ 
     let durationKey1 = duration[0]
     let durationKey2 = duration[1]
 
-    // poor man's console.group()
-    console.log([valueKey1, valueKey2, valueKey3, valueKey4, valueKey5, valueKey6, valueKey7, valueKey8, valueKey9])     // ['0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0', undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
+    console.log([valueKey1, valueKey2, valueKey3, valueKey4, valueKey5, valueKey6, valueKey7, valueKey8, valueKey9])     // poor man's console.group()
+    // ['0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0', undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
 
     // in an ideal app you would have some inputs and a display screen from which: [user||human] can select options. 
-    // You wouldn't want to use this too much, but what we are selecting doesn't depend heavily on state and doesn't mesh 
+    
     // if (typeof n === 'string' || typeof n === 'integer') {
         if (parseInt(n) === 2) {    // was going to provide 2, 4, or 8.
             console.log('hey were in the 2 part')
-        selector
-        .css("border", '5px solid brown')
-        selector        // was worried
-        .animate({ [propertyBucket]:  '0.1'}, 4000) // {propertyBucket[0]} || `${opacityKey}` neither of these options work. 
-
+        complete(target, 'border', '5px solid green')
+        // target
+        // .css("border", '5px solid brown')
+        // .animate({ [opacityKey || propertyBucket[0]]:  '0.4'}, ) // {propertyBucket[0]} || `${opacityKey}` neither of these options work. 
+        // .animate({ [opacityKey] : '0.1'}, 0.1 )
+        
+        }
         // https://stackoverflow.com/questions/11508463/javascript-set-object-key-by-variable 
         // The solution in ES6 is to put the variable in square brackets in order to evaluate it. var key = "happyCount"; myArray.push({ [key]: someValueArray });
 
-    // return $(selector).css(`${property}`, `${value}`)    
-        }
+    // return $(target).css(`${property}`, `${value}`)    
     }
