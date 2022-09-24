8b7377147cc3387b8f80842cb34b394e3c76453d
Frank Collins <fwc3rd@gmail.com>
2022-09-24 13:02:31 -0400

    [TextTool]
    1) import TextTool function which is a pure-function: 1) accept arguments, 2) run expressions, return outputs/results/response
    2) abide by function parameter dataType validations and set a: 
    * selector to mutate ** a method: either .html() .text() by 'html'/'text'/ or any non 'html' string characters
    
    return the rest of the characters.

--- pokedex/src/components/utility/TextTool.js
+++ pokedex/src/components/utility/TextTool.js
@@ -1,12 +1,16 @@
 import { $ } from 'react-jquery-plugin' 
 export default function TextTool (selector, method, text) {        
     if (typeof method === 'string' && typeof text === 'string' && typeof selector === 'object') {     
-        let newText;          // [let !const] never knew this! you cant just [const variable;] which makes sense because you couldn't change it from there being a const. obvious but interesting to see.
+        let newText;         
+// [let !const] never knew you cant just [const variable;] which makes sense because you couldn't change it from there w/ a const. obvious but interesting to see.
 
         let firstChar = text.charAt(0)
         // let firstChar = text.charAt[0]
         console.log('firstChar')
         console.log(firstChar)
+        let restOfTextChars = text.slice(1) // first attempt: text.slice(1, text.length)
+        console.log('restOfTextChars')
+        console.log(restOfTextChars)
 
         method === 'html' ? $(selector).html(text) : $(selector).text(text)
     }
