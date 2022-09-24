fad76d060bdbab54466811c6c4fc50b6f3eb73e8
Frank Collins <fwc3rd@gmail.com>
2022-09-24 13:31:41 -0400

    [TextTool] surprise lesson in addition assignment
    Using addition assignment coupled with string concatentation:
    
    let newText;
    let newText = ''
    
    newText += newString 
    
    let newText; -----> [undefinedBulbasaur: #1] our variable that held a value of undefined as being concatenated without bias/opinionated-inference
    
    let newText = ' ' -----> specify an empty string and concatenate with the new string.
    
    *** personal ***
    but I've seen this type of re-specifying data before in APPs. I didn't understand it and didn't pay it much mind.
    Though this looks redundant, I believe this practice follows the mind of:
    everything is specified elsewhere, and used/referenced elsewhere.
    
    example:
    
    let image = pokemon.data.results.sprites.front_default
    v.s.
    let pokemon = pokemon.data.results
    let image = pokemon.sprites
    let front = image.front_default
    
    img.src = front 
    
    👍 
    theres nothing wrong with using newText but it feels more code-like to append it to a new value. It also feels redundant and unnecessary.

--- pokedex/src/components/utility/TextTool.js
+++ pokedex/src/components/utility/TextTool.js
@@ -1,17 +1,18 @@
 import { $ } from 'react-jquery-plugin' 
 export default function TextTool (selector, method, text) {        
-    if (typeof method === 'string' && typeof text === 'string' && typeof selector === 'object') {     
-        let newText;         
-// [let !const] never knew you cant just [const variable;] which makes sense because you couldn't change it from there w/ a const. obvious but interesting to see.
+    if (typeof method === 'string' && typeof text === 'string' && typeof selector === 'object') {         
+        let firstChar = text.charAt(0).toUpperCase()                 
+        let restOfTextChars = text.slice(1) // first attempt: text.slice(1, text.length)    
+        // let regex = restOfTextChars.replace(/[/\/s:.#]/g, '') (/[ [/s][/\/:.#] ]/g, '') // this isn't removing the white space like i'd like and i can spend time fixing it but this isn't needed.        
+        let newString = `${firstChar}${restOfTextChars}`
 
-        let firstChar = text.charAt(0)
-        // let firstChar = text.charAt[0]
-        console.log('firstChar')
-        console.log(firstChar)
-        let restOfTextChars = text.slice(1) // first attempt: text.slice(1, text.length)
-        console.log('restOfTextChars')
-        console.log(restOfTextChars)
+        // let newText;          // [let !const] never knew this! you cant just [const variable;] which makes sense because you couldn't change it from there being a const. obvious but interesting to see.
+        let newText = '' // [undefinedBulbasaur: #1] revealing xp-lvl by admitting to be out of loop on [+= / addition assignment] is returned if newText isn't initialized as an empty string. the undefined value returns undefined. also obvious. also neat.
+        newText += newString // i used to see this done all the time and it is not needed for what we're doing now. Quick enough to give it a quick test go.
+        console.log('newText')
+        console.log(newText)
 
-        method === 'html' ? $(selector).html(text) : $(selector).text(text)
+
+        if (text.length < 15) method === 'html' ? $(selector).html(text) : $(selector).text(text)
     }
 }
