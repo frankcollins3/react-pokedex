4b78e52c3e3dee5a78cc961cfd997e5596e08d7a
Frank Collins <fwc3rd@gmail.com>
2022-09-24 17:11:28 -0400

    [AnimationTool] function() not working
    complete function initiates at the last moment and doesn't stick. 
    This utility function has been modified to put to use the final argument in the export function parameters. (function() { complete for jq Animation }
    
    👍 in this case the exported myCSS() tool allows us to manipulate the objects. 
    
    👎 it works but it's not sticking / persisting through for some reason.
    
    this is so that any pokemon/data that is saved by clicking the pokeball, Those pokemon that have been saved are {opacity: 0.1} for purpose of indication

--- pokedex/src/components/utility/animationTool.js
+++ pokedex/src/components/utility/animationTool.js
@@ -1,19 +1,15 @@
 export default async function animate (target, n, propertyBucket, valueBucket, duration, complete) { 
-    console.groupCollapsed() // *** console.group() || console.groupCollapsed()
-    console.log('propertyBucket')
-    console.log(propertyBucket)
-    console.log('duration')
-    console.log(duration)
-    console.log('valueBucket')
-    console.log(valueBucket)
-    console.log('complete')
-    console.log(complete)
-    console.groupEnd()
+    // console.groupCollapsed() // *** console.group() || console.groupCollapsed()
+    // console.log(propertyBucket)
+    // console.log(duration)
+    // console.log(valueBucket)
+    // console.log(complete)
+    // console.groupEnd()
     
     let opacityKey = propertyBucket[0]
     let borderKey = propertyBucket[1]
 
-    let valueKey1 = valueBucket[0]
+    let valueKey1 = [valueBucket[0]]
     let valueKey2 = valueBucket[1]
     let valueKey3 = valueBucket[2]
     let valueKey4 = valueBucket[3]
@@ -23,27 +19,19 @@ export default async function animate (target, n, propertyBucket, valueBucket, d
     let valueKey8 = valueBucket[7] 
     let valueKey9 = valueBucket[8]
     let valueKey10 = valueBucket[9]
- 
-    let durationKey1 = duration[0]
-    let durationKey2 = duration[1]
-
-    console.log([valueKey1, valueKey2, valueKey3, valueKey4, valueKey5, valueKey6, valueKey7, valueKey8, valueKey9])     // poor man's console.group()
-    // ['0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0', undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
-
-    // in an ideal app you would have some inputs and a display screen from which: [user||human] can select options. 
-    
-    // if (typeof n === 'string' || typeof n === 'integer') {
-        if (parseInt(n) === 2) {    // was going to provide 2, 4, or 8.
-            console.log('hey were in the 2 part')
-        complete(target, 'border', '5px solid green')
-        // target
-        // .css("border", '5px solid brown')
-        // .animate({ [opacityKey || propertyBucket[0]]:  '0.4'}, ) // {propertyBucket[0]} || `${opacityKey}` neither of these options work. 
-        // .animate({ [opacityKey] : '0.1'}, 0.1 )
         
-        }
-        // https://stackoverflow.com/questions/11508463/javascript-set-object-key-by-variable 
-        // The solution in ES6 is to put the variable in square brackets in order to evaluate it. var key = "happyCount"; myArray.push({ [key]: someValueArray });
-
-    // return $(target).css(`${property}`, `${value}`)    
+    let durationKey0 = duration[0]      // duration bucket setup with [0] = '500' so that [1] = '1000' [2] = '2000' etc.
+    let durationKey1 = duration[1]
+    let durationKey2 = duration[2]
+    let durationKey3 = duration[3]
+    let durationKey10 = duration[4]
+   
+    if (parseInt(n) === 2) {    // was going to provide 2, 4, or 8.
+        target      // i changed the name of this parameter from 'selector' to 'target'. i can double check but am moving on. our myCSS() uses 'selector' as its first parameter.
+        .animate({ [opacityKey || propertyBucket[0]]:  valueBucket[9]}, durationKey3, function () {
+            complete(target, 'opacity', '0.1')
+        }) // {propertyBucket[0]} || `${opacityKey}` neither of these options work.             
     }
+}
+    // if (typeof n === 'string' || typeof n === 'integer') {
+    
