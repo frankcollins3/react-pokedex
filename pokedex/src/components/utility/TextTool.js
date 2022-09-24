5e73e64a4a16fa814607fa1c56b4294369651f99
Frank Collins <fwc3rd@gmail.com>
2022-09-24 12:34:29 -0400

    [utility/TextTool]
    typeof:
    method === 'string' : 
    1) verify the string data and proceed to conditional logic. 
    2) if method === 'html' ()=> selector.html()         if method === 'text' || method !== 'html'()=> selector.text()
    
    👍 
    3) the 3rd expression in this 3-condition piece of logic:
    verifies the selector is an object 
    and that we won't be invoking the jq CSS method on a string or other type of data besides something that can be mutated by its properties.

--- pokedex/src/components/utility/TextTool.js
+++ pokedex/src/components/utility/TextTool.js
@@ -1,14 +1,6 @@
 import { $ } from 'react-jquery-plugin' 
-export default function TextTool (selector, method, text) {
-    console.log('selector')
-    console.log(selector)
-
-    if (typeof method === 'string') {
-        console.log('method')
-        console.log(method)
-        console.log('text')
-        console.log(text)
+export default function TextTool (selector, method, text) {        
+    if (typeof method === 'string' && typeof text === 'string' && typeof selector === 'object') {        
         method === 'html' ? $(selector).html(text) : $(selector).text(text)
     }
-
 }
