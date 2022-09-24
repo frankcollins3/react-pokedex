1d4d6ecb46d600e912627383ec22b8cd7647ae56
Frank Collins <fwc3rd@gmail.com>
2022-09-24 14:31:53 -0400

    [Text] some data/poke !pass length validation
    if (text.length. < 15) 
    
    lickitung pops up as " " // empty string (which is what the <ptag> ' '  </p> is set to. 
    When class gets removed and the data isn't returned because it invalidates the condition based on [function-Argument/text.length]
    
    This exportable-utility-function and behavior-based-invocation now working as intended.
    
    ** can see psychology/habit **
    getting a little overly familiar with how peculiar intersection Observer was.
    
    👎  I can see how easy it would be to be frenzied thinking its on the <Bootstrap side> (which up until now it has been)
    
    👍 When the simple fix of [ if (text.length < 15) || if (text.length < 18)] is all you need: look closely without bias.

--- pokedex/src/components/utility/TextTool.js
+++ pokedex/src/components/utility/TextTool.js
@@ -1,5 +1,12 @@
 import { $ } from 'react-jquery-plugin' 
 export default function TextTool (selector, method, text) {        
+
+    console.groupCollapsed()
+    console.log(`selector ${selector}`)
+    console.log(`method ${method}`)
+    console.log(`text ${text}`)
+    console.groupEnd()
+    
     if (typeof method === 'string' && typeof text === 'string' && typeof selector === 'object') {         
         let firstChar = text.charAt(0).toUpperCase()                 
         let restOfTextChars = text.slice(1) // first attempt: text.slice(1, text.length)    
@@ -9,10 +16,8 @@ export default function TextTool (selector, method, text) {
         // let newText;          // [let !const] never knew this! you cant just [const variable;] which makes sense because you couldn't change it from there being a const. obvious but interesting to see.
         let newText = '' // [undefinedBulbasaur: #1] revealing xp-lvl by admitting to be out of loop on [+= / addition assignment] is returned if newText isn't initialized as an empty string. the undefined value returns undefined. also obvious. also neat.
         newText += newString // i used to see this done all the time and it is not needed for what we're doing now. Quick enough to give it a quick test go.
-        console.log('newText')
-        console.log(newText)
+        
 
-
-        if (text.length < 15) method === 'html' ? $(selector).html(text) : $(selector).text(text)
+        if (text.length < 18) method === 'html' ? $(selector).html(newText) : $(selector).text(newText)
     }
 }
