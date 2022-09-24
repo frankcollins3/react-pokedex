dd0e353f446e7c6e7610f868a8a8f248c3071ff0
Frank Collins <fwc3rd@gmail.com>
2022-09-24 16:25:00 -0400

    MainScreen
    might send props down for conditional logic so that the navbar appears based on the state being changed.

--- pokedex/src/components/webpage/MainScreen.js
+++ pokedex/src/components/webpage/MainScreen.js
@@ -1,11 +1,22 @@
-
+import React, { useState, useEffect} from 'react'
+import APIcall from '../utility/pokeAPI'
+import Screen from '../element/Screen'
+import BootstrapScreen from '../element/Bootstrap'
+import Nav from '../element/NavBar'
 
 function Main () {
+    const [ghost, setGhost] = useState('false')
+    const [pokedexClick, setPokedexClick] = useState('false')
+    // I didn't think of the navbar also needing access to this state.
+
     return (
-        <>
-        <p> visual component test </p>
-        </>
+        // <>
+        <div className="Column-Center">                        
+        <Nav ghost={ghost} setGhost={setGhost}/>
+        <BootstrapScreen pokedexClick={pokedexClick} setPokedexClick={setPokedexClick}/>
+        </div>
+    
+        // </>
     )
 }
-
 export default Main
