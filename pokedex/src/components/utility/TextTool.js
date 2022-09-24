4a3507874b35d8b32af9a2a397e6b83fb9081b8d
Frank Collins <fwc3rd@gmail.com>
2022-09-24 12:56:39 -0400

    [TextTool] return first letter of 3rd argument of pure-function
    This character needs to be 'dataTypes.string' for this to work.

--- pokedex/src/components/utility/TextTool.js
+++ pokedex/src/components/utility/TextTool.js
@@ -1,6 +1,13 @@
 import { $ } from 'react-jquery-plugin' 
 export default function TextTool (selector, method, text) {        
-    if (typeof method === 'string' && typeof text === 'string' && typeof selector === 'object') {        
+    if (typeof method === 'string' && typeof text === 'string' && typeof selector === 'object') {     
+        let newText;          // [let !const] never knew this! you cant just [const variable;] which makes sense because you couldn't change it from there being a const. obvious but interesting to see.
+
+        let firstChar = text.charAt(0)
+        // let firstChar = text.charAt[0]
+        console.log('firstChar')
+        console.log(firstChar)
+
         method === 'html' ? $(selector).html(text) : $(selector).text(text)
     }
 }
