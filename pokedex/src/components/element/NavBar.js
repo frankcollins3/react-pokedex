2b744397407dc9af9c1853acb8c1b05d573b0f42
Frank Collins <fwc3rd@gmail.com>
2022-09-25 18:28:18 -0400

    [Nav] util-func CleanUrl() 6:28
    uses ES6 constructor class to return an object that invokes a this.method()  that accepts the url and returns a parsed with .replace() regex escaping a-z characters
    👍 leaving us with the ID returned from the pokeapi.co [/api/v2/type] endpoint.

--- pokedex/src/components/element/NavBar.js
+++ pokedex/src/components/element/NavBar.js
@@ -1,32 +1,38 @@
 import { useEffect, useRef, useState } from 'react'
 import { $ } from 'react-jquery-plugin'
 import TypeLooper from '../utility/TypeTool'
+import GiveAndGet from '../utility/GiveAndGetData'
+import CleanUrl from '../utility/CleanUrlTool'
+
 
 let i = 0
 
 
 function Nav () {
-    console.log("navbar ready jq style")
     // $(document).on('mousemove') 
 
 
     let ghost = useRef()
 
     useEffect( () => {
-        const evaluateGhost = async () => {
-            
+        const evaluateGhost = async () => {            
             let ghostdata = await TypeLooper('ghost')
-
             let len = ghostdata.len
-
             console.groupCollapsed()
             console.log(ghostdata)
             const { data, pokemon, raw } = ghostdata
           
+            
             const moves = data.moves
             const method = raw.config.method
             const randompokemon = pokemon[Math.floor(Math.random()*pokemon.length)]
-            console.log(randompokemon)
+            let url = randompokemon.pokemon.url            
+            let ghostid = await CleanUrl(`${url}`)
+            console.log('ghostid')
+            console.log(ghostid)
+
+            
+
             
           
 
