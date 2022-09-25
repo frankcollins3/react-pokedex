13c9d0675d14c76c0b8fbc9d3d3fe291b6b7dfa3
Frank Collins <fwc3rd@gmail.com>
2022-09-25 18:03:10 -0400

    [UrlTool] try to use ES6 class to handle regex return clean ID
    this would be an exportable utility pure function that would take in arguments and return a value.
    i'm trying to do so through an ES6 class

--- pokedex/src/components/utility/CleanUrlTool.js
+++ pokedex/src/components/utility/CleanUrlTool.js
@@ -1,7 +1,35 @@
 export default async function CleanUrl (url) {
     if (typeof url === 'string' && url.includes('pokeapi')) {
-        console.log('url')
-        console.log(url)
-    }
-    
-}
+
+        class Cleaner {
+            constructor(url) {
+              this.url = url
+            }
+            // Getter
+            get cleanup() {
+              return this.regex();
+            }
+            // Method
+            regex() {
+                return url.slice(url.length-5).replace(/[[/\/a-z]]/g, '')
+            }
+          }              
+          const newid = new Cleaner(url)
+          console.log('newid')
+          console.log(newid)
+
+        //   return url.slice(url.length-5).replace(/[[/\/a-z]]/g, '')
+        //   Cleaner {url: 'https://pokeapi.co/api/v2/pokemon/864/'}
+// url
+// : 
+// "https://pokeapi.co/api/v2/pokemon/864/"
+// cleanup
+// : 
+// (...)
+// [[Prototype]]
+// : 
+// Object
+          
+    }   // typeof conditional end
+}   // async funct CleanUrl() { ____ END____}
+
