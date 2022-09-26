16655df1386d0f9f6568a4199514952dc6127a75
Frank Collins <fwc3rd@gmail.com>
2022-09-26 00:05:50 -0400

    [FuncTimer] pure-function (seconds, cb) for react-timer-hook

---
+++ pokedex/src/components/utility/PureFuncTimer.js
@@ -0,0 +1,7 @@
+import { useTimer } from 'react-timer-hook';
+export default async function FuncTimer (seconds, useTimerFunc) {
+    console.log("hello from timer")
+    let now = new Date()
+    console.log('now')
+    console.log(now)
+}
