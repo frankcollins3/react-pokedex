d206589371c33f5ef36f4bd3e2e6c431b2c002d0
Frank Collins <fwc3rd@gmail.com>
2022-09-26 00:40:44 -0400

    [FuncTimer] allow 3rd parameter:
    the 3rd 'string data' parameter evaluating for:
    'timeout' || 'interval'
    
    this timer will serve as either a setTimeout based or setInterval based timer.
    12:40

--- pokedex/src/components/utility/PureFuncTimer.js
+++ pokedex/src/components/utility/PureFuncTimer.js
@@ -1,9 +1,18 @@
-export default async function FuncTimer (countdown, countdowncallback) {
-    let duration = countdown * 1000 // 30000
+export default async function FuncTimer (countdown, countdowncallback, timertype) {
+    let duration = countdown * 1000
     console.log('duration')
     console.log(duration)
-    setTimeout( () => {
-        // console.log("hey thats easy")
-        countdowncallback()
-    }, duration)
+
+    if (typeof timertype === 'string' && timertype === 'timeout') {
+        setTimeout( () => {
+            console.log('pure function timer')
+            countdowncallback()
+            return 'hey you'
+            // return countdowncallback()
+        }, duration)
+    }
+    else if (typeof timertype === 'string' && timertype === 'interval') {
+        console.log("weve got an interval")
+    }
+
 }
