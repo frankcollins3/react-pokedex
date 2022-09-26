c9f47c7462fc05032921c4bc2a65bc4bbf03e346
Frank Collins <fwc3rd@gmail.com>
2022-09-25 22:02:32 -0400

    [NavBar] condition change to factor .len < 4

--- pokedex/src/components/element/NavBar.js
+++ pokedex/src/components/element/NavBar.js
@@ -13,8 +13,15 @@ import RandomPosition from '../utility/RandomPosition'
 let i = 0
 let pageheight = $(document).height().toFixed()
 let pagewidth = $(document).width().toFixed()
-console.log('pageheight')
-console.log(pageheight)
+
+let posx = (Math.random() * ($(document).width())).toFixed();     // mvp if you wanted to .width()-divsize)) to factor in size of element into screen size
+let posy = (Math.random() * ($(document).height())).toFixed();
+let posx2 = Math.floor(Math.random() * ($(document).width())).toFixed();
+let posy2 = Math.floor(Math.random() * ($(document).height())).toFixed();
+let posx3 = Math.floor(Math.random() * ($(document).width())).toFixed();
+let posy3 = Math.floor(Math.random() * ($(document).height())).toFixed();
+// console.log(pageheight)
+let ghostbtn = $('.ghostbtn')
 
 // I was going to make this an exportable tool but wanted to make sure it was grabbing the size of this page for reference. 
 const randomlocation = () => {
@@ -24,50 +31,55 @@ const randomlocation = () => {
 function Nav () {
     // $(document).on('mousemove') 
 
-    const [ghostBallPoke, setGhostBallPoke] = useState('')
+    const [ghostBallPoke, setGhostBallPoke] = useState([])
 
     let ghost = useRef()
     
     useEffect( () => {
-        const evaluateGhost = async () => {       
-
-            let ghostdata = await TypeLooper('ghost')
-            let len = ghostdata.len
-            console.groupCollapsed()
-            console.log(ghostdata)
-            const { data, pokemon, raw } = ghostdata
-          
-            
-            const moves = data.moves
-            const method = raw.config.method
-            const randompokemon = pokemon[Math.floor(Math.random()*pokemon.length-150)]
-            let url = randompokemon.pokemon.url            
-            let ghostid = await CleanUrl(`${url}`)
-
-            
-            // const checkForGhost = () => {
-                // if (ghostid < 100) {
-
-                    let ghostobjects = await APIcall('specify', ghostid)
-                    // console.log('ghostobjects')
-                    // console.log(ghostobjects)
-                    let name = ghostobjects[1].name                
-                    let ghostImage = await ImageTool(name, 'front')
-                    // console.log('ghostImage')
-                    // console.log(ghostImage)
-                    setGhostBallPoke(ghostImage)
-                    // myCSS(ghostImage, )
-                    RandomPosition(ghostImage)
-                    // return ghostimage
-                    // NavBar.js:43 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/478.png
-
-                // }
-            // }
-        }        
-        evaluateGhost()
+      
     })
 
-    $(document).on('mousemove', () => {
+    $(document).on('mousemove', async () => {
+        // console.log('document mousemove')
+        $('.ghostbtn').css('border', '5px solid hotpink')
+
+            let ghostdata = await TypeLooper('ghost')
+            const { pokemon } = ghostdata 
+            const randompokemon = pokemon[Math.floor(Math.random()*pokemon.length)]
+            let  url = randompokemon.pokemon.url
+            let ghostid = await CleanUrl(`${url}`)
+            let char4 = ghostid.charAt(3)
+            
+
+            if (ghostid < 151 && ghostid.length < 4) {
+                console.log('ghostid')
+                console.log(ghostid)
+                let ghostimg = await ImageTool(ghostid, 'front')
+                console.log(ghostimg)
+                setGhostBallPoke(ghostimg)
+            } else {
+                
+                // setGhostBallPoke('')
+            }
+        // const evaluateGhost = async () => {       
+
+            // let ghostdata = await TypeLooper('ghost')
+            // let len = ghostdata.len
+            
+            // const { data, pokemon, raw } = ghostdata
+            
+            
+            // const moves = data.moves
+            // const method = raw.config.method
+            // const randompokemon = pokemon[Math.floor(Math.random()*pokemon.length)]
+            // let url = randompokemon.pokemon.url            
+            // let ghostid = await CleanUrl(`${url}`)
+            
+            
+
+           
+            
+            
 
     })
     return (
@@ -75,7 +87,10 @@ function Nav () {
             <button className="navBall Half-Size" id="Pokeball"></button>
             <button className="navBall Half-Size" id="Greatball"></button>
             <button className="navBall Half-Size" id="Ultraball"></button>
-            <img className="navBall Half-Size" id="Ghost" src={ghostBallPoke} />
+            <img 
+            className="navBall Half-Size ghostbtn" 
+            id="Ghost" 
+            src={ghostBallPoke || 'img/energy/energyPsychic.jpg'} />
             {/* <button ref={ghost} className="navBall Half-Size" id="Ghost"></button> */}
         </div>
     )
