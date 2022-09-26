3e147f3b26d2a79691d08033c1b32d1302b177fe
Frank Collins <fwc3rd@gmail.com>
2022-09-25 20:42:13 -0400

    [Navbar] changed length of randomizer
    if data id > 1000 it returned an error because of the way it was being parsed. 
    having an id thats very high still allows for a long wait between successful original game ghost-type pokemon retrievals for its effect

--- pokedex/src/components/element/NavBar.js
+++ pokedex/src/components/element/NavBar.js
@@ -5,11 +5,21 @@ import GiveAndGet from '../utility/GiveAndGetData'
 import CleanUrl from '../utility/CleanUrlTool'
 import APIcall from '../utility/pokeAPI'
 import ImageTool from '../utility/ImageTool'
-import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript'
+import myCSS from '../utility/ClassAction'
+import RandomPosition from '../utility/RandomPosition'
+// import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript'
 
 
 let i = 0
+let pageheight = $(document).height().toFixed()
+let pagewidth = $(document).width().toFixed()
+console.log('pageheight')
+console.log(pageheight)
 
+// I was going to make this an exportable tool but wanted to make sure it was grabbing the size of this page for reference. 
+const randomlocation = () => {
+
+}
 
 function Nav () {
     // $(document).on('mousemove') 
@@ -19,7 +29,8 @@ function Nav () {
     let ghost = useRef()
     
     useEffect( () => {
-        const evaluateGhost = async () => {            
+        const evaluateGhost = async () => {       
+
             let ghostdata = await TypeLooper('ghost')
             let len = ghostdata.len
             console.groupCollapsed()
@@ -29,33 +40,29 @@ function Nav () {
             
             const moves = data.moves
             const method = raw.config.method
-            const randompokemon = pokemon[Math.floor(Math.random()*pokemon.length)]
+            const randompokemon = pokemon[Math.floor(Math.random()*pokemon.length-150)]
             let url = randompokemon.pokemon.url            
             let ghostid = await CleanUrl(`${url}`)
+
             
             // const checkForGhost = () => {
                 // if (ghostid < 100) {
 
                     let ghostobjects = await APIcall('specify', ghostid)
-                    console.log('ghostobjects')
-                    console.log(ghostobjects)
+                    // console.log('ghostobjects')
+                    // console.log(ghostobjects)
                     let name = ghostobjects[1].name                
                     let ghostImage = await ImageTool(name, 'front')
-                    console.log('ghostImage')
-                    console.log(ghostImage)
+                    // console.log('ghostImage')
+                    // console.log(ghostImage)
                     setGhostBallPoke(ghostImage)
+                    // myCSS(ghostImage, )
+                    RandomPosition(ghostImage)
                     // return ghostimage
                     // NavBar.js:43 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/478.png
-                    
+
                 // }
             // }
-            
-            
-
-            
-          
-
-            console.groupEnd()
         }        
         evaluateGhost()
     })
