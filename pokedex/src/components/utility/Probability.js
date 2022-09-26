2aa0f9eab379b5af71a584889f19fddad30f4936
Frank Collins <fwc3rd@gmail.com>
2022-09-26 16:17:24 -0400

    [Probability]
    pure function accepts 1 [typeof 'number'] argument and verifies that argument data as such.
    create a for loop that is limited in incrementation by the specified number.
    
    in this exampe:
    if chance == 10 {
    let i = 0; i < 10; i++ {
    we push the index in the loop into an array.
    
    access the ReturnRandom() exported function and return a random value.
    If number is: 
    1 out of 'chance' ----> i.e.  if 1/10 return true
    
    1 )if chance = 100 there will be 100 loop[i] to push into the array
    2) and a 1/100 chance to grab literally the value 1 from this bucket of [1-100] index references that act as our probability 
    
    3) again this probability is replacing the very [more async-intensive + more importantly very low probability] of accessing PokeAPI for an original ghost

--- pokedex/src/components/utility/Probability.js
+++ pokedex/src/components/utility/Probability.js
@@ -1,4 +1,33 @@
+import ReturnRandom from './ReturnRandom'
 export default async function ProbAbility (chance) {
-    console.log('chance')
-    console.log(chance)
+    let i = 0;
+    let applebobberbucket = []
+    if (typeof chance === 'number') {   
+        // primitives string, number, boolean from typescript
+        const loopAndPush = () => {
+            for (i; i < chance; i++) {
+                applebobberbucket.push([i]) // editing during committing: applebobberbucket.push([i + 1])
+            }
+        }
+        loopAndPush()
+        // const checkBucket = () => {
+        //     console.groupCollapsed()
+        //     console.log('this is the applebobber bucket')
+        //     console.log('applebobberbucket')
+        //     console.log(applebobberbucket)
+        //     console.groupEnd()
+        // }
+
+        const evaluate = async () => {
+            let chancevalue = ReturnRandom(applebobberbucket)
+            console.log('chancevalue')
+            console.log(chancevalue)
+        }
+        const multifunc = async () => {
+            await loopAndPush()
+
+            // await checkBucket()
+        }
+        multifunc()
+    }
 }
