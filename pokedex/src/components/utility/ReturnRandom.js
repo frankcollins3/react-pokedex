ce6d64922c8eac484db6e7ae8ce825ef1cfa2111
Frank Collins <fwc3rd@gmail.com>
2022-09-26 12:26:21 -0400

    [ReturnRandom]
    done enough arr[Math.floor(Math.random() to have a reusable tool that accepts that array on which the Math.methods() are based

---
+++ pokedex/src/components/utility/ReturnRandom.js
@@ -0,0 +1,9 @@
+
+export default async function ReturnRandom (array) {  // (array, range)
+    console.log('array')
+    console.log(array)
+    // also might allow a specification for range but this is to return 1 value. This app has helped learning react but is redundant. 
+    if (typeof array === 'object') {
+        console.log(array)
+    }
+}
