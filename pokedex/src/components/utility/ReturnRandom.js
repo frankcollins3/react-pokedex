72eafea97168b5cbef0aab5d0ec5fa26882d704f
Frank Collins <fwc3rd@gmail.com>
2022-09-26 13:20:03 -0400

    [ReturnRandom] testing our callback function
    ReturnRandom (array, min, max, constraincb)
    
    if constraincb is populated, there is conditional logic to:
    1) evaluate the array (in this case the /apiv2/type/ghost array ((78) [{…}, {…}, {…}, {…}, {…}) [all of the ghosts of every game. 78 of them from id 92-10000
    
    2) put the constraincb between:
    * the array-evaluation-and storing of randomValue in variable 
    ** (the constraincb) will be before the ReturnRandom function can return the value
    3) this inbetween expression that in this case checks for id length: allows the constraincb to tell the evaluateAndReturn() which gets/sets value to re-run
    
    (this essentially gives us a way to specify conditions that will have us reject the random return value and ask the function to rerun the grabbing of random val

--- pokedex/src/components/utility/ReturnRandom.js
+++ pokedex/src/components/utility/ReturnRandom.js
@@ -1,15 +1,38 @@
 
-export default async function ReturnRandom (array, min, max) {  // (array, range)
+export default async function ReturnRandom (array, min, max, constraincb) {  // (array, range)
     // also considering a fourth parameter which would be a constraintfunction callback: a conditional expression that would cause the ReturnRandom to run again if value not met.
     console.log(array)
     // console.log(min)
     console.log(max)
+    console.log('constraincb')
+    console.log(constraincb)
     // also might allow a specification for range but this is to return 1 value. This app has helped learning react but is redundant. 
     if (typeof array === 'object') {       
-
-            let length = array.length
-            let randomValue = array[Math.floor(Math.random()*length)]
-            return randomValue
-
+            if (constraincb !== null) {
+                console.log("constraincb is not equal to null")
+                const evaluateAndReturn = async () => {
+                    let length = array.length
+                    let randomValue = array[Math.floor(Math.random()*length)]
+                    console.log('randomValue')
+                    console.log(randomValue)
+                    let evalcb = await constraincb(randomValue)
+                    console.log('evalcb')
+                    console.log(evalcb)
+                    return randomValue
+                }
+                evaluateAndReturn()
+            } else {
+                console.log('constraincb is equal to null')
+                let length = array.length
+                let randomValue = array[Math.floor(Math.random()*length)]                
+                return randomValue
+            }
     }
 }
+
+// this is working. its taking an id of 10000 and returning false because we will get an empty value for that every time. 
+// randomValue
+// ReturnRandom.js:17 {pokemon: {…}, slot: 2}pokemon: {name: 'zoroark-hisui', url: 'https://pokeapi.co/api/v2/pokemon/10239/'}slot: 2[[Prototype]]: Object
+// NavBar.js:56 well maybe thatll work
+// ReturnRandom.js:19 evalcb
+// ReturnRandom.js:20 false
