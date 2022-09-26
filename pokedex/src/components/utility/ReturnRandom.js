0bffbfebf2f1e960f283cd1fc317db7dc1fe80a4
Frank Collins <fwc3rd@gmail.com>
2022-09-26 13:03:41 -0400

    [ReturnRandom] possible constraint cb for export randomizer

--- pokedex/src/components/utility/ReturnRandom.js
+++ pokedex/src/components/utility/ReturnRandom.js
@@ -1,9 +1,15 @@
 
-export default async function ReturnRandom (array) {  // (array, range)
-    console.log('array')
+export default async function ReturnRandom (array, min, max) {  // (array, range)
+    // also considering a fourth parameter which would be a constraintfunction callback: a conditional expression that would cause the ReturnRandom to run again if value not met.
     console.log(array)
+    // console.log(min)
+    console.log(max)
     // also might allow a specification for range but this is to return 1 value. This app has helped learning react but is redundant. 
-    if (typeof array === 'object') {
-        console.log(array)
+    if (typeof array === 'object') {       
+
+            let length = array.length
+            let randomValue = array[Math.floor(Math.random()*length)]
+            return randomValue
+
     }
 }
