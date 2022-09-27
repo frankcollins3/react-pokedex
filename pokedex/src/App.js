5da86627f01a352844c022cb3f03f4a5455b2792
Frank Collins <fwc3rd@gmail.com>
2022-09-26 22:50:16 -0400

    [App] switch state from boolean to array
    instead of doing:
    if (ghostState) == 'true' {
    
    more specific for 
    let ghostType = TypeLooper('ghost')
    let ghostpokemon = ghostType.pokemon
    
    setGhost(ghostpokemon) // (78) [{…}, {…}, {…}, {…}, {…}
    
    img.behavior/click/hover/----> setGhost.reduce() ||setGhost.map () --> mutate array and remove the $(event.target) / designated-upon-behavior element.

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   const [fakeDbState, setFakeDbState] = useState([])
-  const [ghost, setGhost] = useState('false')
+  const [ghost, setGhost] = useState([])
 
 
   return (
