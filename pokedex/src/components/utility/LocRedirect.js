ea78d6bd068ec59a3ade2354d59461af66f370d4
Frank Collins <fwc3rd@gmail.com>
2022-09-27 22:44:11 -0400

    [LocRedirect] calling  a Hook outside of a functional component
    as stated in the past commit you can't be on '/' as the current browser location and have useNavigate fire a refresh by invoking the method with ('/' first arg)
    using a hook in an export function that just returned an error before for calling { useNavigate } loc = useNavig() loc('/') FUNCTIONAL COMPONENT ERROR
    
    this passing over the useNavigate() as a function within a function parameter (3rd arg): selector, newurl, navigator allows us to use the Hook:
    which we do so by accessing its method to redirect our browser url
    
    ** there could also be a util/func() that access window or $(document) and does a regex to to reduce and compare '/' without the [server por href & friends]

--- pokedex/src/components/utility/LocRedirect.js
+++ pokedex/src/components/utility/LocRedirect.js
@@ -1,6 +1,32 @@
-export default async function LocationTool (selector, newurl) {
-    console.log('selector')
-    console.log(selector)
-    console.log('newurl')
-    console.log(newurl)
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+// let navBarNavigate = useNavigate()
+
+export default async function LocationTool (selector, newurl, navigator) {
+    console.log(typeof navigator)
+    if (typeof selector === 'string') {
+        console.log('if selector = string')
+        if (selector === 'Greatball') {
+            navigator(`${newurl}`)
+        }
+        // if (selector === 'Greatball') navigator('/')
+    }
+
 }
+
+
+// function Invoices() {
+//   let navigate = useNavigate();
+//   return (
+//     <div>
+//       <NewInvoiceForm
+//         onSubmit={async (event) => {
+//           let newInvoice = await createInvoice(
+//             event.target
+//           );
+//           navigate(`/invoices/${newInvoice.id}`);
+//         }}
+//       />
+//     </div>
+//   );
+// }
