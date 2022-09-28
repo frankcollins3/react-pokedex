56e6c81eee669f5b1a87945666f8ad5b953cf2a0
Frank Collins <fwc3rd@gmail.com>
2022-09-28 02:19:49 -0400

    [bg] set img-src url into array and return array
    export function that does not need any invocation parameters. store an [await bg()] call into a variable and access the arrays.

---
+++ pokedex/src/components/utility/bgList.js
@@ -0,0 +1,12 @@
+export default async function bg () {
+    let psychic = `public/img/energy/energyPsychic.jpg`
+    let electric = `public/img/energy/energyElectric.jpg`
+    let fire =    `public/img/energy/energyFire.jpg`
+    let leaf = `public/img/energy/energyLeaf.jpg`
+    let normal = `public/img/energy/energyNormal.jpg`
+    let water = `public/img/energy/energyWater.jpg`
+    let fighting = `public/img/energy/energyFighting.jpg`
+    let response = [psychic, electric, fire, leaf, normal, water, fighting]
+    return response
+}
+// electric normal fire water leaf fighting psychic 
