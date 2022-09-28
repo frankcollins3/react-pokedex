623c170be327b63a30570464a2bbb39271ec4ad5
Frank Collins <fwc3rd@gmail.com>
2022-09-28 15:30:15 -0400

    PushPop
    Handling the Push part of our logic where we would be pushing the prefix back onto the original string
    3:30pm

--- pokedex/src/components/utility/PushAndPop.js
+++ pokedex/src/components/utility/PushAndPop.js
@@ -1,5 +1,6 @@
-import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript'
 import TypeLooper from './TypeTool'
+import bgList from './bgList'
+import ReturnRandom from './ReturnRandom'
 export default async function PushPop (str, splitchar,  method) {
     // str: string to mutate 2) splitchar: the character from which to base our split method invocation 3) our method: 'push' or 'pop' 
     // push or pop takes our bg url [`/img/energy/energyNormal.png`] and adds the /img/ back onto it.     
@@ -7,21 +8,33 @@ export default async function PushPop (str, splitchar,  method) {
         if (method === 'pop') {     //             
             // console.log(str) //    /img/energy/grass.jpeg            
            let typelist  = await TypeLooper() // should set this up to receive no arguments and bring back all of the types so we can loop and validate but that is unnecessary        
-           console.log('typelist')
-           console.log(typelist)
+        //    console.log('typelist')
+        //    console.log(typelist)
            let textAfterLastSlash = str.substring(str.lastIndexOf("/") + 1)
              const remove_after = textAfterLastSlash.indexOf('.')
             let cleantype = textAfterLastSlash.substring(0, remove_after)            
     
             typelist.find( (type) => {
-                console.log(type)
                 if (type.name === cleantype) {
-                    console.log("we are verifying our returned type with the types from the pokeAPI /type endpoint")
-                    console.log(`type ${type.name}`)
-                    console.log(`cleantype ${cleantype}`)
+                    return cleantype
                 }
             })
-            
+        } else if (method === 'push') {        
+            console.log('in the push else')
+            let randombg = await ReturnRandom(await bgList())
+            console.log('randombg')
+            console.log(randombg)
+
+            let prefix = randombg.substring(0, randombg.lastIndexOf('/') + 1)
+            if (prefix.includes('.')) {
+                console.log("prefix includes a dot probably from .png .jpeg .jpg! NO GOOD!")
+            } else {
+                console.log('prefix doesnt include a . we have only the /img/energy part ')
+                console.log(prefix)
+            }
+
+
         }            
+            
     }   
 }
