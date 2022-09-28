bc1822b5ac361f92f0ff65d4803d031c606ce042
Frank Collins <fwc3rd@gmail.com>
2022-09-28 13:42:18 -0400

    [TypeTool] allow tool to return all typedata
    1) this tool was set up to receieve a specified type as a string data argument in the async function parameters
    2) I'm changing the specify-type code to run in an else block in the event of (func arg 'type') being null or undefined.
    
    1:42pm

--- pokedex/src/components/utility/TypeTool.js
+++ pokedex/src/components/utility/TypeTool.js
@@ -1,18 +1,30 @@
 import Axios from 'axios'
+import { collapseTextChangeRangesAcrossMultipleVersions, createNoSubstitutionTemplateLiteral } from 'typescript'
 
 let typeurl = `https://pokeapi.co/api/v2/type/`
 
 export default async function TypeLooper (type) {
-    let newurl = typeurl += type
-    console.log('newurl')
-    console.log(newurl)
-    let typelist = await Axios.get()
-    console.log('typelist')
-    console.log(typelist)
+    if (type === null || type === undefined) {
+        let alltypes = await Axios.get(typeurl)
+        console.log('alltypes')
+        console.log(alltypes)
+        return alltypes
+    } else {
+        let url = `${typeurl}${type}`
+        
+        let typelist = await Axios.get(url)
+        let specifiedlist = typelist.data
+        let pokemonlist = specifiedlist.pokemon
+        // console.log('pokemonlist')
+        // console.log(pokemonlist)
+        let typedata = {
+            raw: typelist,
+            data: specifiedlist,
+            pokemon: pokemonlist
+        }
+        return typedata
+
+    }
+
+    
 }
-
-
-
-// export default function GhostCatcher () {
-//     console.log("hello from the ghost function")
-// }
