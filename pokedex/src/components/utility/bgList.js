e64a96019238da3515211ae166c1ebc1b9d7383e
Frank Collins <fwc3rd@gmail.com>
2022-09-28 12:12:57 -0400

    bgList change to reduce regex needed to get type

--- pokedex/src/components/utility/bgList.js
+++ pokedex/src/components/utility/bgList.js
@@ -1,11 +1,11 @@
 export default async function bg () {
-    let psychic = `public/img/energy/energyPsychic.jpg`
-    let electric = `public/img/energy/energyElectric.jpg`
-    let fire =    `public/img/energy/energyFire.jpg`
-    let leaf = `public/img/energy/energyLeaf.jpg`
-    let normal = `public/img/energy/energyNormal.jpg`
-    let water = `public/img/energy/energyWater.jpg`
-    let fighting = `public/img/energy/energyFighting.jpg`
+    let psychic = `/img/energy/psychic.jpg`
+    let electric = `/img/energy/electric.jpeg`
+    let fire =    `/img/energy/fire.png`
+    let leaf = `/img/energy/grass.jpeg`
+    let normal = `/img/energy/normal.png`
+    let water = `/img/energy/water.png`
+    let fighting = `/img/energy/fighting.jpg`
     let response = [psychic, electric, fire, leaf, normal, water, fighting]
     return response
 }
