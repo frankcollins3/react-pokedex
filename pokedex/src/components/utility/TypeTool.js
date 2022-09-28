466c9f8f1e5eb68daa59aba5d829accaadd27114
Frank Collins <fwc3rd@gmail.com>
2022-09-28 15:00:50 -0400

    [TypeTool] endpoint fix
    👎  let data = alltypes.data ---> data.find || data.forEach() is undefined
    
    👍  let data = alltypes.data.results
    
    3:00pm

--- pokedex/src/components/utility/TypeTool.js
+++ pokedex/src/components/utility/TypeTool.js
@@ -1,30 +1,20 @@
 import Axios from 'axios'
-import { collapseTextChangeRangesAcrossMultipleVersions, createNoSubstitutionTemplateLiteral } from 'typescript'
-
 let typeurl = `https://pokeapi.co/api/v2/type/`
-
 export default async function TypeLooper (type) {
     if (type === null || type === undefined) {
         let alltypes = await Axios.get(typeurl)
-        console.log('alltypes')
-        console.log(alltypes)
-        return alltypes
+        let data = alltypes.data.results
+        return data
     } else {
-        let url = `${typeurl}${type}`
-        
+        let url = `${typeurl}${type}`        
         let typelist = await Axios.get(url)
         let specifiedlist = typelist.data
-        let pokemonlist = specifiedlist.pokemon
-        // console.log('pokemonlist')
-        // console.log(pokemonlist)
+        let pokemonlist = specifiedlist.pokemon        
         let typedata = {
             raw: typelist,
             data: specifiedlist,
             pokemon: pokemonlist
         }
         return typedata
-
-    }
-
-    
+    }    
 }
