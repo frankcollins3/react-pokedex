b53d274bdf2563012abeffb24e8ba1d62f6a3fde
Frank Collins <fwc3rd@gmail.com>
2022-09-28 01:27:32 -0400

    [Realistic Screen] for page 2
    1) plain red pokedex bg with no screen
    2) plain screenshot of screen
    
    [ O O O O O O ] there will be an above bar with several buttons to click to change [typeList, setTypeList]
    pokemon under the selected type will have ternary operator expressions in the in-line element props to render based on whatever type is selected

---
+++ pokedex/src/components/element/RealisticScreen.js
@@ -0,0 +1,9 @@
+function RealisticScreen() {
+    return (
+        <div className="Real-Screen">
+            {/* possible bootstrap carousel given that it wont be a giant span of data unless there are many many saved favorites. */}
+        </div>
+    )
+}
+
+export default RealisticScreen
