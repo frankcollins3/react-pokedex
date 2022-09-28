b2872ff26f2af85a75e4cbf109f97b179bd4865b
Frank Collins <fwc3rd@gmail.com>
2022-09-28 12:42:26 -0400

    [PushAndPop] not ideal but can keep stacking replace
    can easily just use lastIndexOf our just accept the [.png || .jpeg file extension]

--- pokedex/src/components/utility/PushAndPop.js
+++ pokedex/src/components/utility/PushAndPop.js
@@ -1,16 +1,45 @@
+import { collapseTextChangeRangesAcrossMultipleVersions, createNoSubstitutionTemplateLiteral } from "typescript"
+
 export default async function PushPop (str, splitchar,  method) {
     // str: string to mutate 2) splitchar: the character from which to base our split method invocation 3) our method: 'push' or 'pop' 
     // push or pop takes our bg url [`/img/energy/energyNormal.png`] and adds the /img/ back onto it. 
-    console.groupCollapsed()
-    // this will chop off the first part of our url or add it back on 
+    
+    if (typeof str === 'string' ) {     // had str instead of str invalidated the whole conditional
+        if (method === 'pop') {     //             
 
-    console.log('str')
-    console.log(str)
-    console.log(str.split(splitchar))
+            console.log(str)
+
+            const ignoreFlagStr = str.replace(/energy/i, '').replace(/[/\/]/g, '').replace(/img/i, '')
+
+            
+            // const ignoreFlagStr = str.replace(/energy/i, '').replace(/[/\/])
+            console.log('ignoreFlagStr')
+            console.log(ignoreFlagStr)
+
+            // const str = 'Twas the night before Xmas...';
+            // const newstr = str.replace(/xmas/i, 'Christmas');
+            // console.log(newstr);  // Twas the night before Christmas...
+
+            // /img/energy/grass.jpeg
+
+
+            // let processedValue = str.split(splitchar)
+            // console.log('processedValue')            
+            // console.log(processedValue)            
+            // let expression = /^[.^]+.\s*/;
+            // let lastchunk = processedValue[processedValue.length-1]
+            // console.log(lastchunk.replace(expression, ""))
+            // // .replace(/^[^-]+ - /,"")
+            // console.log('lastchunk')
+            // console.log(lastchunk)
+
+        }
+
+
+    }
+
     // (4) ['', 'img', 'energy', 'energyPsychic.jpg'] just like the functionality from the other side. 
 
-    console.log('method')
-    console.log(method)
-    console.groupEnd()
+
 }
 
