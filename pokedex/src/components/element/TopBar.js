4943d687ae8ed43b66d792e81e0d83a9780827f0
Frank Collins <fwc3rd@gmail.com>
2022-09-29 04:42:53 -0400

    [TopBar] regex to return behavior based target.urlType
    also looking into lookaheads and lookbehinds. Moving ahead is probably a better idea
    4:40am

--- pokedex/src/components/element/TopBar.js
+++ pokedex/src/components/element/TopBar.js
@@ -20,13 +20,14 @@ function TypeBar() {
     const [indexInt, setIndexInt] = useState(0)
 
     const [selectedType, setSelectedType] = useState('') // one type at a time that changes when you click one of the circles.
-    const [arrayPopulation, setArrayPopulation] = useState('false')
-
     const [appliedElements, setAppliedElements] = useState([])
 
     const [screenPokemon, setScreenPokemon] = useState('') // id or imgSrc or name     selectedType->selectedPokemon to go to pg 3 form hovering upon
 
-    useEffect( () => {
+    useEffect( (event) => {
+        $('*').css('overflow', 'hidden') // couldn
+        
+        
         const getBackgrounds = async () => {
             let importbg = await bg()
             // seteTypeBucket(importbg) wrong. it has to go to bg bucket. t
@@ -45,18 +46,10 @@ function TypeBar() {
             } else {
                 // let randombg = await ReturnRandom(bgBucket) 
 
-                if ($(event.target.attributes[1])) {
-                    console.log("there is an id")
-                    console.log(datagrab)
-                    console.log("there is no id")
-                    
-                    console.log(event)
-                    console.log(datagrab)
-
+                if ($(event.target.attributes[1])) {                
                     $(event.target)
-                    console.log($(event.target).attr('id'))
                     if ($(event.target).attr('id') === 'setbg') {
-                        console.log("its got that id....")
+                        console.log("its got that id....")                        
                     } else {
                         $(event.target)
                         .attr('id', 'setbg')
@@ -72,26 +65,48 @@ function TypeBar() {
         evaluateBg()
     }
 
-    const checkbg = () => {
-        console.log('alreadyUsed')
-        console.log(alreadyUsed)
-    }   
+    const TypeToggle = (event) => {
+        console.log(event)
+        // console.log($(event.target.outerHTML))
+        console.log(event.target.outerHTML)
+        let nonprettyurl = event.target.outerHTML
+        let length = nonprettyurl.length
+        let cleaner = nonprettyurl.substring(79, length-31)
+        let cleantype = cleaner.replace(/[\.]/g, '')
+        console.log('cleantype')
+        console.log(cleantype)
+        
+        
+        // let preregex = hi.substring(hi.IndexOf('/'[3]) + 1)
+        // let preregex = hi.substring(hi.lastIndexOf('/') + 1)
+        // console.log('preregex')
+        // console.log(preregex)
+        // console.log($(event.target.attributes
+        
+        // console.log(pregex)
+        // let prefix = randombg.substring(0, randombg.lastIndexOf('/') + 1)
 
-    const changeStateSource = async () => {
-        // console.log("change state source")
+        // console.log('preregex')
+        // console.log(preregex)
+        
+
+        // the goal is to find the type of the selected element from its inner object endpoint/properties.
+        // it will be attached to the imgSrc.
+        // change state with this
     }
+    
         
 
     return (
         <div className="Type-Bar Row-Center"> 
-        <button onClick={checkbg} className="navBall" id="Ultraball"> </button>
-        <div onMouseEnter={addBg} onMouseLeave={changeStateSource} className="Circle"></div>      
-        <div onMouseEnter={addBg} onMouseLeave={changeStateSource} className="Circle"></div>      
-        <div onMouseEnter={addBg} onMouseLeave={changeStateSource} className="Circle"></div>      
-        <div onMouseEnter={addBg} onMouseLeave={changeStateSource} className="Circle"></div>      
-        <div onMouseEnter={addBg} onMouseLeave={changeStateSource} className="Circle"></div>      
-        <div onMouseEnter={addBg} onMouseLeave={changeStateSource} className="Circle"></div>      
-        <div onMouseEnter={addBg} onMouseLeave={changeStateSource} className="Circle"></div>      
+        {/* <button  className="navBall" id="Ultraball"> </button> */}
+        <div onMouseEnter={addBg} onClick={TypeToggle}  className="Circle"></div>      
+        <div onMouseEnter={addBg} onClick={TypeToggle}  className="Circle"></div>      
+        <div onMouseEnter={addBg} onClick={TypeToggle}  className="Circle"></div>      
+        <div onMouseEnter={addBg} onClick={TypeToggle}  className="Circle"></div>      
+        <div onMouseEnter={addBg} onClick={TypeToggle}  className="Circle"></div>      
+        <div onMouseEnter={addBg} onClick={TypeToggle}  className="Circle"></div>      
+        <div onMouseEnter={addBg} onClick={TypeToggle}  className="Circle"></div>      
         </div>
     )
 }
