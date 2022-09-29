ad7cdb789cef4e286d7721db3299a17947731fb2
Frank Collins <fwc3rd@gmail.com>
2022-09-29 19:06:02 -0400

    [RealisticScreen] import pokeAPI function tool
    1) so the pokemon are rendered from:
    * pokeRefs.currnt.map( () => { (
    the refs.current.length is set/changed from the pokeDex.click() => input to specify how many pokemon.
    
    2) the map in the index is used as the id (starting from 0 so added {i + 1}) 
    3) the index that is set as an id is accessed and re-referenced through kids[0].innerHTML
    4 that mapindex/id/innerHTML.regex() integer is used to access the pokeAPI async function to return pokedata to access the type of the pokemon.
    
    5)
    👍  this type will then be used to match strictly in equality with the [type, setType] state
    
    6:58 PM

--- pokedex/src/components/element/RealisticScreen.js
+++ pokedex/src/components/element/RealisticScreen.js
@@ -1,5 +1,6 @@
 import { $ } from 'react-jquery-plugin'
 import CleanUrl from '../utility/CleanUrlTool'
+import APIcall from '../utility/pokeAPI'
 // $('*').css('overflow', 'scroll')
 $('.Real-Screen').css('overflow-y', 'scroll')
 
@@ -10,6 +11,16 @@ function RealisticScreen(props) {
         let kids = $(event.target).children()
         let childtext = kids[0].innerHTML
         let cleantext = childtext.replace(/[\s]/g, '')
+        let axiosaccess = await APIcall('specify', cleantext)
+        console.log('axiosaccess')
+        console.log(axiosaccess)
+
+// (2) [{…}, {…}] successful access grab. I clicked on number 19 from fakeDbState it brings up #19. Rattata. 
+// {abilities: Array(3), base_experience: 51, forms: Array(1), game_indices: Array(20), height: 3, …} 
+// {name: 'rattata', id: 19, image: 'https:raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png'}
+
+
+        
         // console.log(cleantext) console.log(cleantext.length)        
         // 5 -> RealisticScreen.js:20 (1) // 148 -> RealisticScreen.js:20 (3) 5 is 1 char '148 is 3 char. all good. 
     }
@@ -18,6 +29,7 @@ function RealisticScreen(props) {
         return (
             <div onClick={checkThat}className="Screen-Parents">
                 <p> {mapitem} </p>
+                <p> </p>
             </div>
         )
     })
