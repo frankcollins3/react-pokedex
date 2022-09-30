97ec6340f6cfd88600bcf988496ae141329f54f7
Frank Collins <fwc3rd@gmail.com>
2022-09-29 23:11:14 -0400

    [RealisticScreen] useEffect()=> dep. array type state
    confirm linking of state between 2 components.
    
    11:11pm

--- pokedex/src/components/element/RealisticScreen.js
+++ pokedex/src/components/element/RealisticScreen.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { $ } from 'react-jquery-plugin'
 import CleanUrl from '../utility/CleanUrlTool'
 import APIcall from '../utility/pokeAPI'
@@ -7,34 +8,26 @@ $('.Real-Screen').css('overflow-y', 'scroll')
 
 function RealisticScreen(props) {
     let fakedb = props.fakeDbState
+    let typestate = props.selectedType
+    
+
+    useEffect( () => {
+        console.log("we are changing the type A.K.A. clicking on a <TopBar/> .Circle elem that also has access to props.selectedType")
+        console.log(props.selectedType)
+        
+    }, [props.selectedType])
 
     const checkThat = async (event) => {
         let kids = $(event.target).children()
         let childtext = kids[0].innerHTML
         let cleantext = childtext.replace(/[\s]/g, '')
-        console.log('cleantext')
-        console.log(cleantext)
+        // console.log('cleantext')
+        // console.log(cleantext)
         let axiosaccess = await APIcall('specify', cleantext)
         let types = axiosaccess[0].types
         let type = types[0].type.name
         
-        let typecardlist = await bgList('typecard')
-        // (7) ['/img/text/electric.png', '/img/text/fighting.png', '/img/text/fire.png',  [correct]
-        // '/img/text/grass.png', '/img/text/normal.png', '/img/text/water.png', '/img/text/electric.png']
-
-        let energytest = await bgList('energy')
-        // test [correct]
-        // RealisticScreen.js:27 (7) ['/img/energy/psychic.jpg', '/img/energy/electric.jpeg', '/img/energy/fire.png', 
-        // '/img/energy/grass.jpeg', '/img/energy/normal.png', '/img/energy/water.png', '/img/energy/fighting.jpg']
-      
-
-
-
-
-
         
-        // console.log(cleantext) console.log(cleantext.length)        
-        // 5 -> RealisticScreen.js:20 (1) // 148 -> RealisticScreen.js:20 (3) 5 is 1 char '148 is 3 char. all good. 
     }
 
     let dbmap = fakedb.map( (mapitem) => {
