e75049845b031901c607462a75b9ee8a35fd82b9
Frank Collins <fwc3rd@gmail.com>
2022-09-30 17:13:08 -0400

    [Realistic]1 data now shows up mouseEnter() [5:12]
    the approach of adding state as an index to increment through to display based on value 
    is competing against fakeDb.map( () => rendering all of them at once like the first screen or with a carousel.

--- pokedex/src/components/element/RealisticScreen.js
+++ pokedex/src/components/element/RealisticScreen.js
@@ -6,6 +6,7 @@ import bgList from '../utility/bgList'
 import myCSS from '../utility/CSStool'
 import toggleHideShow from '../utility/hideShow';
 import {Alert, Button, Card, Carousel}  from 'react-bootstrap';   
+import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
 
 // $('*').css('overflow', 'scroll')
 $('.Real-Screen')
@@ -14,32 +15,54 @@ $('.Real-Screen')
 let i = 0;
 
 function RealisticScreen(props) {
+
+    
     let fakedb = props.fakeDbState
+    const [scrollIdx, setScrollidx] = useState(fakedb[0] || [])
     let typestate = props.selectedType
-    console.log('props')
-    console.log(props)
+    
     
 
     useEffect( () => {
-         $('.Screen-Parents').click()
+
+        //  $('.Screen-Parents').click()
+        // fakeDb[0].click()
+
          // ? $('.Screen-Parents').click() every time state is changed.
          console.log("we are changing the type A.K.A. clicking on a <TopBar/> .Circle elem that also has access to props.selectedType")
+         console.log(props.selectedType)
     }, [props.selectedType])
 
     // * AUTO CLICK FUNCTION TO FIRE EVERY TIME state is toggled.
     // * I have to move this function so it can be available to TopBar as well. 
     const checkThat = async (event) => {
+        console.log('check that')
+        console.log(event)
         // this function runs automatically upon state being set to corresponding pokemon id types. if you click on the RealisticScreen child element this will run.
         let kids = $(event.target).children()        
-        let text = event.target.innerText    
-        let cleantext = text.replace(/[\s]/g, '')        
-        let axiosaccess = await APIcall('specify', cleantext) || new Array()    
+
+        let text = event.target.innerText  
+        console.log('text')  
+        console.log(text)  
+        let cleantext = text.replace(/[\s]/g, '')    
+        console.log('text')    
+        console.log(text)    
+        let axiosaccess = await APIcall('specify', cleantext) || new Array()   
+        console.log('axiosaccess') 
+        console.log(axiosaccess) 
         let image = axiosaccess[1].image
+        console.log('image')
+        console.log(image)
+
         let types = axiosaccess[0].types
         let type = types[0].type.name        
+        console.log('type')
+        console.log(type)
         if (type !== typestate) {
+            console.log('type doesnt equal stateType')
             toggleHideShow($(event.target), 'hide')
         } else {
+            console.log('type == stateType')
             let typebg = await bgList('typecard', type) // async  Promise {<fulfilled>: undefined}            
             $(event.target).css('background', `url('${typebg}')`)
             await toggleHideShow($(event.target), 'show')
@@ -50,6 +73,14 @@ function RealisticScreen(props) {
             })            
         }
     }
+
+    const indexChanger = () => {
+        $('.Display-Poke').click()
+        console.log('were doing this')
+        setInterval(setScrollidx(scrollIdx + 1), 2000)
+    }
+
+
 
     let dbmap = fakedb.map( (mapitem, idx) => {        
         return (
@@ -65,16 +96,26 @@ function RealisticScreen(props) {
 
     return (
         
-        <div className="Real-Screen Column-Between">
+        <div onMouseEnter={indexChanger} className="Real-Screen Column-Between">
             {/* {dbmap} */}
-            <p> {fakedb[1]} </p> 
+            <p className="Display-Poke" onClick={checkThat}> { fakedb[scrollIdx] || fakedb[0]} </p> 
             {/* {dbmap} */}
        
     {/* <Carousel>
       <Carousel.Item>
       </Carousel.Item>
     </Carousel> */}
-    
+
+
+
+
+
+
+
+
+
+
+
         </div>
     )
 }
