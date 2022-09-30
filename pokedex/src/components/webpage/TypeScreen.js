26331a03408b8895fc6f74f8ba886012fe2bbc75
Frank Collins <fwc3rd@gmail.com>
2022-09-29 23:03:06 -0400

    [TypeScreen] not parent props. func comp() state
    11:02 pm

--- pokedex/src/components/webpage/TypeScreen.js
+++ pokedex/src/components/webpage/TypeScreen.js
@@ -4,8 +4,8 @@ import RealisticScreen from '../element/RealisticScreen'
 import MiniScreen from '../element/MiniRealScreen'
 import TopBar from '../element/TopBar'
 function TypeScreen (props) {
-    const [selectedType, setSelectedType] = useState('')
-        
+    const [selectedType, setSelectedType] = useState([])
+    console.log(setSelectedType)
 
 
     return (
@@ -13,20 +13,20 @@ function TypeScreen (props) {
             <div className="Type-Wrap Column-Center">
                 <RealisticScreen 
                 fakeDbState={props.fakeDbState} setFakeDbState={props.setFakeDbState}
-                selectedType={props.selectedType} setSelectedType={props.setSelectedType}
+                selectedType={selectedType} setSelectedType={setSelectedType}
                 />
 
                 <TopBar 
                 fakeDbState={props.fakeDbState} setFakeDbState={props.setFakeDbState}
-                selectedType={props.selectedType} setSelectedType={props.setSelectedType}
+                selectedType={selectedType} setSelectedType={setSelectedType}
                 />
                 <div className="Second-Half">
                 <MiniScreen
-                selectedType={props.selectedType} setSelectedType={props.setSelectedType}
+                selectedType={selectedType} setSelectedType={setSelectedType}
                 />
                 <Nav /> 
                 </div>
-
+                {/* {fakeDbState: Array(0), selectedType: Array(0), setFakeDbState: ƒ, setSelectedType: ƒ} */}
                 <div className="Type-Wrap-2 Column-Between">
                 </div>
             </div>
