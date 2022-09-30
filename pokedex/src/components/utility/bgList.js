c02c6d9b84b64c0d78d85999d39835aea0fccc39
Frank Collins <fwc3rd@gmail.com>
2022-09-29 20:09:51 -0400

    [bgList] added bg (bgtype) arg to distinguish return data
    1) 'energy' ----> return the .Circle backgrounds
    2) typeCard ----> return the type-card-tops that will be in the big screen (rendered accordingly to [stateType, setStateType] === 'psychic' (i.e.)
    
    was going to make other tool for the other bg list but decided to split into 2

--- pokedex/src/components/utility/bgList.js
+++ pokedex/src/components/utility/bgList.js
@@ -1,12 +1,28 @@
-export default async function bg () {
-    let psychic = `/img/energy/psychic.jpg`
-    let electric = `/img/energy/electric.jpeg`
-    let fire =    `/img/energy/fire.png`
-    let leaf = `/img/energy/grass.jpeg`
-    let normal = `/img/energy/normal.png`
-    let water = `/img/energy/water.png`
-    let fighting = `/img/energy/fighting.jpg`
-    let response = [psychic, electric, fire, leaf, normal, water, fighting]
-    return response
+export default async function bg (bgtype) {
+    console.log('bgtype from the bgList')
+    console.log(bgtype)
+    if (bgtype === 'energy') {
+        let psychic = `/img/energy/psychic.jpg`
+        let electric = `/img/energy/electric.jpeg`
+        let fire =    `/img/energy/fire.png`
+        let leaf = `/img/energy/grass.jpeg`
+        let normal = `/img/energy/normal.png`
+        let water = `/img/energy/water.png`
+        let fighting = `/img/energy/fighting.jpg`
+        let response = [psychic, electric, fire, leaf, normal, water, fighting]
+        return response
+    } else if (bgtype === 'typecard' && typeof bgtype === 'string') {
+        console.log('bgtype === typecard')
+        console.log(bgtype)
+        let psychic = '/img/text/electric.png'
+        let fighting = '/img/text/fighting.png'
+        let fire = '/img/text/fire.png'
+        let grass = '/img/text/grass.png'
+        let normal = '/img/text/normal.png'        
+        let water = '/img/text/water.png'
+        let electric = '/img/text/electric.png'
+        let response = [psychic, fighting, fire, grass, normal, water, electric]
+        return response
+    }
 }
 // electric normal fire water leaf fighting psychic 
