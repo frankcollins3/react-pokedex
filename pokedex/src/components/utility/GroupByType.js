8e1a3db2fc7082e4f2b28afc2c161fd0d208a697
Frank Collins <fwc3rd@gmail.com>
2022-09-30 18:32:49 -0400

    [GroupByType] returning data before group [6:30pm]
    this function placed in the useEffect that triggers on selectedType change is exactly what we want

--- pokedex/src/components/utility/GroupByType.js
+++ pokedex/src/components/utility/GroupByType.js
@@ -1,11 +1,23 @@
 // import TypeTool from '../utility/TypeTool' nvm this won't help.
 import APIcall from './pokeAPI'
+import Axios from 'axios'
+let url = `https://pokeapi.co/api/v2/pokemon/`
+let bucket = [] || new Array()
+let typebucket = new Array() || []
 
-export default async function GroupByType([arr]) {
-    arr.forEach( (arritem) => {
-        let pokemon = APIcall('specify', arritem)
-        console.log('pokemon')
-        console.log(pokemon)
-        console.log(pokemon[0])
+export default async function GroupByType(arr, type) {
+    console.log('type')
+    console.log(type)
+    console.log('arr')
+    console.log(arr)
+
+     arr.map(async(mapitem) => {        
+        let datacall = await APIcall('specify', url += mapitem)
+        console.log('datacall')
+        console.log(datacall)
+        // // let datacall = await Axios.get(url += mapitem)
     })
+    
+
+    
 }
