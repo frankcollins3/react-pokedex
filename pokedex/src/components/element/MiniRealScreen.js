1afe777bda6ca676b8fab0aef855ae9dd7b111a4
Frank Collins <fwc3rd@gmail.com>
2022-09-30 15:56:14 -0400

    [MiniScreen] ternary/conditional rendering
    parent state based image data if no image data then nothing in the screen

--- pokedex/src/components/element/MiniRealScreen.js
+++ pokedex/src/components/element/MiniRealScreen.js
@@ -1,6 +1,15 @@
-function MiniScreen() {
+function MiniScreen(props) {
+    console.log("hello from minirealscreen!")
+    console.log(props)
     return (
-        <div className="Real-Screen Column-Center Quarter-Size"></div>
+        <div className="Real-Screen Column-Center Quarter-Size">
+        {props.miniScreenPokemon ?
+        <img 
+        style={{ height: '100px', width: '100px'}}
+        className="Sprite Quadruple" src={props.miniScreenPokemon}/>
+        :
+        <div className="Invisible"> </div>}
+        </div>
     )
 }
 
