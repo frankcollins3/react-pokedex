e7f651861d1c3fa49aed3e748fce25d926d0a5a6
Frank Collins <fwc3rd@gmail.com>
2022-10-01 19:35:53 -0400

    [Info] retrieve url param id and invoke cleanUrl
    {react: cons.info(window)} || {jq: $(document)}. 
    rather use react practices
    [7:35pm]

--- pokedex/src/components/webpage/InfoScreen.js
+++ pokedex/src/components/webpage/InfoScreen.js
@@ -1,5 +1,6 @@
 import { $ } from 'react-jquery-plugin'
 import React, { useEffect, useState } from 'react'
+import CleanUrl from '../utility/CleanUrlTool'
 // this is a webpage that will import multiple components that have access to the same urlparam state.
 function InfoScreen() {
     const [paramPoke, setParamPoke] = useState('')
@@ -14,14 +15,30 @@ function InfoScreen() {
         //  second arg: `/pokemon/${cleanurl}`  LocationTool(null || undefined, `/pokemon/${cleanurl}`, imagenavigate)
         console.log('win')
         console.log(win)
+        const paramState = async () => {
+            let currloc = win.navigation.currentEntry.url
+            console.log('currloc')
+            console.log(currloc)
+            let cleanint = await CleanUrl(currloc)
+            console.log(cleanint)
+            // await setParamPoke(cleanint)
+        }
+        paramState()
 
+        
     }, [])
-
+    
+    const check = () => {
+        console.log('paramPoke')
+        console.log(paramPoke)
+    }
+    
     // }, [paramPoke]) wouldn't have this because if the paramPoke changed the whole webpage would change anyways.
 
     return (
         <div className="Info-Wrap">
             <p> Hey !</p>
+            <button onClick={check} className="navBall" id="Pokeball"></button>
         </div>
     )
 }
