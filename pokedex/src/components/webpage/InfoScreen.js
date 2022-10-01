979af67dda6fac7a160dddf8ebc7ee234f1fa7fb
Frank Collins <fwc3rd@gmail.com>
2022-10-01 18:01:18 -0400

    [InfoPage] set up to dig for urlparams setState()
    shall setState once that '/pokemon/:id
    
    once the id sent from regex'ing .replace() removing ./ and a-z to retrieve int from the image src url from event.target
    [6:01pm]

--- pokedex/src/components/webpage/InfoScreen.js
+++ pokedex/src/components/webpage/InfoScreen.js
@@ -1,4 +1,24 @@
+import { $ } from 'react-jquery-plugin'
+import React, { useEffect, useState } from 'react'
+// this is a webpage that will import multiple components that have access to the same urlparam state.
 function InfoScreen() {
+    const [paramPoke, setParamPoke] = useState('')
+
+    useEffect( () => {
+        let jqdoc = $(document)
+        let win = window
+        console.log('jqdoc')
+        console.log(jqdoc)
+        // * we are digging for that urlparam which is our /pokemon/:id  
+        // * urlparam will be set when we get it from the secondArg in our pure function tool
+        //  second arg: `/pokemon/${cleanurl}`  LocationTool(null || undefined, `/pokemon/${cleanurl}`, imagenavigate)
+        console.log('win')
+        console.log(win)
+
+    }, [])
+
+    // }, [paramPoke]) wouldn't have this because if the paramPoke changed the whole webpage would change anyways.
+
     return (
         <div className="Info-Wrap">
             <p> Hey !</p>
