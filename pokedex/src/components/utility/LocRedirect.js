1c0672757d2adc68c7f0ea91e80f0cd6fbcd8d5b
Frank Collins <fwc3rd@gmail.com>
2022-10-01 17:45:01 -0400

    [href=] modified tool to accept null value [5:43]
    I wasn't able to get this working
    then I went the jquery route for minimum viability 
    then using the useNavigate() hook as is.
    
    👍 got this to work by letting the export pure-function accept null or undefined as the first argument that is its checking for an existence of a selector
    
    this is set up so that we have a navigator <img> outside of the navbar. This is for the 2nd pages miniscreen pokemon hover that brings us to the 3rd page.
    /pokemon/${hoverpokemonID}/

--- pokedex/src/components/utility/LocRedirect.js
+++ pokedex/src/components/utility/LocRedirect.js
@@ -1,40 +1,20 @@
 import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
-// let navBarNavigate = useNavigate()
+import { useNavigate } from 'react-router-dom'// let navBarNavigate = useNavigate()
 
-export default async function LocationTool (selector, newurl, navigator, link, component) {    
-    console.log('link')
-    console.log(link || 'nolink')
-    if (typeof selector === 'string') {         
-        if (link === null || link === undefined) {
-            console.log('it is a string redirect tool')
-            if (selector === 'Greatball')  navigator(`${newurl}`)
-            
-        }
-        if (selector === 'Pokeball') {
-            console.log("atleast we have the pokeball")
+export default async function LocationTool (selector, newurl, navigator) {        
+    if (selector === null || selector === undefined) {
+        navigator(`${newurl}`)
+    }
+    if (typeof selector === 'string' && selector !== null || selector !==  undefined) {         
+        if (selector === 'Greatball' || 'Pokeball') {
+                if (selector === 'Greatball')  navigator(`${newurl}`)
+                if (selector === 'Pokeball')  navigator(`${newurl}`)               
+        } else {
+            console.log('selector that isnt pokeball or greatball')
+            console.log(selector)
             navigator(`${newurl}`)
         }
-        // if (selector === 'Pokeball') navigator(`${newurl}`)
-    } 
-    
-    // if (selector === 'Greatball') navigator('/')
 
+    }     
 }
 
-
-// function Invoices() {
-//   let navigate = useNavigate();
-//   return (
-//     <div>
-//       <NewInvoiceForm
-//         onSubmit={async (event) => {
-//           let newInvoice = await createInvoice(
-//             event.target
-//           );
-//           navigate(`/invoices/${newInvoice.id}`);
-//         }}
-//       />
-//     </div>
-//   );
-// }
