24b57478a0524aa33b0565cf25ccc0fec235aee0
Frank Collins <fwc3rd@gmail.com>
2022-10-01 19:26:55 -0400

    [urlTool] change handling of parameter [7:25]pm
    to make another constructor class or do a regular .substring() or .regex() or not. I don't use ES6 classes too often.

--- pokedex/src/components/utility/CleanUrlTool.js
+++ pokedex/src/components/utility/CleanUrlTool.js
@@ -1,9 +1,7 @@
 // Axios could also use Axios and access the name with the url parameter and return this.name = the name of the pokemon. 
 // we can also use Axios on the other side and leave this as single responsibility only return the clean ID tool. 
-
 export default async function CleanUrl (url) {
     if (typeof url === 'string' && url.includes('pokeapi')) {
-
         class Cleaner {
 
             constructor(url) {
@@ -17,12 +15,20 @@ export default async function CleanUrl (url) {
             regex() {
                 return this.url.slice(url.length-5).replace(/[/\/a-z]/g, '')                                
             }
+            whitespace() {
+              return 
+            }
           }              
-          const newid = new Cleaner(url)          
+          const newid = new Cleaner(url)                
           const returnId = new Cleaner(url).cleanup
-          console.log(returnId)
+          // console.log(returnId)
             return returnId
-
+          } else if (typeof url === 'string' && url.includes('localhost')) {
+            console.log('url')
+            console.log(url)
+            return url
+          }
+ 
 
 
 
@@ -38,6 +44,5 @@ export default async function CleanUrl (url) {
 // : 
 // Object
           
-    }   // typeof conditional end
 }   // async funct CleanUrl() { ____ END____}
 
