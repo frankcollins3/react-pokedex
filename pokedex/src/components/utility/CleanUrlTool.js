69b569e4791302b2deb4e7261983c35ebbf9ea52
Frank Collins <fwc3rd@gmail.com>
2022-10-01 19:33:22 -0400

    [UrlTool] multiple url handling arg [7:32pm]
    1) this ES6 class that accepts and runs handling expressions based on (url parameter) which would be a pokemon related url of some sort:
    2)  is returning data and console.logs() while invoking its this.methods()

--- pokedex/src/components/utility/CleanUrlTool.js
+++ pokedex/src/components/utility/CleanUrlTool.js
@@ -1,32 +1,41 @@
 // Axios could also use Axios and access the name with the url parameter and return this.name = the name of the pokemon. 
 // we can also use Axios on the other side and leave this as single responsibility only return the clean ID tool. 
 export default async function CleanUrl (url) {
-    if (typeof url === 'string' && url.includes('pokeapi')) {
-        class Cleaner {
+  class Cleaner {
 
-            constructor(url) {
-              this.url = url
-            }
-            // Getter
-            get cleanup() {
-              return this.regex();
-            }
-            // Method
-            regex() {
-                return this.url.slice(url.length-5).replace(/[/\/a-z]/g, '')                                
-            }
-            whitespace() {
-              return 
-            }
-          }              
+      constructor(url) {
+        this.url = url
+      }
+      // Getter
+      get cleanup() {
+        return this.regex();
+      }
+      get localhostcleanup() {
+        return this.localhostregex()
+      }
+      // Method(s)
+      regex() {
+          return this.url.slice(url.length-5).replace(/[/\/a-z]/g, '')                                
+      }
+      localhostregex() {
+        console.log("just doing a quick test and firing this")
+        return {here: 'a_gift'}
+        // {here: 'a_gift'} *** this is the terminal output this was getting set up correctly.  
+      }
+      
+    }              
+    if (typeof url === 'string' && url.includes('pokeapi')) {
           const newid = new Cleaner(url)                
           const returnId = new Cleaner(url).cleanup
           // console.log(returnId)
             return returnId
           } else if (typeof url === 'string' && url.includes('localhost')) {
-            console.log('url')
+            console.log('url down here')
             console.log(url)
-            return url
+            const checkclass = new Cleaner(url).localhostcleanup
+            return checkclass
+            console.log(checkclass)
+            
           }
  
 
