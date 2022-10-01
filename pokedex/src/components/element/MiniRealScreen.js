68c9a24b04d69592f5f0739e51c536869ebb4f5d
Frank Collins <fwc3rd@gmail.com>
2022-10-01 16:39:01 -0400

    [Mini] click pokemon returns accurate img src [4:37]
    👎  slight problem though.  it might be related to the .wheel event being quick, but the id can get out of sync. (venusaur can show up as #3-#5
    
    👍 good news is the pokemon img data retrieved from hovering and accessing the event.target obj endpoints is dead accurate.

--- pokedex/src/components/element/MiniRealScreen.js
+++ pokedex/src/components/element/MiniRealScreen.js
@@ -1,10 +1,20 @@
 function MiniScreen(props) {
-    console.log("hello from minirealscreen!")
-    console.log(props)
+    let assessItem = (event) => {
+        console.log(event)
+        console.log(event.target)
+        // let hoverimage = event.target.currentSrc // not right this brings back the <img. you can see the element, not as a string.
+        let hoverimage = event.target.attributes[1].nodeValue
+        console.log('hoverimage')
+        console.log(hoverimage)
+        // let name = APIcall('specify', hoverimage.regex)
+        // change url to localhost:3000/pokemon/name to the 3rd page which will be an info page accessing a few different api endpoints 
+        // from pokeapi for this specific pokemon 
+    }
     return (
         <div className="Real-Screen Column-Center Quarter-Size">
         {props.miniScreenPokemon ?
         <img 
+        onClick={assessItem}
         style={{ height: '100px', width: '100px'}}
         className="Sprite Quadruple" src={props.miniScreenPokemon}/>
         :
