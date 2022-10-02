2e531d436f9e5c41a7b83b5324ed84ea997473f9
Frank Collins <fwc3rd@gmail.com>
2022-10-01 20:48:03 -0400

    [Info] access API util-func & prop.urlparam[8:47]
    access url params from window.navigation object endpoints
    access pokeapi and image data from APIcall()module by invoking it and using the props.id as the APIcall('', 'id')  data specifying argument

--- pokedex/src/components/element/InfoPokeImage.js
+++ pokedex/src/components/element/InfoPokeImage.js
@@ -1,8 +1,21 @@
 import React, {useEffect, useState} from 'react'
+import APIcall from '../utility/pokeAPI'
 
 function InfoPokeImage(props) {
     console.log('props')
-    console.log(props)
+    // console.log(props)     {paramPoke: '1', setParamPoke: ƒ}
+    const [imageUrl, setImageUrl] = useState('')
+        
+    useEffect( () => {
+        (async () => {
+            console.log("iife for liife")
+            let id = props.paramPoke
+            let pokeprops = await APIcall('specify', id) // ty typeof === 'string' || int handling logic
+            let image = pokeprops[1].image
+        })()
+        
+
+    })
     return (
         <div className="Info-Image-Container">
             <p> Pokemon Image Demo text</p>
