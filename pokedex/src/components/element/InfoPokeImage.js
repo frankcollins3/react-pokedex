a1e20a95b48d1048a7a2fcaecbfcae852a933272
Frank Collins <fwc3rd@gmail.com>
2022-10-01 21:49:02 -0400

    [InfoImage]
    1) object.vals / keys ()  
    2) for (in) loop
    
                        // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png
                        null
                        // InfoPokeImage.js:26 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png
                        null // this is how the console was
                        // InfoPokeImage.js:26 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
                        // InfoPokeImage.js:26 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png
    
    to iterate through object and push array into state
    [9:48pm]

--- pokedex/src/components/element/InfoPokeImage.js
+++ pokedex/src/components/element/InfoPokeImage.js
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react'
 import APIcall from '../utility/pokeAPI'
 import Axios from 'axios'
+import { createNoSubstitutionTemplateLiteral } from 'typescript'
 
 function InfoPokeImage(props) {
     console.log('props')
     // console.log(props)     {paramPoke: '1', setParamPoke: ƒ}
     const [imageBucket, setImageBucket] = useState([])
     const [imageUrl, setImageUrl] = useState('')
+    const plainbucket = new Array() || []
         
     useEffect( () => {
         (async () => {
@@ -14,33 +16,53 @@ function InfoPokeImage(props) {
             console.log('props.paramPoke')
             let data = await Axios.get(`https://pokeapi.co/api/v2/pokemon/${props.paramPoke}`)
             // let data = await APIcall('specify', `${props.paramPoke}`)            
-            let pokemon = data.data
-            console.log
-            // pokemon.sprites.forEach( (img) => {
-            //     console.log('img')
-            //     console.log(img)
-            // })
-            // let image = pokemon.sprites.front_default
-            // console.log
+            console.log(data)
+            let keys = Object.keys(data.data)
+            let vals = Object.values(data.data)
+            let images = vals[14]
+            for (const img in images) {                
+                // console.log(`${images[img]}`)
+                if (`${images[img]}`.includes('usercontent')) {
+                    // console.log(`${images[img]}`) GOT IT. all URLS no nulls or [objectobject]
+                    // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png
+                    // InfoPokeImage.js:26 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png
+                    // InfoPokeImage.js:26 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+                    // InfoPokeImage.js:26 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png
+                }
+                    
+                // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png
+                // InfoPokeImage.js:26 null
+                // InfoPokeImage.js:26 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png
+                // InfoPokeImage.js:26 null
+                // InfoPokeImage.js:26 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+                // InfoPokeImage.js:26 null
+                // InfoPokeImage.js:26 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png
+                // InfoPokeImage.js:26 null
+                // setImageBucket([...imageBucket, `${images[img]}`])
+            }
+            if (vals.includes('front_default')) {
+            }
 
-            
-            // let image = data[1].image
-            //         Uncaught (in promise) TypeError: Cannot read properties of undefined (reading '1')
-            // at InfoPokeImage.js:14:1
-
-            
-            
         })()
-        
-
     })
+
+    const seeState = async () => {
+        if (imageBucket.length < 5) {
+            console.log("filling bucket!")
+            await setImageBucket([plainbucket])
+            console.log(imageBucket)
+
+        }
+        
+    }
+    
     return (
         <div className="Info-Image-Container">
-            <p> Pokemon Image Demo text</p>
+            <p onMouseEnter={seeState}>  Pokemon Image Demo text</p>
             {imageUrl.length ?
             <img className="Sprite"src={imageUrl} />
             :
-            <p> no state no image</p>
+            <p onClick={seeState}> no state no image</p>
         }
 
         </div>
