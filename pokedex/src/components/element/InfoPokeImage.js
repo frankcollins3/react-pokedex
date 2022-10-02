f3ec3689830c36b48a7590222ce46732715d86b7
Frank Collins <fwc3rd@gmail.com>
2022-10-01 21:16:57 -0400

    [InfoImage] acces pokeAPI && module func
    getting uncaught promise error and am looping through the sprites image endpoints
    [9:16pm]

--- pokedex/src/components/element/InfoPokeImage.js
+++ pokedex/src/components/element/InfoPokeImage.js
@@ -1,17 +1,35 @@
 import React, {useEffect, useState} from 'react'
 import APIcall from '../utility/pokeAPI'
+import Axios from 'axios'
 
 function InfoPokeImage(props) {
     console.log('props')
     // console.log(props)     {paramPoke: '1', setParamPoke: ƒ}
+    const [imageBucket, setImageBucket] = useState([])
     const [imageUrl, setImageUrl] = useState('')
         
     useEffect( () => {
         (async () => {
             console.log("iife for liife")
-            let id = props.paramPoke
-            let pokeprops = await APIcall('specify', id) // ty typeof === 'string' || int handling logic
-            let image = pokeprops[1].image
+            console.log('props.paramPoke')
+            let data = await Axios.get(`https://pokeapi.co/api/v2/pokemon/${props.paramPoke}`)
+            // let data = await APIcall('specify', `${props.paramPoke}`)            
+            let pokemon = data.data
+            console.log
+            // pokemon.sprites.forEach( (img) => {
+            //     console.log('img')
+            //     console.log(img)
+            // })
+            // let image = pokemon.sprites.front_default
+            // console.log
+
+            
+            // let image = data[1].image
+            //         Uncaught (in promise) TypeError: Cannot read properties of undefined (reading '1')
+            // at InfoPokeImage.js:14:1
+
+            
+            
         })()
         
 
@@ -19,6 +37,12 @@ function InfoPokeImage(props) {
     return (
         <div className="Info-Image-Container">
             <p> Pokemon Image Demo text</p>
+            {imageUrl.length ?
+            <img className="Sprite"src={imageUrl} />
+            :
+            <p> no state no image</p>
+        }
+
         </div>
     )
 }
