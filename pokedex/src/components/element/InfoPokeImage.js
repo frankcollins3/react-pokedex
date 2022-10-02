9f1317c05354a9aa2d1c2d205807ec167c52e2d6
Frank Collins <fwc3rd@gmail.com>
2022-10-01 20:40:43 -0400

    [InfoPokeimage] 3rd page Left component [8:40pm]
    large sized container & image that is based on the url/pokemon/${integerState}
    This is part of the 3rd and final page.
    1st page: API consumption set by pokeRefs.current.length. The .length is established by an $(target: ${input}} input data. [Select 30. See id #1-30]
    
    2nd page:
    1) organized by type. click leaf icon ----> see chosen leaf pokemon and scroll through when hovered on the big container. 
    2) when a particular pokemon appears, set the state and miniscreenpokemon to have the pokemon of the type of the big screen appear on the mini screen.
    3) hover over that pokemon ----> redirect to 3rd screen.

---
+++ pokedex/src/components/element/InfoPokeImage.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from 'react'
+
+function InfoPokeImage(props) {
+    console.log('props')
+    console.log(props)
+    return (
+        <div className="Info-Image-Container">
+            <p> Pokemon Image Demo text</p>
+        </div>
+    )
+}
+export default InfoPokeImage
