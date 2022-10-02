33239cc5dac49a1ae3f1e1f8c13a9774fde72538
Frank Collins <fwc3rd@gmail.com>
2022-10-02 14:05:21 -0400

    change bg of pokeimage by changing state [2:00]pm
    utilize pure-function getImage(id/props.paramPoke, 'front || back || shiny)
    should:
    👍  await setImageUrl(frontimg) give this vehicle some bumper room with async specification as well but its working without it.

--- pokedex/src/components/element/InfoPokeImage.js
+++ pokedex/src/components/element/InfoPokeImage.js
@@ -1,50 +1,98 @@
-import { $ } from 'react-jquery-plugin'
-import React, { useEffect, useState } from 'react'
-import CleanUrl from '../utility/CleanUrlTool'
-import InfoPokeImage from '../element/InfoPokeImage'
+import React, {useEffect, useState, useRef} from 'react'
+import APIcall from '../utility/pokeAPI'
+import GetImage from '../utility/ImageTool'
+import ReturnRandom from '../utility/ReturnRandom'
+import myCSS from '../utility/CSStool'
+import { $ } from 'react-jquery-plugin' 
+import toggleHideShow from '../utility/hideShow'
+import Axios from 'axios'
 
+function InfoPokeImage(props) {
+    console.log('props')
+    // console.log(props)     {paramPoke: '1', setParamPoke: ƒ}
+    const [imageBucket, setImageBucket] = useState([])
 
-// this is a webpage that will import multiple components that have access to the same urlparam state.
-function InfoScreen() {
-    const [paramPoke, setParamPoke] = useState('')
+    const [imageUrl, setImageUrl] = useState('')
 
-    useEffect( () => {
-        let jqdoc = $(document)
-        let win = window
-        console.log('jqdoc')
-        console.log(jqdoc)
-        // * we are digging for that urlparam which is our /pokemon/:id  
-        // * urlparam will be set when we get it from the secondArg in our pure function tool
-        //  second arg: `/pokemon/${cleanurl}`  LocationTool(null || undefined, `/pokemon/${cleanurl}`, imagenavigate)
-        console.log('win')
-        console.log(win)
-        const paramState = async () => {
-            let currloc = win.navigation.currentEntry.url
-            console.log('currloc')
-            console.log(currloc)
-            let cleanint = await CleanUrl(currloc)
-            setParamPoke(cleanint)
-        }
-        paramState()
-
-        
-    }, [])
+    const inforef = useRef()
     
-    const check = () => {
-        console.log('paramPoke')
-        console.log(paramPoke)
+    // setTimeout( () => {
+    //     $(inforef).css('border', '5px solid purple')
+    //     $(inforef).css('border', '5px dotted orange')
+    // }, "3000")
+    // $(inforef).css('')
+    // myCSS($(inforef).current, 'border', '5px dotted orange')
 
-        // paramPoke InfoScreen.js:32 1 parampoke now returns 1 because of our export function t
+    // const [randomImage, setRandomImage] = useState(imageBucket[Math.floor(Math.random()*imageBucket.length)] || ['hey'])
+    const plainbucket = new Array() || []
+        
+    useEffect( () => {
+        (async () => {
+            
+            // let data = await Axios.get(`https://pokeapi.co/api/v2/pokemon/${props.paramPoke}`)
+            // let data = await APIcall('specify', `${props.paramPoke}`)            
+            // console.log(data)
+            
+            // let allimages = await GetImage('', 'all')            
+            // console.log('allimages')
+            // console.log(allimages)
+
+            // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+            // let keys = Object.keys(data.data)
+            // let vals = Object.values(data.data)
+            // let images = vals[14]
+            // for (const img in images) {                
+            //     if (`${images[img]}`.includes('usercontent')) {                    
+            //     }                
+            // }
+            
+
+        })()
+    }, [])  // error set state run over and over no dependency array to trigger end. 
+
+    const seeState = async () => {
+        console.log('imageBucket')
+        console.log(imageBucket)
+    }
+
+    const inspectcont = async (event) => {
+        setTimeout(async() => {
+            let shinyparam = await GetImage(props.paramPoke, 'shiny')
+            console.log('shinyparam')
+            console.log(shinyparam)
+            setImageUrl(shinyparam)
+            // toggleHideShow($('.Sprite'), 'hide')
+            // myCSS($('.Sprite').css('background', `url('${shinyparam}')`))
+        }, "2000")
+        setTimeout( () => {
+                console.log(inforef)
+                console.log(inforef.current)
+            }, "3000")
+        let frontimg = await GetImage(props.paramPoke, 'front')
+        setImageUrl(frontimg)
     }
     
-    // }, [paramPoke]) wouldn't have this because if the paramPoke changed the whole webpage would change anyways.
-
     return (
-        <div className="Info-Wrap">
-            <button onClick={check} className="navBall" id="Pokeball"></button>
-            <InfoPokeImage paramPoke={paramPoke} setParamPoke={setParamPoke}/>
+        <div onMouseEnter={inspectcont} className="Info-Image-Container Column-Center">
+            {/* <p onMouseEnter={seeState}>  Pokemon Image Demo text</p> */}
+            {imageUrl.length ?
+            <img className="Sprite Quadruple"src={imageUrl} />
+            :
+            <p ref={inforef} className="Invisible" onClick={seeState}> no state no image</p>
+        }
+
         </div>
     )
 }
+export default InfoPokeImage
 
-export default InfoScreen
+                // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png
+                // InfoPokeImage.js:26 null
+                // InfoPokeImage.js:26 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png
+                // InfoPokeImage.js:26 null
+                // InfoPokeImage.js:26 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+                // InfoPokeImage.js:26 null
+                // InfoPokeImage.js:26 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png
+                // InfoPokeImage.js:26 null
+                // setImageBucket([...imageBucket, `${images[img]}`])
