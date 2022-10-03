c489ff4598a42cdc06aae283f7cc80d09f252fc7
Frank Collins <fwc3rd@gmail.com>
2022-10-02 23:01:38 -0400

    updated image tool to handle shiny image arg [11:00]
    i might let this image tool accept a 3rd arg that is the props.imageUrl 
    if the props.imageUrl has front_shiny return back_shiny
    
    props.imageUrl.includes(back_shiny) ? return front_shiny : ''

--- pokedex/src/components/utility/ImageTool.js
+++ pokedex/src/components/utility/ImageTool.js
@@ -1,28 +1,43 @@
 import APIcall from './pokeAPI'
  
 export default async function GetImage(pokemon, imgtype) {
+    console.log(pokemon)
     // 1) this tool will accept a "pokemon" parameter which would be either the pokemon.name or pokemon.id 
     // 2) use that string data to access pokeapi so we can return image data by accessing the poke data with out pure-function imgtype parameter. We will make a url that uses the pokemon parameter to fetch our data.
-    console.log("getImage tool! options are: [ front || back || shiny ]")
+    // console.log("getImage tool! options are: [ front || back || shiny ]")
     let keypiece;
     if (typeof pokemon === 'string')     keypiece = pokemon
     else if (typeof pokemon === 'number') keypiece = pokemon  // this number would be a returned string already. 
-    
+
     let dataForImage = await APIcall('specify', keypiece)
     let actualPokeAPI = dataForImage[0]
     let images = actualPokeAPI.sprites
 
+    if (imgtype === 'all') {
+        return images
+
+    } else if (imgtype === 'front' || imgtype === 'back') {
+        
+        const front = images.front_default     // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+        const back = images.back_default
+        if (imgtype == 'front') return front
+        if (imgtype == 'back') return `${back}`
+        
+    } else {
+        console.log("were in the else block")
+        const shiny1 = images.front_shiny
+        const shiny2 = images.back_shiny
+        // if (typeof imgtype == 'string') {
+            if (imgtype == 'frontshiny') {
+                return shiny1
+            } 
+            if (imgtype == 'backshiny') return `${shiny2}`
+        }
+    }
     
-    const front = images.front_default     // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
-    const back = images.back_default
-    const shiny = images.front_shiny
-    
-    if (imgtype == 'front') return front
-    if (imgtype == 'back') return back
-    if (imgtype == 'shiny') return shiny
 
     
-}
+// }
 
 // google chrome inspect ----> console.log(dataForImage)
 
