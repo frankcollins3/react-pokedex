ffe95268d770c8aad2684e01b1169a8722ba1358
Frank Collins <fwc3rd@gmail.com>
2022-10-02 23:57:09 -0400

    [BtnBar] set counterState [11:36pm]
    setting the counter state to increment upon clicking the 'shiny' button allows for [% modulo based conditional logic]
    2 components sharing state

--- pokedex/src/components/element/InfoBtnBar.js
+++ pokedex/src/components/element/InfoBtnBar.js
@@ -1,20 +1,36 @@
 import { useEffect, useState } from 'react';
 import GetImage from '../utility/ImageTool'
 function InfoBtnBar (props) {
+    const [shinyCounter, setShinyCounter] = useState(0)
+
     const frontImage = async () => {
         let frontimage = await GetImage(props.paramPoke, 'front')
-        console.log('frontimage')
-        console.log(frontimage)
-//         151
-// InfoBtnBar.js:6 frontimage
-// InfoBtnBar.js:7 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/151.png
+        await props.setImageUrl(frontimage)
+    }
+    const backImage = async () => {
+        let backimage = await GetImage(props.paramPoke, 'back')
+        await props.setImageUrl(backimage)
+    }
+    const shinyImage = async () => {
+        await setShinyCounter(shinyCounter + 1)
+            console.log('props.imageUrl')
+            console.log(props.imageUrl)
+            if (shinyCounter % 2 == 0) {
+                let shinyimage = await GetImage(props.paramPoke, 'frontshiny', props.imageUrl) 
+                await props.setImageUrl(shinyimage)            
+            } else {
+                let shinyimage = await GetImage(props.paramPoke, 'backshiny') 
+                await props.setImageUrl(shinyimage)            
+            }
+
+            // await props.setImageUrl(shinyimage)
     }
     return (
         // diamond icon for shiny pokemon
         <div className="Info-Bg-Bar">
         <button onClick={frontImage}  className="front"> </button>
-        <button className="back"> </button>
-        <button className="shiny"> </button>
+        <button onClick={backImage} className="back"> </button>
+        <button onClick={shinyImage} className="shiny"> </button>
         </div>
 
     )
