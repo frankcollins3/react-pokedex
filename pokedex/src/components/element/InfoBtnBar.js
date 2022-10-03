057e9f75a191f60ed30cea455ab55120010a2292
Frank Collins <fwc3rd@gmail.com>
2022-10-02 22:28:39 -0400

    [Info] buttons linked to parent props and state
    correction:
    paramPoke state is needed to invoke the getImage function.
    1) import GetImage
    2) invoke function using props which is the urlparams/pokemonID 
    [10:25pm]

--- pokedex/src/components/element/InfoBtnBar.js
+++ pokedex/src/components/element/InfoBtnBar.js
@@ -1,9 +1,18 @@
 import { useEffect, useState } from 'react';
+import GetImage from '../utility/ImageTool'
 function InfoBtnBar (props) {
+    const frontImage = async () => {
+        let frontimage = await GetImage(props.paramPoke, 'front')
+        console.log('frontimage')
+        console.log(frontimage)
+//         151
+// InfoBtnBar.js:6 frontimage
+// InfoBtnBar.js:7 https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/151.png
+    }
     return (
         // diamond icon for shiny pokemon
         <div className="Info-Bg-Bar">
-        <button className="front"> </button>
+        <button onClick={frontImage}  className="front"> </button>
         <button className="back"> </button>
         <button className="shiny"> </button>
         </div>
