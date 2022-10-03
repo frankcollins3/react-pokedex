617d46205469f26ab81cd7d0a87b13d1267e0253
Frank Collins <fwc3rd@gmail.com>
2022-10-03 10:40:33 -0400

    [InfoScreen] add blackPokedex background
    this page now looks like the original pokedex. This seems to be the mature looking page. 
    [10:40am]

--- pokedex/src/components/webpage/InfoScreen.js
+++ pokedex/src/components/webpage/InfoScreen.js
@@ -1,44 +1,53 @@
 import { $ } from 'react-jquery-plugin'
 import React, { useEffect, useState } from 'react'
 import CleanUrl from '../utility/CleanUrlTool'
+import InfoPokeImage from '../element/InfoPokeImage'
+import InfoBtnBar from '../element/InfoBtnBar'
+
+
 // this is a webpage that will import multiple components that have access to the same urlparam state.
 function InfoScreen() {
     const [paramPoke, setParamPoke] = useState('')
+    const [imageUrl, setImageUrl] = useState('')
+
+    
 
     useEffect( () => {
+        $('.Info-Wrap').parents().css('background-image', `url(${'/img/blackpokedex.jpeg'})`)  
+        $('.Info-Wrap').parents().css('background-size', 'cover')
+        // $('.Info-Wrap').parents().css('background-color', 'whitesmoke')  
         let jqdoc = $(document)
-        let win = window
-        console.log('jqdoc')
-        console.log(jqdoc)
+        let win = window        
         // * we are digging for that urlparam which is our /pokemon/:id  
         // * urlparam will be set when we get it from the secondArg in our pure function tool
-        //  second arg: `/pokemon/${cleanurl}`  LocationTool(null || undefined, `/pokemon/${cleanurl}`, imagenavigate)
-        console.log('win')
-        console.log(win)
+        //  second arg: `/pokemon/${cleanurl}`  LocationTool(null || undefined, `/pokemon/${cleanurl}`, imagenavigate)        
         const paramState = async () => {
-            let currloc = win.navigation.currentEntry.url
-            console.log('currloc')
-            console.log(currloc)
+            let currloc = win.navigation.currentEntry.url            
             let cleanint = await CleanUrl(currloc)
-            console.log(cleanint)
-            // await setParamPoke(cleanint)
+            setParamPoke(cleanint)
         }
-        paramState()
-
-        
+        paramState()        
     }, [])
     
-    const check = () => {
-        console.log('paramPoke')
-        console.log(paramPoke)
+    const check = () => {        
+        // paramPoke InfoScreen.js:32 1 parampoke now returns 1 because of our export function t
     }
     
     // }, [paramPoke]) wouldn't have this because if the paramPoke changed the whole webpage would change anyways.
 
     return (
         <div className="Info-Wrap">
-            <p> Hey !</p>
-            <button onClick={check} className="navBall" id="Pokeball"></button>
+            {/* <button onClick={check} className="navBall" id="Pokeball"></button> */}
+            <InfoPokeImage
+             paramPoke={paramPoke} setParamPoke={setParamPoke}
+             imageUrl={imageUrl} setImageUrl={setImageUrl}
+             />
+             <InfoBtnBar
+            paramPoke={paramPoke} setParamPoke={setParamPoke}
+            imageUrl={imageUrl} setImageUrl={setImageUrl}
+            // was thinking if this needs paramPoke state & i don't believe so. 
+            />
+            
         </div>
     )
 }
