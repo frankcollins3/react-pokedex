7dc8e24e08162cca0756b1e6540d2379f881bf86
Frank Collins <fwc3rd@gmail.com>
2022-10-02 22:16:57 -0400

    import some buttons that change access to state
    [10:12]

---
+++ pokedex/src/components/element/InfoBtnBar.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+function InfoBtnBar (props) {
+    return (
+        // diamond icon for shiny pokemon
+        <div className="Info-Bg-Bar">
+        <button className="front"> </button>
+        <button className="back"> </button>
+        <button className="shiny"> </button>
+        </div>
+
+    )
+}
+export default InfoBtnBar
