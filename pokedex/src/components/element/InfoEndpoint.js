198d1a8e9b27189da8796e303ea2499f93acfd29
Frank Collins <fwc3rd@gmail.com>
2022-10-03 20:46:42 -0400

    [InfoEndpoint] / [Endpoint] state based container
    button onClick=> {  setStateChange() ---> change display content of the container }:
    👍 this will also require the use of the APIcall() export function from /components/utility
    [8:44pm]

---
+++ pokedex/src/components/element/InfoEndpoint.js
@@ -0,0 +1,9 @@
+function InfoEndpoint () {
+    return (
+        <div className="Endpoint-Container Column-Center">
+            <p> endpoint test! click button and change container content state</p>
+        </div>
+    )
+}
+
+export default InfoEndpoint
