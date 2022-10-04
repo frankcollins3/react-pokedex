34db8f902deae4a8969df29f7da2ada95a385da9
Frank Collins <fwc3rd@gmail.com>
2022-10-03 21:58:29 -0400

    Rcontainer linked to button bar [9:50pm]
    clicking on button runs useEffect which is triggered to run when [dependency array] = props.endpointState
    endpoint state is 'string' state that will be the data around which the conditional logic will perform assertions upon the container.
    if props.endpoint === 'MOVES' : access our modularized export function that calls the Pokeapi and return 'MOVES'
    
    if props.endpoint === 'damage' : show api endpoint 'damage relations' 
    // water strong against fire, grass strong against water etc.

--- pokedex/src/components/element/InfoEndpoint.js
+++ pokedex/src/components/element/InfoEndpoint.js
@@ -1,4 +1,13 @@
-function InfoEndpoint () {
+import { useEffect, useState } from 'react'
+function InfoEndpoint (props) {
+
+    useEffect( () => {
+        console.log("infoendpoint useEffect endpoint state is being changed")
+    }, [props.endpointState])
+    console.log("in the info endpoint not the bar")
+    console.log('props.endpointState')
+    console.log(props.endpointState)
+    console.log("in the info endpoint not the bar")
     return (
         <div className="Endpoint-Container Column-Center">
             <p> endpoint test! click button and change container content state</p>
@@ -7,3 +16,21 @@ function InfoEndpoint () {
 }
 
 export default InfoEndpoint
+
+// moves function
+// InfoPokeImage.js:11 props
+// InfoPokeImage.js:11 props
+// InfoEndpoint.js:7 in the info endpoint not the bar
+// InfoEndpoint.js:8 props.endpointState
+// InfoEndpoint.js:9 moves
+// InfoEndpoint.js:10 in the info endpoint not the bar
+// InfoEndpoint.js:7 in the info endpoint not the bar
+// InfoEndpoint.js:8 props.endpointState
+// InfoEndpoint.js:9 moves
+// InfoEndpoint.js:10 in the info endpoint not the bar
+// InfoEndpointBar.js:2 props
+// InfoEndpointBar.js:3 {endpointState: 'moves', paramPoke: '78', setEndpointState: ƒ, setParamPoke: ƒ}
+// InfoEndpointBar.js:2 props
+// InfoEndpointBar.js:3 {endpointState: 'moves', paramPoke: '78', setEndpointState: ƒ, setParamPoke: ƒ}
+// InfoEndpoint.js:5 infoendpoint useEffect endpoint state is being changed
+// 2InfoEndpointBar.js:6 moves function
