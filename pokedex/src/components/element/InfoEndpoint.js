27f3a7741a48728116d030afe0be734ab719b7e7
Frank Collins <fwc3rd@gmail.com>
2022-10-05 17:28:43 -0400

    [InfoEndpoint] state populated
            // props.setEndpointState('') in the useEffect with empty dependency Array to load first and not !rerun
    
    {typeof props.endpointState === 'object' 
                // {typeof props.endpointState === 'object' 
                ?
                <ul>
                    <p> have our object </p> 
    
    using (typeof [state]) as our conditional upon which our rendering is based.
    
    [5:21pm]

--- pokedex/src/components/element/InfoEndpoint.js
+++ pokedex/src/components/element/InfoEndpoint.js
@@ -1,36 +1,30 @@
 import { useEffect, useState } from 'react'
 function InfoEndpoint (props) {
+    // console.log(props.endpointState)
+    
+    useEffect( () => {
+        props.setEndpointState('')
+    }, [])
 
     useEffect( () => {
-        console.log("infoendpoint useEffect endpoint state is being changed")
+        // console.log("infoendpoint useEffect endpoint state is being changed")
+        // console.log(props.endpointState)
+        // console.log(typeof props.endpointState)
     }, [props.endpointState])
-    console.log("in the info endpoint not the bar")
-    console.log('props.endpointState')
-    console.log(props.endpointState)
-    console.log("in the info endpoint not the bar")
+    
     return (
         <div className="Endpoint-Container Column-Center">
-            <p> endpoint test! click button and change container content state</p>
+            {/* {props.endpointState.ref === 'moves'  */}
+            {typeof props.endpointState === 'object' 
+            // {typeof props.endpointState === 'object' 
+            ?
+            <ul>
+                <p> have our object </p>                    
+            </ul>
+            :
+            <p> not equal to null </p>
+        }           {/* map end */}
         </div>
     )
 }
-
 export default InfoEndpoint
-
-// moves function
-// InfoPokeImage.js:11 props
-// InfoPokeImage.js:11 props
-// InfoEndpoint.js:7 in the info endpoint not the bar
-// InfoEndpoint.js:8 props.endpointState
-// InfoEndpoint.js:9 moves
-// InfoEndpoint.js:10 in the info endpoint not the bar
-// InfoEndpoint.js:7 in the info endpoint not the bar
-// InfoEndpoint.js:8 props.endpointState
-// InfoEndpoint.js:9 moves
-// InfoEndpoint.js:10 in the info endpoint not the bar
-// InfoEndpointBar.js:2 props
-// InfoEndpointBar.js:3 {endpointState: 'moves', paramPoke: '78', setEndpointState: ƒ, setParamPoke: ƒ}
-// InfoEndpointBar.js:2 props
-// InfoEndpointBar.js:3 {endpointState: 'moves', paramPoke: '78', setEndpointState: ƒ, setParamPoke: ƒ}
-// InfoEndpoint.js:5 infoendpoint useEffect endpoint state is being changed
-// 2InfoEndpointBar.js:6 moves function
