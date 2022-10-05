85f5b8d5e4a2be8f18da2a6b679ce0adc5ef21b1
Frank Collins <fwc3rd@gmail.com>
2022-10-04 23:08:02 -0400

    [Endpointbar] [] || '' || { key: '', bucket: [] }
     await props.setEndpointState({ ref: 'moves', bucket: movebucket})
    
    1) i wanted to use the movebucket as state to render onto a container.
    2) i did a minimumviable test with setState('moves') // after doing so: realized why its easier to reference string.
    3) created object so there's a simple key/ref to check just to hook things up and not lose ease of 'moves', while still using the array is the source of data.
    
    
    [10:58pm]

--- pokedex/src/components/element/InfoEndpointBar.js
+++ pokedex/src/components/element/InfoEndpointBar.js
@@ -1,38 +1,40 @@
 import CleanData from '../utility/CleanData'
 import { useEffect, useState } from 'react'
 function EndpointBar (props) {    
+    // console.log('props infoendpointbar')
+    // console.log(props)
     let movesbucket = new Array() || []
     
     let urlpokemon = props.paramPoke
 
     const getMoves = async () => {
        let movebucket = await CleanData(urlpokemon, 'moves')
-       console.log('movebucket')
-       console.log(movebucket)
-              
+    //    await props.setEndpointState(movebucket)
+    //    await props.setEndpointState('moves')
+       await props.setEndpointState({ ref: 'moves', bucket: movebucket})
+
     }
-    getMoves()
+    // getMoves()
 
     // will need access to parent state props.paramPoke
     const moves = async () => {
-        console.log("moves function")
-        // props.endpointState 
-        await props.setEndpointState('moves')   
+
     }
 
     const checkState = () => {
-        console.log(props.endpointState)
+        console.log("check state")
+        console.log(props.endpointState)        
     }
     return (
         <div className="Info-Endpoint-Bar Row-Center">
 
-        <button onClick={moves}></button>
+        <button onClick={getMoves}></button>
 
         <button>
 
         </button>
 
-        <button onClick={checkState}>
+        <button className="navBall" id="Ultraball" onClick={checkState}>
 
         </button>
         </div>
