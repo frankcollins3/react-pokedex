e93f22e53b4e58227ade9736fba3c91216607fbd
Frank Collins <fwc3rd@gmail.com>
2022-10-05 13:30:26 -0400

    [InfoBar] double-check data against doubleBucket
    bucketBucket
    
    (3) [Array(0), Array(1), Array(0)]
    []
    Array(1) 
    Array(2)
    {name: 'own-tempo', url: 'https://pokeapi.co/api/v2/ability/20/'}
    {name: 'oblivious', url: 'https://pokeapi.co/api/v2/ability/12/'}
    []
    
     
    3
    
    
    
    [1:28]

--- pokedex/src/components/element/InfoEndpointBar.js
+++ pokedex/src/components/element/InfoEndpointBar.js
@@ -3,31 +3,53 @@ import { useEffect, useState } from 'react'
 function EndpointBar (props) {    
     // console.log('props infoendpointbar')
     // console.log(props)
-    let movesbucket = new Array() || []
-    
+    let movesBucket = new Array() || []
+    let abilitiesBucket = [] 
+    let dmgRelationsBucket = new Array()
+
+    let bucketBucket = [
+        movesBucket, 
+        abilitiesBucket,
+        dmgRelationsBucket
+    ]    
+
     let urlpokemon = props.paramPoke
 
     const getMoves = async () => {
        let movebucket = await CleanData(urlpokemon, 'moves')       
+       movesBucket.push(movebucket)       
     }
-    // getMoves()
+    
     
     const dmgrelation = async () => {
-        let dmgbucket = await CleanData(urlpokemon, 'damage')
-        return dmgbucket
+        let dmgbucket = await CleanData(urlpokemon, 'damage')        
+    }
+
+    const getabilities = async () => {
+        let abilitybucket = await CleanData(urlpokemon, 'ability')
+        abilitiesBucket.push(abilitybucket)
+        // (2) [{…}, {…}]
+        // {name: 'own-tempo', url: 'https://pokeapi.co/api/v2/ability/20/'}
+        // {name: 'oblivious', url: 'https://pokeapi.co/api/v2/ability/12/'}
+
+
+        // abilitybucket.length > 1 ?  :
     }
 
     const checkState = () => {
-        console.log("check state")
-        console.log(props.endpointState)  
-        console.log(typeof props.endpointState)      
+        // console.log("check state")
+        // console.log(props.endpointState)  
+        // console.log(typeof props.endpointState)      
+        console.log(bucketBucket)
     }
     return (
         <div className="Info-Endpoint-Bar Row-Center">
 
-        <button onClick={getMoves}></button>
+        <button onClick={getabilities}> </button>
+        {/* <button  onClick={getMoves}></button> */}
 
-        <button className="navBall" id="Greatball" onClick={dmgrelation}>
+        <button className="navBall" id="Greatball" onClick={checkState}>
+        {/* <button className="navBall" id="Greatball" onClick={dmgrelation}> */}
 
         </button>
 
