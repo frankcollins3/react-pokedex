dc2317976f86e4df56c1daf914f26af96587d956
Frank Collins <fwc3rd@gmail.com>
2022-10-05 11:26:06 -0400

    [endpoint] add params and receive data
    modified url a few times with [concatenation += params]
    [11:25am]

--- pokedex/src/components/utility/pokeEndpoint.js
+++ pokedex/src/components/utility/pokeEndpoint.js
@@ -1,14 +1,20 @@
-let url = `https://pokeapi.co/api/v2/pokemon/`
 import Axios from 'axios'
-export default async function pokeEndpoint (pokemon, endpoint) {
+
+let url = `https://pokeapi.co/api/v2/pokemon/`
+export default async function pokeEndpoint (pokemon, key, endpoint) {
 // ability test
 if (typeof endpoint === 'string' && typeof pokemon === 'string' || 'number') {
     console.log("we are in the main conditional separating error producing user entries as func(params)")
     
-        let newurl = url += endpoint
-        let endpointcheck = await Axios.get(newurl)
-        console.log('endpointcheck')
-        console.log(endpointcheck)
+
+        if (endpoint === 'ability') {
+            console.log("endpoint === ability ")
+            let newurl = url += key += endpoint    // 
+            let endpointcheck = await Axios.get(newurl)
+            console.log('endpointcheck')
+            console.log(endpointcheck)
+        }
+
         
     }
 }
