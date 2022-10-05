cfcf52f26c128a92c35a36fa23e4a5b03c6ff9db
Frank Collins <fwc3rd@gmail.com>
2022-10-05 12:01:10 -0400

    [Endpoint]
       // interesting. you can't use string interpolation within concatenation. (as it is its own vehicle of concatenation. still a little surprised.)
                // const newurl = url += `${endpoint}/` += key
                // forgot the slash https://pokeapi.co/api/v2/[ability20] no slash.
    
    this code could be seen as a sports-suicide-drill of just running back and forth for sake of running back and forth. 
    👎  I didn't need the CleanUrl tool, but it works!
    
    👍 using the CleanUrl and taking /pokeapi/ability/id and turning it into /id requires us to do a 3 part concatenation using params on other side.
    I've never done more than a basic string-to-string concatenation. 
    
    It's Nice to put more weight on the bar so we can do a [3 string .concat()]   || 2 string concat
    
    as new front end instructor:
    i'm referencing how easy it is for a new coder to be so confused on concatenation and for what uses and why we would attach 1 string to another
    
    
    FUN FACT: 
    i was surprised to see you be able to use jquery targeting in reacts rendering return statement.
    im almost more surprised that you cant use string interpolation in the middle of concatenation. I know interpolation is concatenation but still.
    
    // const newurl = url += `${endpoint}/` += key
    this code does not work.
    
    
    
    [11:37]

--- pokedex/src/components/utility/pokeEndpoint.js
+++ pokedex/src/components/utility/pokeEndpoint.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
-let url = `https://pokeapi.co/api/v2/pokemon/`
-export default async function pokeEndpoint (pokemon, key, endpoint) {
+let url = `https://pokeapi.co/api/v2/`
+export default async function pokeEndpoint (key, endpoint) {
 // ability test
 if (typeof endpoint === 'string' && typeof pokemon === 'string' || 'number') {
     console.log("we are in the main conditional separating error producing user entries as func(params)")
@@ -9,7 +9,15 @@ if (typeof endpoint === 'string' && typeof pokemon === 'string' || 'number') {
 
         if (endpoint === 'ability') {
             console.log("endpoint === ability ")
-            let newurl = url += key += endpoint    // 
+            // let newurl = url += key += endpoint    // 
+            let nonbrokenkey = `/${key}`
+            const newurl = url += endpoint += nonbrokenkey 
+         
+               // interesting. you can't use string interpolation within concatenation. (as it is its own vehicle of concatenation. still a little surprised.)
+            // const newurl = url += `${endpoint}/` += key
+            // forgot the slash https://pokeapi.co/api/v2/[ability20] no slash.
+            
+            
             let endpointcheck = await Axios.get(newurl)
             console.log('endpointcheck')
             console.log(endpointcheck)
