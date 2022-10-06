a9d353621423a0756470f70fe989bd3b96b55d12
Frank Collins <fwc3rd@gmail.com>
2022-10-05 23:38:07 -0400

    [CreateElem] all set up. fn not working yet but not needed
    React.createElement || $(document).createElement()
    
    [11:34pm]

--- pokedex/src/components/utility/CreateElem.js
+++ pokedex/src/components/utility/CreateElem.js
@@ -1,22 +1,35 @@
+import React from 'react';
 import { $ } from 'react-jquery-plugin'
-export default async function CreateElem(elem, nameofclass, id, fnfunc ) {
+import ClassAction from './ClassAction'
+import attrTool from './attrTool'
+
+export default async function CreateElem(elem, nameofclass, idtoset, fnfunc ) {
+    console.groupCollapsed()
     console.log("in the createElem")
     console.log(elem)
     console.log(nameofclass)
-    console.log(id)
+    console.log(idtoset)
     console.log(fnfunc)
+    console.groupEnd()
+    let newelement = await React.createElement(elem)
+    // newelement.props.className = nameofclass
+    
+    await ClassAction('add', $(newelement), nameofclass) 
+    await attrTool($('.nameofclass'), 'id', idtoset)
+    await console.log('newelement')
+    await console.log(newelement)
+
+    $.fn.changeAgain = fnfunc(elem)
+    setTimeout($(elem).changeAgain()) // see if $.fn. is working.
+    
+    // * $.fn.changeAgain = fnfunc()
+    // check out the code a line below. see (event)? if fn.changeAgain = fnfunc() empty parameter we get an undefined error
+    // $.fn.changeAgain = fnfunc(elem) the word elem has to be there and it makes sense if you look at both lines of code a little closely.
+    // * const changeitem = (event) =>  $(event.target).css('border', '10px dotted purple') 
+    
+
+
 }
 
 
 
-// $.fn.someFunction = function(){
-//     this.each(function(){
-//         if (false == $(this).hasClass("someElement")) {
-//             return; 
-//         }
-//         $(this).append(" some element has been modified");
-//         return $(this); // support chaining
-//     });    
-// };
-// $('.someElement').someFunction();       
-// now you can call your function like this
