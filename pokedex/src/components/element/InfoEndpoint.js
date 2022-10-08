d432df4b85e1188660a805250b788bf454a961af
Frank Collins <fwc3rd@gmail.com>
2022-10-07 20:02:39 -0400

    [InfoEndpoint] scroll up & down [6:22pm]
    || 
    increment && decrement the state that the conditionals to render the data are based on

--- pokedex/src/components/element/InfoEndpoint.js
+++ pokedex/src/components/element/InfoEndpoint.js
@@ -14,37 +14,75 @@ function InfoEndpoint (props) {
         // console.log(typeof props.endpointState)
     }, [props.endpointState])
 
+    // find where theres a distinction in up or down wheel. 
+
+    //*  */ this is the object to access that will give us our distinction. NON JQUERY!
+// view 
+// Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+// wheelDelta
+// -120
+// wheelDeltaX
+// 0
+// wheelDeltaY
+// -120
+
     let moveIndexIncrement = async (event) => {
-        // find where theres a distinction in up or down wheel. 
-        // let int = props.moveIndex + 1
-        // console.log(event)
-
-
+        console.log(event)
+        let wheeldelta = event.nativeEvent.wheelDelta
+        let increment = props.moveIndex + 1
+        let decrement = props.moveIndex - 1
+        
         let movedatalength = props.endpointState[1].length
+
+
         if (props.moveIndex < movedatalength) { // props.endpointState[1][props.moveIndex]
-            await props.setMoveIndex(props.moveIndex + 1) 
-            await props.setEndpoint(props.endpointState[1][props.moveIndex].move.name) 
+            if (wheeldelta > 1) {
+                console.info('LESS than 0')
+                await props.setMoveIndex(decrement) 
+                // await props.setMoveIndex(props.moveIndex + 1) 
+                await props.setEndpoint(props.endpointState[1][props.moveIndex].move.name) 
+            }
+            else if (wheeldelta < -1) {
+                console.info('MORE!!!!!  than 0')
+                await props.setMoveIndex(increment) 
+                await props.setEndpoint(props.endpointState[1][props.moveIndex].move.name) 
+            }
+            // await props.setMoveIndex(props.moveIndex + 1) 
+
         } else {
             await props.setMoveIndex(props.moveIndex - movedatalength) // almost wrote props.moveIndex.length
         }
-        // console.log('movedatalength')
-        // console.log(movedatalength)
-        // await console.log(props.moveIndex)
+        console.log('movedatalength')
+        console.log(movedatalength)
+        await console.log(props.moveIndex)
     }
 
-    $('.Endpoint-Container').on('wheel', (event) => {
-        console.log('jquery event guys')
-        console.log(event)
-        let D = event.originalEvent.deltaX  
-        let delta = event.originalEvent.wheelDelta
-        // got it. this will be either 120 or - 120 we can now run increment or decrement code
-        console.log('delta')
-        console.log(delta)        
-    })
+    // $('.Endpoint-Container').on('wheel', async (event) => {
+    //     console.log('jquery event guys')
+    //     console.log(event)
+    //     let D = event.originalEvent.deltaX  
+    //     let delta = event.originalEvent.wheelDelta
+    //     if (delta > 1) {
+    //         console.log("delta is greater than 1")
+    //         await props.setMoveIndex(props.moveIndex + 1) 
+    //         await props.setEndpoint(props.endpointState[1][props.moveIndex].move.name)
+    //         console.log('delta')
+    //         console.log(delta)        
+    //     } else if (delta < 1) {
+    //         console.log("delta is less than 1")
+    //         await props.setMoveIndex(props.moveIndex -1) 
+    //         await props.setEndpoint(props.endpointState[1][props.moveIndex].move.name)
+    //         // got it. this will be either 120 or - 120 we can now run increment or decrement code
+    //         console.log('delta')
+    //         console.log(delta)        
+    //     }
+    // })
     
     return (
         <div 
-        onClick={moveIndexIncrement} onWheel={moveIndexIncrement} onScroll={moveIndexIncrement}
+        onClick={moveIndexIncrement}
+         onWheel={moveIndexIncrement} 
+         onScroll={moveIndexIncrement}
         className="Endpoint-Container Column-Center">
             {/* {props.endpointState.ref === 'moves'  */}
             {typeof props.endpointState === 'object' 
