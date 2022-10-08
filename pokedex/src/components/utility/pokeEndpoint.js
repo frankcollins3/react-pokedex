2190da0a3cf65609b6b6a85aa97ef251d4038256
Frank Collins <fwc3rd@gmail.com>
2022-10-08 18:21:00 -0400

    util: return [data.data] not {data} [6:30pm]

--- pokedex/src/components/utility/pokeEndpoint.js
+++ pokedex/src/components/utility/pokeEndpoint.js
@@ -4,26 +4,16 @@ let url = `https://pokeapi.co/api/v2/`
 export default async function pokeEndpoint (key, endpoint) {
 // ability test
 if (typeof endpoint === 'string' && typeof pokemon === 'string' || 'number') {
-    console.log("we are in the main conditional separating error producing user entries as func(params)")
-    
-
-        if (endpoint === 'ability') {
-            console.log("endpoint === ability ")
+        if (endpoint === 'ability') {            
+            const accessEndpoint = async () => {
+                let endpointcheck = await Axios.get(key) // key is now url instead of chopped id from string                
+                console.log('endpointcheck')
+                console.log(endpointcheck)
+                return endpointcheck.data
+            }
+            return accessEndpoint()
+        }}}
             // let newurl = url += key += endpoint    // 
             // let nonbrokenkey = `/${key}`
             // const newurl = url += endpoint += nonbrokenkey 
             // let endpointcheck = await Axios.get(newurl)
-            // this code above allows for 1 good .get() call through Axios but then double-appends /pokemon/ability/20/ability/20 for the second call
-            
-
-
-            const accessEndpoint = async () => {
-                let endpointcheck = await Axios.get(key) // key is now url instead of chopped id from string                              
-                return endpointcheck
-            }
-            return accessEndpoint()
-        }
-
-        
-    }
-}
