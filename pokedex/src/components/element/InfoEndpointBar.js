4f69a91508f420affceadf7adabaa93246f08080
Frank Collins <fwc3rd@gmail.com>
2022-10-09 11:24:10 -0400

    [InfoEndpointBar] [0] || obj.property [11:23am]
    array based indexing vs obj dot notation.
    InfoEndpointBar.js:158 console.groupCollapsed
    
    InfoEndpointBar.js:159 [{…}]0: {name: 'fighting', url: 'https://pokeapi.co/api/v2/type/2/'}length: 1[[Prototype]]: Array(0)
    InfoEndpointBar.js:160 undefined
    InfoEndpointBar.js:161 undefined
    InfoEndpointBar.js:162 undefined
    InfoEndpointBar.js:163 undefined
    InfoEndpointBar.js:164 undefined

--- pokedex/src/components/element/InfoEndpointBar.js
+++ pokedex/src/components/element/InfoEndpointBar.js
@@ -3,10 +3,12 @@ import { useEffect, useState, useRef } from 'react'
 import { $ } from 'react-jquery-plugin'
 import myCSS from '../utility/CSStool'
 import toggleHideShow from '../utility/hideShow'
+
+
 // import CreateElem from '../utility/CreateElem'
 function EndpointBar (props) {        
-    console.log(props)
-    console.log(props.endpoint)
+    // console.log(props)
+    // console.log(props.endpoint)
     let movesBucket = new Array() || []
     let abilitiesBucket = [] 
     let dmgRelationsBucket = new Array()
@@ -47,8 +49,8 @@ function EndpointBar (props) {
          }
          const fillContainer = async () => {
             let movebucket = await CleanData(urlpokemon, 'moves')
-            let abilitybucket = await CleanData(urlpokemon, 'ability')
-            let dmgbucket = await CleanData(urlpokemon, 'damage')                    
+            let abilitybucket = await CleanData(urlpokemon, 'ability')        
+            let dmgbucket = await CleanData(urlpokemon, 'damage')                                
             let bucketofbuckets = [...props.endpointState]
             bucketofbuckets.push(abilitybucket, movebucket, dmgbucket)
             await props.setEndpointState(bucketofbuckets)        
@@ -102,51 +104,30 @@ function EndpointBar (props) {
 
         if (idvalue === 'ability') {
                 let abilities = props.endpointState[0]
-                let abilitylength = abilities.length
-                // let ability1 = abilities[0]
-                // let ability2 = abilities[1]
-                // console.log(ability1)
-                // console.log(ability2)
-                console.log('abilities')
-                console.log(abilities)
-                
+                let abilitylength = abilities.length                
+                                
                 try {
                     if (abilitylength) {
                         let currentstate = props.endpoint
-
+                        let headerstate = props.headerText
                         props.setHeaderText('abilities:')
                         await props.setClickedGloves('false')
                         myCSS(target.siblings(), 'opacity', '0.1')
-                        myCSS(target, 'opacity', '1.0')
-                        // myCSS(target, 'order', 1)
-                        let abilitydata = await CleanData(props.paramPoke, 'ability')   
-                        console.log(abilitydata)
-
+                        myCSS(target, 'opacity', '1.0')                        
+                        let abilitydata = await CleanData(props.paramPoke, 'ability')                           
                         let ability1 = abilitydata[0]
                         let name1 = ability1.name
                         let text1 = ability1.text
-
                         let ability2 = abilitydata[1]
                         let name2 = ability2.name
-                        let text2 = ability2.text
-
-                        console.log(name1)
-                        console.log(name2)
-                        console.log(text1)
-                        console.log(text2)
-                        
-                        
-                        
-                        if (currentstate === ability1.name) {
-                            console.log("ability1 === currentstate")
-                            console.log(currentstate)
+                        let text2 = ability2.text                                                                                             
+                        if (headerstate === ability1.name) {                            
+                            props.setHeaderText(ability2.name)
+                            props.setEndpoint(ability2.text)                        
                         }
-                        else if (currentstate === ability2.name) {
-                            console.log("currentsate === ability2")
-                        } else {
-                            console.log("currentstate doesn't equal either ability so value(s) set")
-                       
-                            // props.setHeaderText(ability1)
+                        else {
+                            props.setHeaderText(ability1.name)
+                            props.setEndpoint(ability1.text)                                                        
                         }
                     }   
                 }
@@ -157,27 +138,46 @@ function EndpointBar (props) {
                     // location.href = 'views/404page' 
                 }
 
-
-
-
-
-                console.info('ability warn')
-                console.log(props.endpointState[0])
-                // props.setEndpoint(props.endpointState[0][0].name)             
-                // props.setEndpoint(props.endpointState[1][0].move.name) 
-                // * nice 15 minute error. didn't invoke this function and wasn't using [setFakeDom == ''] aka wasn't triggering useEffect so there were no changes being asserted and it was clicking why                
-           
         }  else checkState()    
-            if (idvalue === 'damage') { 
-            props.setHeaderText('damage:')
-            await props.setClickedGloves('false')
-            myCSS(target.siblings(), 'opacity', '0.1')
-            myCSS(target, 'opacity', '1.0')
-            // myCSS(target, 'order', 1)
-            props.setEndpoint('damage') 
-             checkState()
-        } else { return  }
+            try {
+                if (typeof idvalue === 'string') {
+                    if (idvalue === 'damage') { 
+                        console.log(props.endpointState[2])
+                        let dmgendpoints = props.endpointState[2]
+                        console.log('dmgendpoints')
+                        console.log(dmgendpoints)
+                        // let dblFrom = dmgendpoints[0]
+                        let dblFrom = dmgendpoints.double_damage_from
+                        let dblTo = dmgendpoints[1]
 
+                        let halfFrom = dmgendpoints[2]
+                        let halfTo = dmgendpoints[3]
+
+                        let noneFrom = dmgendpoints[4]
+                        let noneTo = dmgendpoints[5]
+                        console.groupCollapsed()
+                        console.log(dblFrom)
+                        console.log(dblTo)
+                        console.log(halfFrom)
+                        console.log(halfTo)
+                        console.log(noneFrom)
+                        console.log(noneTo)
+                        console.groupEnd()
+
+                         
+
+                        console.log(typeof idvalue)       // log(...data: any[]): void            // (...data : any[]) can see the TS syntax type for .log()                                        
+                        props.setHeaderText('damage:')
+                        await props.setClickedGloves('false')
+                        myCSS(target.siblings(), 'opacity', '0.1')
+                        myCSS(target, 'opacity', '1.0')
+                        // myCSS(target, 'order', 1)
+                        props.setEndpoint('damage') 
+                        checkState()
+                    }
+            } else { return  }
+            }
+            catch (err) { console.log(err) }             // catch (err)  console.log(err)  kind of surprised you can't 1 line this out
     }
     return (
         // <div onClick={addState} className="Info-Endpoint-Bar Row-Center">
