e1afaef8a8060b7a866d0f071b3dd244322f5967
Frank Collins <fwc3rd@gmail.com>
2022-10-10 17:45:13 -0400

    [InfoBar] updated tool to return data [5:44pm]
    it also evaluates type to make sure were not getting integers or numbers to run a loop upon.

--- pokedex/src/components/element/InfoEndpointBar.js
+++ pokedex/src/components/element/InfoEndpointBar.js
@@ -2,9 +2,11 @@ import CleanData from '../utility/CleanData'
 import { useEffect, useState, useRef } from 'react'
 import { $ } from 'react-jquery-plugin'
 import myCSS from '../utility/CSStool'
+import forInTool from '../utility/damageForInLoop'
 import toggleHideShow from '../utility/hideShow'
-import { createImportSpecifier } from 'typescript'
-
+let thisobject = { name: 'myself', body: 'autobody', mph: 100}
+// forInTool(thisobject)
+// let newobj = await forInTool(thisobject)
 
 // import CreateElem from '../utility/CreateElem'
 function EndpointBar (props) {        
@@ -112,8 +114,7 @@ function EndpointBar (props) {
 
         if (idvalue === 'ability') {
                 let abilities = props.endpointState[0]
-                let abilitylength = abilities.length                
-                                
+                let abilitylength = abilities.length                                                
                 try {
                     if (abilitylength) {
                         let currentstate = props.endpoint
@@ -156,7 +157,7 @@ function EndpointBar (props) {
                         myCSS(target.siblings(), 'opacity', '0.1')
                         myCSS(target, 'opacity', '1.0')
 
-                        const damageSubmit = () => {
+                        const damageSubmit = async () => {
                             let dblFrom = dmgendpoints.double_damage_from
                             // let dblFrom = {...dmgendpoints.double_damage_from}
                             let dblTo = dmgendpoints.double_damage_to
@@ -164,24 +165,28 @@ function EndpointBar (props) {
                             let halfTo = dmgendpoints.half_damage_to
                             let noneFrom = dmgendpoints.no_damage_from
                             let noneTo = dmgendpoints.no_damage_to
-                            if (damageRelations) {
+                            if (damageRelations.length < 2) {
                                 console.log('there are contents')
+                                console.log("this is who i am now")
                             } else {
                                     const valuebucket = []
                                     const damagebucket = [dblFrom, dblTo, halfFrom, halfTo, noneFrom, noneTo]      
 
-                                // console.log(dblFrom[1]) works but can't iterate through dblFrom.map() or from.forEach()=>
 
-                                for (const endpoint in dblFrom) {
-                                    // console.log(endpoint)
-                                    // console.log(dblFrom)
-                                    // console.log(dblFrom[endpoint])
+                                // console.log(dblFrom[1]) works but can't iterate through dblFrom.map() or from.forEach()=>
+                                for (const endpoint in dblFrom) {                                    
                                     let obj = dblFrom[endpoint]
                                     setDblFrom([...dblFrom, obj])
                                     console.log(obj.name)
                                 }
+                                console.log("we have added and were here now")
+                                let fromData = await forInTool(dblFrom)
+                                // just setting this variable invokes this function
+                                
+                                // console.log('fromData')
+                                // console.log(fromData)
 
-                                                                                                        
+                                                                                                                   
                                     damagebucket.forEach( (bucketitem, idx) => {                                        
                                         for (const item in bucketitem) {                                            
                                             let newitem = bucketitem[item]
@@ -191,16 +196,14 @@ function EndpointBar (props) {
                                                     console.log(`idx: ${idx} item: ${item}`)
                                                     console.log(newitem)
                                                     console.groupEnd()
-                                                }
-                                            }                                            
-                                        }
+                                                }}}
                                     })
                                     return damagebucket
                                     // * loop through the endpoints first and push values to an array
-
                             }
                         }
                         await setDamageRelations(damageSubmit())                                
+                        // damageSubmit()
                   
                         let headerKeys = await Object.keys(dmgendpoints)
                         setDamageIndex(damageIndex + 1) 
@@ -215,11 +218,16 @@ function EndpointBar (props) {
             } else { return  }
             }
             catch (err) { console.log(err) }             // catch (err)  console.log(err)  kind of surprised you can't 1 line this out
+
+            let checkout = () => {
+                console.log('dblFrom')
+                console.log(dblFrom)
+            }
     }
     return (
         // <div onClick={addState} className="Info-Endpoint-Bar Row-Center">
         
-        <div onMouseEnter={()=> console.log(damageRelations)} className="Info-Endpoint-Bar Row-Center">
+        <div onMouseEnter={()=> console.log(dblFrom)} className="Info-Endpoint-Bar Row-Center">
          {/* <div onMouseEnter={mouseMoved == 'false' ? addState : checkState} className="Info-Endpoint-Bar Row-Center">  */}
         {mouseMoved === 'true' 
         ?
