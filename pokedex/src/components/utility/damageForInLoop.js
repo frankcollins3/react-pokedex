791b9988a950ebd65f54de76152ed83bec22c7e6
Frank Collins <fwc3rd@gmail.com>
2022-10-10 17:50:43 -0400

    [forInLoop] invoked return functions wrong [5:49pm]
    this pure function takes in an object parameter which is going to be looped upon by 
    for (data in functionParameterObject)
    
    👍 now the data returns the obj.name

--- pokedex/src/components/utility/damageForInLoop.js
+++ pokedex/src/components/utility/damageForInLoop.js
@@ -1,12 +1,30 @@
 export default async function forInTool (obj) {
     let bucket = [] || new Array() 
+    console.log('obj')
+    console.log(obj)
+    // if (typeof obj === 'object') {
+        console.log("we have evaluated type of our input")
+        const runTheLoop = async () => {
+            for (const listitem in obj) {
+                let newobj = obj[listitem]
+                console.log('obj')
+                console.log(newobj)
+                bucket.push(newobj.name)
+            }    
+        }
 
-    for (const listitem in obj) {
-        let obj = obj[listitem]
-        console.log('obj')
-        console.log(obj)
-        bucket.push(obj)
-    }    
+        const returnBucket = async () => {
+            return bucket
+        }
+
+        const doubleFunctions = async () => {
+            await runTheLoop()
+            return returnBucket()
+        }
+        return doubleFunctions()
+
+
+    // }
 }
 // for (const endpoint in dblFrom) {                                    
 //     let obj = dblFrom[endpoint]
