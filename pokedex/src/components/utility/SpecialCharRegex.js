944ecfbfcf6fe6d6e385692f7519aa30229c1984
Frank Collins <fwc3rd@gmail.com>
2022-10-12 19:24:38 -0400

    SpecialChar() [7:08pm]
    This works!
     escapechar is a 1-and-only function parameter accepting a string argument and returning a regex that cleans that set of .char(), washing it of the specified char.
    const newstr = str.split(`${escapechar}`).join('').replace(/[\,]/g, '') 
    
    the expression returns:
    'double damage from'

--- pokedex/src/components/utility/SpecialCharRegex.js
+++ pokedex/src/components/utility/SpecialCharRegex.js
@@ -2,23 +2,23 @@
 export default async function SpecialCharRegex (str, escapechar) {
     if (typeof str === 'string' && typeof str === 'string') {
         console.log(str)
-        
-
-        let stringdata = 'ilikecheesex10' // for a test regex that didn't work. 
-
+    
         let regexobject = new RegExp(`${escapechar}`)
         let checkForCharacters = regexobject.exec(str)
         console.log('checkForCharacters')
         console.log(checkForCharacters)
 
-        const newstr = str.split(`${escapechar}`)
+        const newstr = str.split(`${escapechar}`).join(' ').replace(/[\,]/g, '') // *** [double damage from]!! got it. 
+        // this code above works but with an additional comma-removing regex set onto the expression. If we can clean this up and let the comma go in str.split(escapechar) then we can allow the pure-func tool to accept an additional argument. 
+
+        // const newstr = str.split(`${escapechar}`).join('').replace(/[\,]/g, '') output: doubledamagefrom 
+        // and without the second replace regex (which breaks point of this utility anyways) double,damage,from it turns all the white space into commas.
+
+        // 1st attempt double,damage,from(`${escapechar}`)
+
         console.log('newstr')
         console.log(newstr)
         // * newstr returns the str 
-        
-
-        
-  
         
         // let newstr = str.replace(/[\${escapechar}]/g, )                
         return newstr
