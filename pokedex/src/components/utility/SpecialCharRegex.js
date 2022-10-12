c410251d40c69eb579eaa91746907cee53ff2418
Frank Collins <fwc3rd@gmail.com>
2022-10-12 18:54:56 -0400

    [specialChar] new RegExp() that uses func param [7:00pm]
    let cleanexample = await SpecialCharRegex(headerKeys[0], '_')
    thats not a face above as the second argument
    
           // thats not a face below as the first argument. 
    ['_', index: 6, input: 'double_damage_from', groups: undefined]0: "_"groups: undefinedindex: 6input: "double_damage_from"length: 1[[Prototype]]: Array(0)
    SpecialCharRegex.js:15 newstr
    SpecialCharRegex.js:16 (3) ['double', 'damage', 'from']
    InfoEndpointBar.js:182 cleanexample
    InfoEndpointBar.js:183 (3) ['double', 'damage', 'from']
    
    we are escaping the white char

--- pokedex/src/components/utility/SpecialCharRegex.js
+++ pokedex/src/components/utility/SpecialCharRegex.js
@@ -3,19 +3,19 @@ export default async function SpecialCharRegex (str, escapechar) {
     if (typeof str === 'string' && typeof str === 'string') {
         console.log(str)
         
-        let newstr = str.replace(/[\_]/g, '')   
 
-        const regex = /\escapechar/
-        let newerstring = new RegExp(`/${escapechar}/`)
-        let paramMatch = str.replace(newerstring)
-        console.log('paramMatch')
-        console.log(paramMatch)
+        let stringdata = 'ilikecheesex10'
 
+        let regexobject = new RegExp(`${escapechar}`)
+        let checkForCharacters = regexobject.exec(str)
+        console.log('checkForCharacters')
+        console.log(checkForCharacters)
+
+        const newstr = str.split(`${escapechar}`)
         console.log('newstr')
         console.log(newstr)
-
-        
-        
+        // * newstr returns the str 
+       
         // let newstr = str.replace(/[\${escapechar}]/g, )                
         return newstr
         // as i get to this point right here i suspect this could need whitespace-escaping as well.
@@ -23,5 +23,3 @@ export default async function SpecialCharRegex (str, escapechar) {
 }
 
 
-// https://stackoverflow.com/questions/1695633/how-to-pass-a-variable-into-regex-in-jquery-javascript
-// hmm
