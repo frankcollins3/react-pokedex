78809788dd9733ccc619c8ff4087586e06dd408d
Frank Collins <fwc3rd@gmail.com>
2022-10-12 00:13:23 -0400

    Match, new RegeExp, RegexObj [12:12]

--- pokedex/src/components/utility/SpecialCharRegex.js
+++ pokedex/src/components/utility/SpecialCharRegex.js
@@ -2,11 +2,26 @@
 export default async function SpecialCharRegex (str, escapechar) {
     if (typeof str === 'string' && typeof str === 'string') {
         console.log(str)
-        console.log(escapechar)
-        let newstr = str.replace(/[\escapechar]/g, '')
+        
+        let newstr = str.replace(/[\_]/g, '')   
+
+        const regex = /\escapechar/
+        let newerstring = new RegExp(`/${escapechar}/`)
+        let paramMatch = str.replace(newerstring)
+        console.log('paramMatch')
+        console.log(paramMatch)
+
         console.log('newstr')
         console.log(newstr)
+
+        
+        
+        // let newstr = str.replace(/[\${escapechar}]/g, )                
         return newstr
         // as i get to this point right here i suspect this could need whitespace-escaping as well.
     }
 }
+
+
+// https://stackoverflow.com/questions/1695633/how-to-pass-a-variable-into-regex-in-jquery-javascript
+// hmm
