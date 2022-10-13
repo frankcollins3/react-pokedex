c59ab20bce616eb5949869e2a2f325f7dc2df494
Frank Collins <fwc3rd@gmail.com>
2022-10-13 19:51:55 -0400

    [NewRealistic] [7:36pm]
    Math.floor(Math.random() * [1, 2, 3].length) // might be trivial but didn't know you could do this.
    
    so there are 15 original types and 7 energy cards.
    A few energy cards are going to share types. (psychic type will encompass: 'psychic', 'poison', 'ghost')
    
    the tool is set up to return type in a way of that considers priority of which type we want returned.
    i.e--> charizard type[1] === 'flying' type[2] === 'fire' we definitely want it to be fire and not flying. 
    
    next up is looping through [props.fakeDbState] and returning the types so we can

--- pokedex/src/components/element/NewRealistic.js
+++ pokedex/src/components/element/NewRealistic.js
@@ -7,6 +7,8 @@ import myCSS from '../utility/CSStool'
 import toggleHideShow from '../utility/hideShow';
 import {Alert, Button, Card, Carousel}  from 'react-bootstrap';   
 import GroupByType from '../utility/GroupByType'
+import ReturnTypes from '../utility/ReturnTypes'
+import ReturnRandom from '../utility/ReturnRandom'
 
 // $('*').css('overflow', 'scroll')
 $('.Real-Screen')
@@ -27,9 +29,10 @@ function RealisticScreen(props) {
         console.log(fakedb)
         fakedb.forEach(async(dbitem) => {
             console.log(dbitem)
-            let dbitemdata = await APIcall('specify', dbitem)
-            console.log('dbitemdata')
-            console.log(dbitemdata)
+            // let dbitemdata = await APIcall('specify', dbitem)
+            // was thinking of making a ReturnType(s) tool. APIcall has several other operations on it and wonder if the issue could come from async
+            // console.log('dbitemdata')
+            // console.log(dbitemdata)
 
         })
     }, [])
@@ -37,6 +40,20 @@ function RealisticScreen(props) {
     useEffect( () => {
         console.log(props.selectedType)
         console.log(fakedb)
+        const findTypeData = async () => {
+            let randompokemon = await ReturnRandom(fakedb) || Math.floor(Math.random() * [1, 2, 3].length)
+            console.log('randompokemon')
+            console.log(randompokemon)
+            let typedata = await ReturnTypes(randompokemon + 1) // somehow grabbing 0 from our [1,2,3].length
+            // this now returns grass instead of poison for bulbasaur. 
+            console.log('typedata')
+            console.log(typedata)
+
+        }
+        findTypeData()
+            
+
+        
     }, [props.selectedType])
 
     // * AUTO CLICK FUNCTION TO FIRE EVERY TIME state is toggled.
@@ -44,23 +61,9 @@ function RealisticScreen(props) {
     const checkThat = async (event) => {
 
     }
-
-    
-
     const indexChanger = async () => {
        
     }
-
-
-    // let dbmap = fakedb.map( (mapitem, idx) => {        
-    //     return (
-    //         <div key={`parent:${idx}`} onClick={checkThat} className="Screen-Parents">
-    //                 <p className="Real-Screen-P" key={idx}> {mapitem} </p>
-    //         </div>
-    //     )
-    // })
-    
-
     return (        
         <div onClick={indexChanger} onWheel={indexChanger} className="Real-Screen Column-Center">
         {/* <p className="Display-Poke" onClick={checkThat}> { fakedb[scrollIdx] || fakedb[0]} </p>  */}
