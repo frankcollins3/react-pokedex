b36396a847bfb82e9a6823575899c1d4faccb862
Frank Collins <fwc3rd@gmail.com>
2022-10-29 20:46:54 -0400

    conditional rendering to add <captcha/>  [8:15pm]

--- pokedex/src/components/webpage/MainScreen.js
+++ pokedex/src/components/webpage/MainScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react'
 import APIcall from '../utility/pokeAPI'
 import Screen from '../element/Screen'
 import BootstrapScreen from '../element/Bootstrap'
+import Captcha from '../element/Captcha'
 import Nav from '../element/NavBar'
 
 function Main (props) {
@@ -10,24 +11,32 @@ function Main (props) {
     // console.log(props.ghost)
     // console.log(props.fakeDbState)
 
+    console.log()
+
     const [pokedexClick, setPokedexClick] = useState('false')
+    const [catchEmAll, setCatchEmAll] = useState([])
     // I didn't think of the navbar also needing access to this state.
 
     return (
         // <>
-        <div className="Column-Center">                        
-        {pokedexClick === 'true' ?
-        <Nav />
+        <div className="Column-Center">   
+        <Captcha />                     
+        {pokedexClick === 'true'  ?
+
+        <Nav ghost={props.ghost} setGhost={props.setGhost} />
         :
         ''
         }
-
+        { catchEmAll === 'true' ? 
         <BootstrapScreen 
         pokedexClick={pokedexClick}
         setPokedexClick={setPokedexClick}
         ghost={props.ghost} setGhost={props.setGhost}
         fakeDbState={props.fakeDbState} setFakeDbState={props.setFakeDbState}
         />
+        :
+        ''
+            }
         </div>
     
         // </>
