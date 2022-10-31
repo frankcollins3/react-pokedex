7053b3ae3ce9dc451a1a7054504e2283fade1e53
Frank Collins <fwc3rd@gmail.com>
2022-10-31 04:15:10 -0400

    [Captcha] created captcha Container [4:15am]
    was going to use a for loop to create the grid items, but might want to use useRefs and react DOM so might want to have elements and inline props 
    instead of using jQuery to create the elements and do our styling which can be obstructive and problematic
    
    this is a simple captcha to pass to load the pokedex (as the pre-introduction) and might have an optional skip / bypass button

---
+++ pokedex/src/components/element/Captcha.js
@@ -0,0 +1,25 @@
+import { $ } from 'react-jquery-plugin';
+let i = 0;
+
+function Captcha (props) {
+    
+    // for (i; i < 9; i++) {
+    //     console.log("hey out here!");
+    //     let box = document.createElement('div');
+    //     $(box)
+    //     .css('border', '5px solid green')
+    //     .css('height', '100px')
+    //     .css('width', '100px')
+    //     // $(box).appendTo($('.Captcha-Cont'))
+    //     $('.Captcha-Cont').append( $(box) )
+    // }
+
+
+    return (
+        <div className="Captcha-Cont">
+
+        </div>
+    )
+}
+
+export default Captcha
