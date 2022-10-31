f3f057cfa6e203b0b55332ddbaa9f0083c347af6
Frank Collins <fwc3rd@gmail.com>
2022-10-31 12:24:43 -0400

    [Captcha] useRef and jQ [12:00pm]

--- pokedex/src/components/element/Captcha.js
+++ pokedex/src/components/element/Captcha.js
@@ -1,8 +1,26 @@
 import { $ } from 'react-jquery-plugin';
+import React, { useRef, useEffect, useState } from 'react';
+import myCSS from '../utility/CSStool';
+
+
+
+
 let i = 0;
 
+
+
 function Captcha (props) {
+    let boxRef = useRef()
+    let curr = boxRef.current
+
+
     
+    const mousecheck = () => {
+        console.log("hey were entering our mouse");
+        myCSS($(boxRef.current), 'border', '5px solid hotpink');
+    }
+    
+
     // for (i; i < 9; i++) {
     //     console.log("hey out here!");
     //     let box = document.createElement('div');
@@ -16,8 +34,18 @@ function Captcha (props) {
 
 
     return (
-        <div className="Captcha-Cont">
-
+        <div className="Captcha-Cont Column-Between">
+            <div onMouseEnter={mousecheck} ref={boxRef} className="Captcha-Box"></div>
+            <div className="Captcha-Box"></div>
+            <div className="Captcha-Box"></div>
+            <div className="Captcha-Box"></div>
+            <div className="Captcha-Box"></div>
+            <div className="Captcha-Box"></div>
+            <div className="Captcha-Box"></div>
+            <div className="Captcha-Box"></div>
+            <div className="Captcha-Box"></div>
+            
+            
         </div>
     )
 }
