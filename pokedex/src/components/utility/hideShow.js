644186d3905fb99c0e5b33bf9744ab50acbee19c
Frank Collins <fwc3rd@gmail.com>
2022-11-03 18:29:22 -0400

    [hideShow] .detach() as possible str-arg [6:27pm]
    added .detach() as a possible parameter for the pure function. It would be the 2nd argument which is described as action:
               allowing [.hide(), .show(), .detach()]

--- pokedex/src/components/utility/hideShow.js
+++ pokedex/src/components/utility/hideShow.js
@@ -3,34 +3,14 @@ export default async function toggleHideShow (elem, action) {
       
     if (action === 'hide') {
         console.log('action')
-        if (typeof elem === 'object' && elem.length > 1) {
-            console.log('hey we have an array here we go')
-            elem.each( (argumentLoop) => {
-                $(elem).hide()
-            })
-        }
-
-        if (typeof elem === 'object' && elem.length === undefined || elem.length === undefined) {
-            console.log("we have our object distinguished from any accessing of an array in this pure function")
-        }
-
-        if (elem.length === 1) elem.hide()
+         elem.hide()
     // if (typeof elem === 'object' && action === 'hide') {
     } else if (typeof elem === 'object' && action === 'show') {
         elem.show()
     }
+    else if (typeof elem === 'object' && action === 'detach') {
+        elem.detach()
+    }
     
 }
 
-// I want to create a: [hideState, setHideState] hook so we can use toggleHideShow([]) on an array. Surprised to see: typeof food/array return object. 
-// we can use the distinguishing factor separating arrays and objects (arrays return .length true value an object returns a falsy value)
-// i also thought it'd be possible to use a for loop, and if error (an array should return an error to try performing for loop) IF forLoop/Array Error: array.loop(elem)=>toggleHide(elem)
-console.log("check array")
-let food = ['hotdogs', 'cookies', 'blizzards']
-const shakes = { blizzard: 'peanut butter', shake: 'vanilla'}
-// console.log(typeof food) interesting enough this returns an object but returns a .length of 3
-// console.log(typeof shakes) this returns object as well but returns undefined for length. that is how we can check for an array.
-console.log(food.length)
-console.log(shakes.length)
-// check array
-// Bootstrap.js:21 object
