f993008f6048794cffed13770c59bd981a89bbef
Frank Collins <fwc3rd@gmail.com>
2022-11-03 19:43:47 -0400

    [Captcha] ghost works [7:00pm]
    if a ghost pokemon is returned ('gastly', 'haunter', 'gengar')  {
           ** img.detach() && div.backgroundImage === 'ghost' **
    }

--- pokedex/src/components/element/Captcha.js
+++ pokedex/src/components/element/Captcha.js
@@ -6,6 +6,7 @@ import GetSiblings from '../utility/JqSiblings'
 import GetChildren from '../utility/JqChildren'
 import attrTool from '../utility/attrTool' // selector, attr, value in most cases [$(obj), id || img-src, 'New-Value']
 import toggleHideShow from '../utility/hideShow'
+import ReturnTypes from '../utility/ReturnTypes'
 
 import { useInView } from 'react-intersection-observer'
 
@@ -25,6 +26,7 @@ function Captcha (props) {
     const [stateInt, setStateInt] = useState(0); // used to increment until 9 (the length of all grid items) and restart when every grid item activated by hover
     const [hoverImage, setHoverImage] = useState('')
     const [randomInt, setRandomInt] = useState(0); 
+    const [hoverType, setHoverType] = useState('');
 
     let vanillaJSDiv = document.querySelectorAll('div');
     let container = $('.Captcha-Cont')
@@ -36,11 +38,9 @@ function Captcha (props) {
          
         if (stateInt === 9) {
             setStateInt(0)
-            myCSS($(event.target), 'border', '5px solid hotpink')  
             let allDivBox = await GetChildren(container)
             let grandKids = await GetChildren(allDivBox)
             console.log(allDivBox);
-            myCSS(allDivBox[0], 'border', '5px dotted orange');
             toggleHideShow(grandKids, 'detach');
             // confused why allDivBox deletes the parent container when its the children that is specified.
             // more confusing when you see this code working for the children and not the parent  myCSS(allDivBox[0], 'border', '5px dotted orange');
@@ -58,8 +58,31 @@ function Captcha (props) {
 
             const stateSetImageGet = async () => {
                 setStateInt(stateInt + 1)
-                let randompokemon = await ReturnRandomPoke(1);        
-                let randomimage = await GetImage(randompokemon.name, 'front')
+                let randompokemon = await ReturnRandomPoke(1);    
+                let name = randompokemon.name
+                let containerChildren = await GetChildren(container)
+                let childrensChildren = await GetChildren(containerChildren);
+
+                if (name === 'gastly' || name === 'haunter' || name === 'gengar') {
+                    // let type = await ReturnTypes(name) I was going to use this code but i set up the ghosts to return psychic to accommodate the second page.
+                    // leaving for proof-of-concept that i'd just keep the returned ghost array for (haunter,gastly,gengar) as [psychic, ghost]
+                    // you would evaluate and if any type was ghost, make the ghost-based changes like change the background to whose-haunter.png
+                    setStateInt(8)
+                    await attrTool(childrensChildren, 'src', '')
+                    await toggleHideShow(childrensChildren, 'detach');
+
+                    myCSS(containerChildren, 'background-image', `url(${'/img/haunter.png'})`);
+                    myCSS(containerChildren, 'background-size', 'cover');
+                    myCSS(containerChildren, 'background-repeat', 'no-repeat');    
+                    
+                    // let ghostimage = await GetImage(name, 'back')
+                    // setHoverImage(ghostimage);
+                }  else {
+                    myCSS(containerChildren, 'background-image', `url(${'/img/openPokeBall.png'})`);    
+                    myCSS(containerChildren, 'background-size', '1000%')                
+                }               
+
+                let randomimage = await GetImage(name, 'front')
                 setHoverImage(randomimage);
             }
     
@@ -78,7 +101,7 @@ function Captcha (props) {
 
                 if (hoverImage.length < 3) {
                 // if (hoverImage === null || hoverImage === undefined) { // thought this would work it doesn't.
-                    myCSS(target, 'border', '5px dotted limegreen');
+                    // myCSS(target, 'border', '5px dotted limegreen');
                 } else {
                     myCSS($(img), 'background-image', `url('${hoverImage}')`)
                     attrTool($(img), 'src', hoverImage);
