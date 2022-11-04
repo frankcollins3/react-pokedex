4460229ee00cd2bfdd9996a82437db9e752e8375
Frank Collins <fwc3rd@gmail.com>
2022-11-04 09:51:39 -0400

    [MainScreen] import jQ as backup doc [9:51am]
    let doc = window ||  $(document) 
            // document didn't work window did.
    
    console.log(document)  console.log( $(document) )  no documentObject from .document 
    
    only the reactElements get returned

--- pokedex/src/components/webpage/MainScreen.js
+++ pokedex/src/components/webpage/MainScreen.js
@@ -4,6 +4,8 @@ import Screen from '../element/Screen'
 import BootstrapScreen from '../element/Bootstrap'
 import Captcha from '../element/Captcha'
 import Nav from '../element/NavBar'
+import { $ } from 'react-jquery-plugin'
+
 
 function Main (props) {
     // console.log('props from Main St.')
@@ -11,28 +13,39 @@ function Main (props) {
     // console.log(props.ghost)
     // console.log(props.fakeDbState)
 
-    console.log()
+    useEffect( () => {
+        console.log('props.currentUrl')
+        console.log(props.currentUrl)
+        let doc = window ||  $(document) 
+        // document didn't work window did.
+        console.log('doc')
+        console.log(doc)
+
+    }, [])
 
     const [pokedexClick, setPokedexClick] = useState('false')
-    const [catchEmAll, setCatchEmAll] = useState('false')
-    const [lock, setLock] = useState('locked')
+
+    // const [lock, setLock] = useState('locked')
     // I didn't think of the navbar also needing access to this state.
 
     return (
         // <>
         <div className="Column-Center">   
         <Captcha
-         catchEmAll={catchEmAll} setCatchEmAll={setCatchEmAll}
-         lock={lock} setLock={setLock}
-        />                     
+         catchEmAll={props.catchEmAll} setCatchEmAll={props.setCatchEmAll}  lock={props.lock} setLock={props.setLock}
+         />                     
         {pokedexClick === 'true'  ?
 
-        <Nav ghost={props.ghost} setGhost={props.setGhost} />
-        :
-        ''
+        <Nav
+         ghost={props.ghost} setGhost={props.setGhost}
+         catchEmAll={props.catchEmAll} setCatchEmAll={props.setCatchEmAll}  lock={props.lock} setLock={props.setLock}
+         />
+         :
+         ''
         }
-        { catchEmAll === 'true' ? 
+        { props.catchEmAll === 'true' ? 
         <BootstrapScreen 
+        catchEmAll={props.catchEmAll} setCatchEmAll={props.setCatchEmAll}  lock={props.lock} setLock={props.setLock}
         pokedexClick={pokedexClick}
         setPokedexClick={setPokedexClick}
         ghost={props.ghost} setGhost={props.setGhost}
