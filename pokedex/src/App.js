a423d6fd4eb4b14bafb04e590f5e958a6f818872
Frank Collins <fwc3rd@gmail.com>
2022-11-04 09:11:55 -0400

    first 2 webpages have access to state [9:11am]

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Main from './components/webpage/MainScreen'
 import TypeScreen from './components/webpage/TypeScreen'
+import InfoScreen from './components/webpage/InfoScreen'
 
 import React, { useEffect, useState} from 'react'
 
@@ -9,6 +10,9 @@ import './App.css';
 function App() {
   const [fakeDbState, setFakeDbState] = useState([])
   const [ghost, setGhost] = useState('false')
+  const [catchEmAll, setCatchEmAll] = useState('false')
+  const [lock, setLock] = useState('locked')
+
 
 
   return (
@@ -16,8 +20,20 @@ function App() {
       <Router>
 
       <Routes>
-      <Route path={'/'} element={<Main fakeDbState={fakeDbState} setFakeDbState={setFakeDbState} ghost={ghost} setGhost={setGhost}/>}/>
-      <Route path={'/pokemon'} element={<TypeScreen fakeDbState = {fakeDbState} setFakeDbState={setFakeDbState} ghost={ghost} setGhost={setGhost}/>}/>
+
+      <Route path={'/'} element={
+      <Main 
+       fakeDbState={fakeDbState} setFakeDbState={setFakeDbState} ghost={ghost} setGhost={setGhost} 
+       catchEmAll={catchEmAll} setCatchEmAll={setCatchEmAll} lock={lock} setLock={setLock}
+      />}/>
+
+      <Route path={'/pokemon'} element={
+      <TypeScreen 
+      fakeDbState = {fakeDbState} setFakeDbState={setFakeDbState} ghost={ghost} setGhost={setGhost}
+      catchEmAll={catchEmAll} setCatchEmAll={setCatchEmAll} lock={lock} setLock={setLock}
+      />}/>
+      
+      <Route path={'/pokemon/:id'} element={<InfoScreen/>}/>
       </Routes>
 
       </Router>
