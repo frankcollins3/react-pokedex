eb2c15dd01501778233ada431e9fd97bdc550031
Frank Collins <fwc3rd@gmail.com>
2022-11-04 21:38:49 -0400

    Add files via upload

---
+++ pokedex/src/components/element/MoonShoe.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react'
+
+function Footer() {
+
+    return (
+        <div>
+            <div className="Sprite Half-Size" id="NightModeBtn"> </div>        
+        </div>
+    )
+}
+
+export default Footer
