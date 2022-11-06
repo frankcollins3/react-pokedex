d6a676b0422e58b345ed2edb3ca20948d35ba072
Frank Collins <fwc3rd@gmail.com>
2022-11-06 15:39:08 -0500

    [StarterPokemon] match id with evolution-chain [3:36pm]
    performing modularized function expressions locally for damage control.
    
    the alternative is:
     going back and re-doing the evolutionChain modular function and re-doing the code that iterated through that very modular function expressions
    changing the parameters and the configuration of the return data
    
    when we click the starter pokeball we will have either id# [1 for bulbasaur, 4 charmander, 7 squirtle] 
    we are matching this first id against the first id in the evolution chain API
    
    if there is a match return all ID's in the evolution chain and change the (fakeDbState) to include this data, 
    thus "seeding" the database with the evolution chain of our random selection that is made during the pre-intro-page-captcha

--- pokedex/src/components/element/StarterPokemon.js
+++ pokedex/src/components/element/StarterPokemon.js
@@ -4,12 +4,15 @@ import APIcall from '../utility/pokeAPI'
 import { $ } from 'react-jquery-plugin'
 import EvolutionChain from '../utility/Evolution'
 import CleanUrl from '../utility/CleanUrlTool'
-import { collapseTextChangeRangesAcrossMultipleVersions, createNoSubstitutionTemplateLiteral } from 'typescript'
+import Axios from 'axios'
+
+let evolutionUrl = `https://pokeapi.co/api/v2/evolution-chain`
 
 
 function StarterPokemon (props) {
     const [ulHover, setUlHover] = useState(false)
     const [start, setStart] = useState(false)
+    const [evolveChain, setEvolveChain] = useState([])
  
     const listHover = () => {
         setUlHover(true)
@@ -25,14 +28,70 @@ function StarterPokemon (props) {
     
         let chain = await EvolutionChain(randomPokemon)
         
-        const chainpromise =  Promise.resolve(chain)
-        chainpromise.then(async(promiseparam) => {
-            await console.log(promiseparam)
-            promiseparam.map( (mapitem) => {
-                console.log('mapitem')
-                console.log(mapitem)
-            })                 
-        })
+        // const chainpromise =  Promise.resolve(chain)
+        // chainpromise.then(async(promiseparam) => {
+        //     await console.log(promiseparam)
+        //     promiseparam.map( (mapitem) => {
+        //         console.log('mapitem')
+        //         console.log(mapitem)
+        //     })                 
+        // })
+
+        let chainpromise = new Promise(resolve => {
+            // let evolvecall =  Axios.get(evolutionUrl).then( (data) => {
+            Axios.get(evolutionUrl).then(async(data) => {
+                console.log('data')
+                console.log(data)
+                let results = data.data.results
+                
+                let one = results[0].url
+                let two = results[1].url
+                let three = results[2].url
+
+                let cleanone = await CleanUrl(one)
+                let cleantwo = await CleanUrl(two)
+                let cleanthree = await CleanUrl(three)
+
+                let prechain1 = await Axios.get(one)
+                let chain1 = prechain1.data.chain
+                let firsturl = chain1.species.url
+                let firstid = await CleanUrl(firsturl)
+
+                let prechain2 = await Axios.get(two)
+                let chain2 = prechain2.data.chain
+                let chain2firsturl = chain2.species.url
+                let chain2id = await CleanUrl(chain2firsturl)
+
+                let prechain3 = await Axios.get(two)
+                let chain3 = prechain3.data.chain
+                let chain3firsturl = chain3.species.url
+                let chain3id = await CleanUrl(chain3firsturl)
+
+                
+                
+                
+        
+
+                // these just return the URL data
+                // Axios.get(`${evolutionUrl}/1`).then( (data) => {
+                //     console.log('data')
+                //     console.log(data)
+                // })
+                
+            })
+
+
+            resolve(chain);
+          });          
+          chainpromise.then(arr => {
+            if (arr.length > 0) {
+            console.log('arr')
+            console.log(arr)
+              // loop over arr          
+            }
+          });
+
+
         console.log('chainpromise')
         console.log(chainpromise)
 
