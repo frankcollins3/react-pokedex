eb451cda55cefeeb3ce8b8c843610a21048fd700
Frank Collins <fwc3rd@gmail.com>
2022-11-06 04:22:23 -0500

    constrained-by-state 1 option starter [4:22am]
    (2) [{…}, {…}]
    {abilities: Array(2), base_experience: 64, forms: Array(1), game_indices: Array(20), height: 7, …} 
    {name: 'bulbasaur', id: 1, image: 'https:raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'}
    
    success! you can only pick 1 pokeball and the others don't run any function

--- pokedex/src/components/element/StarterPokemon.js
+++ pokedex/src/components/element/StarterPokemon.js
@@ -1,13 +1,25 @@
 import React, { useEffect, useState} from 'react'
+import ReturnRandom from '../utility/ReturnRandom'
+import APIcall from '../utility/pokeAPI'
 
 
 function StarterPokemon () {
     const [ulHover, setUlHover] = useState(false)
-
+    const [start, setStart] = useState(false)
+ 
     const listHover = () => {
-        console.log(listHover)
         setUlHover(true)
     }
+
+    const starterpokemon = async () => {
+        await setStart(true)
+        let options = [1, 4, 7]
+        let randomPokemon = await ReturnRandom(options)
+        let pokedata = await APIcall('specify', randomPokemon)
+        console.log(pokedata)
+    }
+
+    const doNothing = () => console.info('nothing')
 
     return (
         <ul onMouseEnter={listHover} id="Start-List">
@@ -22,18 +34,27 @@ function StarterPokemon () {
             style={{ 
                 backgroundImage: `url(${'/img/pokeball.png'})`,
                 display: ulHover === false ? 'none' : 'block'
-                }} ></li>
-            
+            }} 
+            onClick={start === true ? doNothing : starterpokemon}
+            ></li>            
+
             <li 
             style={{ 
                 backgroundImage: `url(${'/img/pokeball.png'})`,
                 display: ulHover === false ? 'none' : 'block'
-                }} ></li>            
+            }}
+            // onClick={starterpokemon}
+            onClick={start === true ? doNothing : starterpokemon}
+            ></li>            
             <li 
             style={{ 
                 backgroundImage: `url(${'/img/pokeball.png'})`,
                 display: ulHover === false ? 'none' : 'block'
-                }} ></li>
+            }} 
+            // onClick={starterpokemon}
+            onClick={start === true ? doNothing : starterpokemon}
+            >
+                </li>
             
             
             
