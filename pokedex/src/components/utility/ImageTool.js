8a399cd756d39547351c38e893e59d9acd4b0ca9
Frank Collins <fwc3rd@gmail.com>
2022-11-11 21:37:52 -0500

    [ImageTool] returns image but returns err [9:36pm]
    this err doesn't cause a problem on local machine but does not return the image on the deployed app

--- pokedex/src/components/utility/ImageTool.js
+++ pokedex/src/components/utility/ImageTool.js
@@ -1,7 +1,9 @@
 import APIcall from './pokeAPI'
+import Axios from 'axios'
+let url = `https://pokeapi.co/api/v2/pokemon/`
  
 export default async function GetImage(pokemon, imgtype) {
-    console.log(pokemon)
+    // console.log(pokemon)
     // 1) this tool will accept a "pokemon" parameter which would be either the pokemon.name or pokemon.id 
     // 2) use that string data to access pokeapi so we can return image data by accessing the poke data with out pure-function imgtype parameter. We will make a url that uses the pokemon parameter to fetch our data.
     // console.log("getImage tool! options are: [ front || back || shiny ]")
@@ -10,29 +12,32 @@ export default async function GetImage(pokemon, imgtype) {
     else if (typeof pokemon === 'number') keypiece = pokemon  // this number would be a returned string already. 
 
     let dataForImage = await APIcall('specify', keypiece)
-    let actualPokeAPI = dataForImage[0]
+    let datacall = await Axios(`${url}${pokemon}`)
+
+    console.log('datacall')
+    console.log(datacall)
+
+    let actualPokeAPI = datacall.data
+    // let actualPokeAPI = dataForImage[0]
+
     let images = actualPokeAPI.sprites
 
     if (imgtype === 'all') {
         return images
 
-    } else if (imgtype === 'front' || imgtype === 'back') {
+    } else if (imgtype === 'front' || imgtype === 'back' || imgtype.includes('shiny')) {
         
         const front = images.front_default     // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
         const back = images.back_default
-        if (imgtype == 'front') return front
-        if (imgtype == 'back') return `${back}`
+        const frontshiny = images.front_shiny
+        const backshiny = images.back_shiny
         
-    } else {
-        console.log("were in the else block")
-        const shiny1 = images.front_shiny
-        const shiny2 = images.back_shiny
+        if (imgtype == 'front') return front
+        if (imgtype == 'back') return back
         // if (typeof imgtype == 'string') {
-            if (imgtype == 'frontshiny') {
-                return shiny1
-            } 
-            if (imgtype == 'backshiny') return `${shiny2}`
-        }
+            if (imgtype == 'frontshiny') return frontshiny
+            if (imgtype == 'backshiny') return backshiny
+        }    
     }
     
 
